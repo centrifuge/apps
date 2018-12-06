9ed61c9a2592f0f88d2740c60e11b93f8b78d8cf
Vasil Dininski <dininski@gmail.com>
2018-12-06 12:35:31 +0100

    Push docker image (#35)
    Add deploy scripts for pushing to docker hub #29

--- server/src/centrifuge-client/centrifuge.client.ts
+++ server/src/centrifuge-client/centrifuge.client.ts
@@ -1,12 +1,10 @@
 import { tokens } from './centrifuge.constants';
+import { env } from 'process';
 import { DocumentServiceApi } from '../../../clients/centrifuge-node/generated-client';
-
-// TODO: read this from config
-const basePath = 'https://localhost:8082';
 
 export const centrifugeClientFactory = {
   provide: tokens.centrifugeClientFactory,
   useFactory: () => {
-    return new DocumentServiceApi({ basePath });
+    return new DocumentServiceApi({ basePath: env.CENTRIFUGE_URL || 'https://localhost:8082' });
   },
 };
