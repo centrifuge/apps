5e4a5b1153945a5799d123c077f005e314032bb2
Vasil Dininski <dininski@gmail.com>
2018-12-07 11:59:59 +0100

    Update build and push script

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -3,8 +3,8 @@ GIT_SHORT_COMMIT=`git rev-parse --short HEAD`
 TRAVIS_BRANCH=`git rev-parse --abbrev-ref HEAD`
 TIMESTAMP=`date -u +%Y%m%d%H`
 TAG="${TRAVIS_BRANCH}-${TIMESTAMP}-${GIT_SHORT_COMMIT}"
-IMAGE_NAME="${DOCKER_USERNAME}/go-centrifuge"
-docker "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+IMAGE_NAME="${DOCKER_USERNAME}/centrifuge-ui-kit"
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t "${IMAGE_NAME}:${TAG}" .
 docker tag "${IMAGE_NAME}:${TAG}" "${IMAGE_NAME}:latest"
 docker push "${IMAGE_NAME}:latest"
