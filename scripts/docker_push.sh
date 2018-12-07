d205ebd58229b89d47923e8dc757e71f2718e759
Vasil Dininski <dininski@gmail.com>
2018-12-07 11:14:42 +0100

    Move build step to deploy script

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -5,6 +5,7 @@ TIMESTAMP=`date -u +%Y%m%d%H`
 TAG="${TRAVIS_BRANCH}-${TIMESTAMP}-${GIT_SHORT_COMMIT}"
 IMAGE_NAME=centrifugeio/go-centrifuge
 docker "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker build -t "${IMAGE_NAME}:${TAG}" .
 docker tag "${IMAGE_NAME}:${TAG}" "${IMAGE_NAME}:latest"
 docker push "${IMAGE_NAME}:latest"
 docker push "${IMAGE_NAME}:${TAG}"
