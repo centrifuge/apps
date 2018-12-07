b4be6ecc0cb34fb935fe62ced748c4292e85246f
Vasil Dininski <dininski@gmail.com>
2018-12-07 12:05:39 +0100

    Update image name

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -3,7 +3,7 @@ GIT_SHORT_COMMIT=`git rev-parse --short HEAD`
 TRAVIS_BRANCH=`git rev-parse --abbrev-ref HEAD`
 TIMESTAMP=`date -u +%Y%m%d%H`
 TAG="${TRAVIS_BRANCH}-${TIMESTAMP}-${GIT_SHORT_COMMIT}"
-IMAGE_NAME="${DOCKER_USERNAME}/centrifuge-ui-kit"
+IMAGE_NAME="centrifugeio/centrifuge-ui-kit"
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build -t "${IMAGE_NAME}:${TAG}" .
 docker tag "${IMAGE_NAME}:${TAG}" "${IMAGE_NAME}:latest"
