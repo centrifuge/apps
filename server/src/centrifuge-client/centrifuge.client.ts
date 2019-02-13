83fb99f0bc57cf7841ef4ce737d52986e0134460
Vasil Dininski <dininski@gmail.com>
2019-02-13 17:56:51 +0200

    Add authorization header (#86)

--- server/src/centrifuge-client/centrifuge.client.ts
+++ server/src/centrifuge-client/centrifuge.client.ts
@@ -1,11 +1,28 @@
 import { tokens } from './centrifuge.constants';
 import { DocumentServiceApi } from '../../../clients/centrifuge-node/generated-client';
 import config from '../config';
+import * as portableFetch from 'portable-fetch';
+
+const fetchWithHeaders = (url, options = { headers: {} }, ...rest) => {
+  return portableFetch(
+    url,
+    {
+      ...(options && options),
+      headers: {
+        Authorization: config.centrifugeId,
+      },
+      ...(options && options.headers && options.headers),
+    },
+    ...rest,
+  );
+};
 
 // set up singleton centrifuge node client
-const centrifugeClient = new DocumentServiceApi({
-  basePath: config.centrifugeUrl,
-});
+const centrifugeClient = new DocumentServiceApi(
+  {},
+  config.centrifugeUrl,
+  fetchWithHeaders,
+);
 
 export const centrifugeClientFactory = {
   provide: tokens.centrifugeClientFactory,
--- server/src/config.ts
+++ server/src/config.ts
@@ -4,6 +4,7 @@ const config = {
   centrifugeUrl: env.CENTRIFUGE_URL || 'https://localhost:8082',
   sessionSecret: env.SESSION_SECRET || 'centrifuge',
   dbPath: env.DB_PATH || '.',
+  centrifugeId: env.CENTRIFUGE_ID,
 };
 
 export default config;
