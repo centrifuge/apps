a66d9ab5046631ba529b891c1155df47dee33e75
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-06-13 19:28:22 +0200

    added event parsing from transaction.receipt

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ const SignerProvider = require('ethjs-provider-signer');
 const sign = require('ethjs-signer').sign;
 const Eth = require('ethjs');
 
+const Abi = require("web3-eth-abi");
+const abiCoder = new Abi.AbiCoder();
+const Web3 = require('web3'); // todo replace with sha3 lib
+const web3 = new Web3(); // todo replace with sha3 lib
+
 let eth;
 let contracts;
 let abiDir
@@ -33,14 +38,14 @@ function Tinlake(rpcUrl,mainAddress,privateKey, contractAbi, contractAddresses, 
     eth = new Eth(provider,options);
 
     contracts = {
-        "nft": getContract('test/SimpleNFT.abi', contractAddresses["nft"]),
-        "title": getContract('Title.abi', contractAddresses["title"]),
-        "currency": getContract('test/SimpleToken.abi',contractAddresses["currency"]) ,
-        "admit": getContract('Admit.abi',contractAddresses["admit"]),
-        "reception": getContract('Reception.abi', contractAddresses["reception"]),
-        "desk": getContract('Desk.abi',  contractAddresses["desk"]),
-        "shelf": getContract('Shelf.abi', contractAddresses["shelf"]),
-        "appraiser": getContract('Appraiser.abi', contractAddresses["appraiser"]),
+        "nft": getContract('test/SimpleNFT.abi', contractAddresses["NFT_COLLATERAL"]),
+        "title": getContract('Title.abi', contractAddresses["TITLE"]),
+        "currency": getContract('test/SimpleToken.abi',contractAddresses["CURRENCY"]) ,
+        "admit": getContract('Admit.abi',contractAddresses["ADMIT"]),
+        "reception": getContract('Reception.abi', contractAddresses["RECEPTION"]),
+        "desk": getContract('Desk.abi',  contractAddresses["DESK"]),
+        "shelf": getContract('Shelf.abi', contractAddresses["SHELF"]),
+        "appraiser": getContract('Appraiser.abi', contractAddresses["APPRAISER"]),
     };
 
 
@@ -50,14 +55,105 @@ function Tinlake(rpcUrl,mainAddress,privateKey, contractAbi, contractAddresses, 
 }
 Tinlake.prototype.approveNFT = () => console.log('not implemented yet');
 
-Tinlake.prototype.mintNFT = (deposit, tokenID) => contracts.nft.mint(deposit, tokenID, ethConfig);
+Tinlake.prototype.mintNFT = (deposit, tokenID) => {
+    return new Promise((resolve, reject)=> {
+        contracts.nft.mint(deposit, tokenID, ethConfig).then(txHash => {
+            console.log("[Mint] txHash: "+txHash);
+            return waitForTransaction(txHash)
+        }).then(tx => {
+            eth.getTransactionReceipt(tx.hash, (err, receipt) => {
+                if (err != null) {
+                    reject("failed to get receipt")
+                }
+                let events = getEvents(receipt, contracts["nft"].abi);
+                resolve({"txHash":tx.hash,"events":events});
+            });
 
-Tinlake.prototype.admit = (registry, nft, principal, usr) => contracts.admit.admit(registry, nft, principal, usr, ethConfig);
+        })
+    });
+
+}
+
+Tinlake.prototype.admit = (registry, nft, principal, usr) => {
+    return new Promise((resolve, reject)=> {
+        contracts.admit.admit(registry, nft, principal, usr, ethConfig).then(txHash => {
+            console.log("[Admit] txHash: "+txHash);
+            return waitForTransaction(txHash)
+        }).then(tx => {
+            eth.getTransactionReceipt(tx.hash, (err, receipt) => {
+                if (err != null) {
+                    reject("failed to get receipt")
+                }
+                let events = getEvents(receipt, contracts["nft"].abi);
+                resolve({"txHash":tx.hash,"events":events});
+            });
+
+        })
+    });
+};
 
 Tinlake.prototype.borrow = () => console.log('not implemented yet');
 
+// todo replace with a better pooling
+let waitForTransaction = (txHash) => {
+    return  new Promise(function(resolve, reject) {
+            let secMax = 5;
+            let sec = 0;
+            let wait = (txHash) => {
+                setTimeout(()=> {
+                    eth.getTransactionByHash(txHash, (err, tx) => {
+                        if (tx.blockHash != null)  {
+                            resolve(tx);
+                            return;
+                        }
+                        console.log("waiting for tx :" + txHash)
+                        sec++;
+                        if (sec != secMax) {
+                            wait(txHash);
+                        }
 
-function getContract(file, address) {
+                    });
+                }, 1000);
+
+            }
+            wait(txHash);
+    })
+}
+
+let findEvent = (abi, funcHash) => {
+    return abi.filter((item) => {
+        if (item.type !== "event") return false;
+        let signature = item.name + "(" + item.inputs.map((input) => input.type).join(",") + ")";
+        let hash = web3.utils.sha3(signature);
+        if (hash === funcHash) return true;
+    });
+
+}
+let getEvents = (receipt, abi) => {
+    let log = receipt.logs[0];
+    let events = [];
+
+    let matches = findEvent(abi, log.topics[0]);
+    if (matches.length === 1) {
+        let event = matches[0];
+        let inputs = event.inputs.map((input)=>input.type);
+
+        // remove 0x prefix from topics
+        let topics = log.topics.map((t) => t.replace("0x",""));
+
+        // concat topics without first topic (function signature)
+        let bytes = "0x"+topics.slice(1).join("");
+
+        let data = abiCoder.decodeParameters(inputs, bytes);
+
+        events.push({"event":event,"data":data})
+
+    }
+    return events;
+
+}
+
+let getContract = (file, address)  => {
     let rawdata = fs.readFileSync(abiDir+file);
     return eth.contract(JSON.parse(rawdata)).at(address);
 }
--- test/integration.js
+++ test/integration.js
@@ -1,4 +1,5 @@
 let assert = require('assert');
+let  fs = require("fs");
 const rpcUrl  = process.env['ETH_RPC_URL'];
 
 const Tinlake = require('../src/index.js');
@@ -6,43 +7,34 @@ const Tinlake = require('../src/index.js');
 let tinlake;
 let ethFrom = "0x"+process.env['ETH_FROM'];
 let nftID;
-let addresses = {
-    "nft":process.env['NFT_COLLATERAL'],
-    "title":process.env['TITLE'],
-    "currency":process.env['CURRENCY'],
-    "admit":process.env['ADMIT'],
-    "reception":process.env['RECEPTION'],
-    "desk":process.env['DESK'],
-    "shelf":process.env['SHELF'],
-    "appraiser":process.env['APPRAISER'],
-};
+let addresses = JSON.parse(fs.readFileSync(process.env['ADDRESSES_TINLAKE']).toString());
 
 describe('functional tinlake tests', function() {
     before(() => {
         tinlake = new Tinlake(rpcUrl,ethFrom, process.env['ETH_PRIVATE_KEY'],process.env['CONTRACTS_ABI'],addresses, {});
     })
     describe('tinlake borrow', function() {
-        it('should mint an NFT', () => {
+        it('should mint an NFT & admit', () => {
             nftID = Math.floor(Math.random()*(10**15));
             tinlake.mintNFT(ethFrom,"0x"+nftID).then((result) => {
-                console.log("[Mint NFT] Ethereum Transaction: " + result);
-            }, (err) => {
-              console.log(err);
-            });
-        });
-        it('should admit NFT', () => {
-            let principal = 100;
-            tinlake.admit(addresses["nft"],nftID, principal, ethFrom).then((result) => {
-                console.log("[admit Admit] Ethereum Transaction: " + result);
+                console.log("mint result");
+                console.log(result.txHash);
+                console.log(result.events);
+                let principal = 100;
+               return tinlake.admit(addresses["NFT_COLLATERAL"],nftID, principal, ethFrom);
+            }).then(result  => {
+                console.log("admit result");
+                console.log(result.txHash);
+                console.log(result.events);
+
+                let loanID = result.events[0].data[2].toString();
+                console.log("Loan ID ==> "+loanID);
+
+
             }, (err) => {
                 console.log(err);
             });
 
         });
-        it('should appraiser file NFT', () => {
-            //todo
-        });
     });
-
-
 });
