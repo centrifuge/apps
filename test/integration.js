0a85626ff8f3f78c970b3de9db36e38edff876a7
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2019-06-17 18:05:07 +0200

    improved functional tests with more asserts

--- test/integration.js
+++ test/integration.js
@@ -4,6 +4,8 @@ const rpcUrl  = process.env['ETH_RPC_URL'];
 
 const Tinlake = require('../src/index.js');
 
+const SUCCESS_STATUS = "0x1";
+
 let tinlake;
 let ethFrom = "0x"+process.env['ETH_FROM'];
 let tokenID;
@@ -16,28 +18,33 @@ describe('functional tinlake tests', function() {
     })
     describe('tinlake borrow and repay', function() {
         this.timeout(50000);
-
         let principal = 100;
         let appraisal = 300;
-        console.log("appraisal: "+appraisal);
-        console.log("principal: "+principal);
         it('borrow and repay successful', () => {
+            console.log("appraisal: "+appraisal);
+            console.log("principal: "+principal);
             tokenID = "0x"+Math.floor(Math.random()*(10**15));
-            console.log("Token ID -> "+tokenID);
+            console.log("token id: "+tokenID);
             return tinlake.mintNFT(ethFrom,tokenID).then((result) => {
                 console.log("mint result");
                 console.log(result.txHash);
-                console.log(result.events);
+                assert.equal(result.status,SUCCESS_STATUS,"tx should be successful");
+                assert.equal(result.events[0].event.name,"Transfer","tx should be successful");
+                console.log("-------------------------------------");
+                console.log("admin whitelist NFT");
+                console.log("-------------------------------------");
 
                return tinlake.adminAdmit(addresses["NFT_COLLATERAL"],tokenID, principal, ethFrom);
             }).then(result  => {
                 console.log("admit result");
                 console.log(result.txHash);
-                console.log(result.events);
 
                 // parse loanID from event
                 loanID = result.events[0].data[2].toString();
-                console.log("Loan ID ==> "+loanID);
+                console.log("Loan id: "+loanID);
+
+                assert.equal(result.status,SUCCESS_STATUS,"tx should be successful");
+                assert.equal(result.events[0].event.name,"Transfer","tx should be successful");
                 return tinlake.approveNFT(tokenID, addresses["SHELF"]);
 
             }, (err) => {
@@ -45,31 +52,39 @@ describe('functional tinlake tests', function() {
             }).then(result => {
                 console.log("approve results");
                 console.log(result.txHash);
-                console.log(result.events);
+                assert.equal(result.status,SUCCESS_STATUS,"tx should be successful");
+                assert.equal(result.events[0].event.name,"Approval","tx should be successful");
                 return tinlake.adminAppraise(loanID, appraisal);
 
             }).then(result => {
                 console.log("appraisal results");
                 console.log(result.txHash);
-                console.log(result.events);
+                assert.equal(result.status,SUCCESS_STATUS,"tx should be successful");
+                console.log("-------------------------------------");
+                console.log("borrow");
+                console.log("-------------------------------------");
                 return tinlake.borrow(loanID, ethFrom);
 
             }).then(result => {
-                console.log("borrow results");
                 console.log(result.txHash);
-                console.log(result.events);
+                assert.equal(result.status,SUCCESS_STATUS,"tx should be successful");
                 return tinlake.balanceOfCurrency(ethFrom);
             }).then(balance => {
                 console.log("DAI Balance after borrow");
                 console.log(balance["0"].toString() + " DAI");
+                console.log("-------------------------------------");
                 console.log("repay");
+                console.log("-------------------------------------");
                 return tinlake.approveCurrency(addresses["PILE"],principal);
             }).then(result => {
-                console.log(result);
+                console.log(result.txHash);
+                assert.equal(result.events[0].event.name,"Approval","tx should be successful");
+                assert.equal(result.status,SUCCESS_STATUS,"tx should be successful");
                 return tinlake.repay(loanID, principal,ethFrom, ethFrom);
 
             }).then(result => {
-                console.log(result);
+                console.log(result.txHash);
+                assert.equal(result.status,SUCCESS_STATUS,"tx should be successful");
                 return tinlake.balanceOfCurrency(ethFrom);
 
             }).then(balance => {
