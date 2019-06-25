74225ed4e1c83f363df8a9b4e100979d0a265156
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2019-06-25 22:46:22 +0200

    Migrate to typescript, prepare for web

---
+++ build/defaultContractAddresses.d.ts
@@ -0,0 +1,19 @@
+declare const _default: {
+    "APPRAISER": string;
+    "TITLE": string;
+    "LIGHTSWITCH": string;
+    "PILE": string;
+    "SHELF": string;
+    "COLLATERAL": string;
+    "DESK": string;
+    "RECEPTION": string;
+    "LENDER": string;
+    "CVTJOIN": string;
+    "CVTPIP": string;
+    "NFT_COLLATERAL": string;
+    "DEPLOYER": string;
+    "ADMIT": string;
+    "SPELL": string;
+    "CURRENCY": string;
+};
+export default _default;
