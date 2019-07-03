38458d8b6e7aa4caaac4b6a5fe8e0d51265e8cce
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2019-07-03 16:48:42 +0200

    Add addresses that are no required by tinlake.js

--- services/tinlake/index.ts
+++ services/tinlake/index.ts
@@ -1,5 +1,7 @@
 // tslint:disable-next-line:import-name
 import Tinlake from 'tinlake';
+// tslint:disable-next-line:import-name
+import contractAddresses from './addresses_tinlake.json';
 
 declare var web3: any;
 
@@ -12,7 +14,7 @@ export async function getTinlake() {
   const account = accounts[0];
   // console.log(`Using account ${account}`);
 
-  tinlake = new Tinlake(web3.currentProvider, {
+  tinlake = new Tinlake(web3.currentProvider, contractAddresses, {
     ethConfig: { from: account },
   });
 
