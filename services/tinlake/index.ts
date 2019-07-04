3947645a2c42dcf51a4b06af3037067afe5cb0bb
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2019-07-04 22:02:24 +0200

    Add web3connect

--- services/tinlake/index.ts
+++ services/tinlake/index.ts
@@ -10,13 +10,52 @@ let tinlake: Tinlake | null = null;
 export async function getTinlake() {
   if (tinlake) { return tinlake; }
 
-  const accounts = await web3.currentProvider.enable();
+  // const provider = web3.currentProvider;
+  const provider = await web3Connect();
+
+  const accounts = await provider.enable();
   const account = accounts[0];
   console.log(`Using account ${account}`);
 
-  tinlake = new Tinlake(web3.currentProvider, contractAddresses, {
+  tinlake = new Tinlake(provider, contractAddresses, {
     ethConfig: { from: account },
   });
 
   return tinlake;
+}
+
+async function web3Connect(): Promise<any> {
+  return new Promise((resolve, reject) => {
+    // require here since we only want it to be loaded in browser, not on server side rendering
+    // tslint:disable-next-line:variable-name
+    const Web3Connect = require('web3connect').default;
+
+    console.log({ Web3Connect });
+
+    const web3Connect = new Web3Connect.Core({
+      providerOptions: {
+        portis: {
+          id: '2ea2735d-4963-40f5-823f-48cab29f7319', // required
+          // network: 'mainnet', // optional
+          network: 'kovan', // optional
+        },
+        // fortmatic: {
+        //   key: 'FORTMATIC_KEY', // required
+        // },
+      },
+    });
+
+    // subscibe to connect
+    web3Connect.on('connect', (provider: any) => {
+      resolve(provider);
+      // const web3 = new Web3(provider); // add provider to web3
+    });
+
+    // subscibe to close
+    web3Connect.on('close', () => {
+      reject('Web3Connect Modal Closed');
+    });
+
+    web3Connect.toggleModal(); // open modal on button click
+  });
 }
