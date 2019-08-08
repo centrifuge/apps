6be800f5ab9653660698710875fb30eb56cfea8b
ilinzweilin <ilinzweilin@ilinzweilins-MacBook-Pro.local>
2019-08-08 11:20:01 +0200

    read nft data definition from env

--- bin/export.sh
+++ bin/export.sh
@@ -1,4 +1,6 @@
 #! /usr/bin/env bash
 echo -e $TINLAKE_ADDRESSES
 echo -e $TINLAKE_ADDRESSES > services/tinlake/addresses_tinlake.json
+echo -e $NFT_DATA_DEFINITION
+echo -e $NFT_DATA_DEFINITION > nft_data_definition.json
 npm run export
