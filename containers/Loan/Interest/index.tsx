d0f62cbe225ab7155140c6e0e1277091359216ee
Charly <charly@centrifuge.io>
2020-03-26 11:37:19 +0100

    fix

--- containers/Loan/Interest/index.tsx
+++ containers/Loan/Interest/index.tsx
@@ -48,7 +48,7 @@ class LoanInterest extends React.Component<Props, State> {
     const { interestRate } = this.state;
     return <Box basis={'1/4'} gap="medium" margin={{ right: "large" }}>
       <Box gap="medium">
-        <FormField label="Interest Rate (APY)">
+        <FormField label="Interest Rate (APR)">
           <NumberInput value={interestRate} suffix=" %"
             onValueChange={({ value }) =>
               this.setState({ interestRate: value })}
