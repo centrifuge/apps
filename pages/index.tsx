053568c82806c7d5d15bd68a036be3811f8cd3c5
Philip Stanislaus <6912756+philipstanislaus@users.noreply.github.com>
2020-05-13 12:16:01 +0200

    Remove unused menuItems

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import { Box } from "grommet";
 import Header from "../components/Header";
 import Dashboard from "../containers/Dashboard";
-import { menuItems } from "../menuItems";
 
 function Home() {
   return (
     <Box align="center" pad={{ horizontal: "small" }}>
-      <Header selectedRoute={"/"} menuItems={menuItems} />
+      <Header selectedRoute={"/"} menuItems={[]} />
       <Box justify="center" direction="row">
         <Box width="xlarge">
           <Dashboard />
