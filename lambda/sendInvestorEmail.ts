b76c795f4cea57974610df2aa18cc63caaa0ae37
Jeroen Offerijns <jeroen@offerijns.com>
2020-07-27 12:03:54 +0200

    refactor: remove cors headers

--- lambda/sendInvestorEmail.ts
+++ lambda/sendInvestorEmail.ts
@@ -7,20 +7,10 @@ import { FormSubmission } from '../components/InvestAction/index'
 
 const { SENDGRID_API_KEY, SENDGRID_FROM_EMAIL, INVESTOR_REQUEST_EMAIL } = process.env
 
-const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
-  'Content-Type': 'application/json',
-  'Access-Control-Allow-Methods': '*',
-  'Access-Control-Max-Age': '2592000',
-  'Access-Control-Allow-Credentials': 'true',
-}
-
 exports.handler = async (event: APIGatewayEvent) => {
   if (!SENDGRID_API_KEY || !SENDGRID_FROM_EMAIL || !INVESTOR_REQUEST_EMAIL) {
     return {
       statusCode: 500,
-      headers: corsHeaders,
       body: 'One of the required environment variables for the sendInvestorEmail lambda to work is not set',
     }
   }
@@ -28,7 +18,6 @@ exports.handler = async (event: APIGatewayEvent) => {
   if (event.httpMethod === 'GET') {
     return {
       statusCode: 405,
-      headers: corsHeaders,
       body: 'Method not allowed',
     }
   }
@@ -36,7 +25,6 @@ exports.handler = async (event: APIGatewayEvent) => {
   if (event.body === null) {
     return {
       statusCode: 500,
-      headers: corsHeaders,
       body: 'You need to pass a JSON object for the form submission',
     }
   }
@@ -65,13 +53,11 @@ exports.handler = async (event: APIGatewayEvent) => {
 
     return {
       statusCode: 200,
-      headers: corsHeaders,
       body: 'Success',
     }
   } catch (e) {
     return {
       statusCode: e.code,
-      headers: corsHeaders,
       body: e.message,
     }
   }
