b318143a00ab8b8077da2ad4ba9803dc747a7bce
Jeroen Offerijns <jeroen@offerijns.com>
2020-07-29 16:31:41 +0200

    fix: modify regenerator runtime import

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-import 'regenerator-runtime/runtime.js'
+require('regenerator-runtime/runtime')
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
