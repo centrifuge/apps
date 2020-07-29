29919bb285eeaa8c2a83bc17667b93cf40386907
Jeroen Offerijns <jeroen@offerijns.com>
2020-07-29 16:34:24 +0200

    fix: babel regenerator build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,8 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheet } from 'styled-components'
 
-require('regenerator-runtime/runtime')
-
 export default class MyDocument extends Document {
   static async getInitialProps(ctx: any) {
     const sheet = new ServerStyleSheet()
