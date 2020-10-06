72146bf9186f806c68e896023394e00da20a42f7
Jeroen Offerijns <jeroen@offerijns.com>
2020-10-06 14:58:46 +0200

    feat: implement redeem amount validation

--- containers/Investment/RevolvingPool/View/OrderCard.tsx
+++ containers/Investment/RevolvingPool/View/OrderCard.tsx
@@ -121,7 +121,8 @@ const OrderCard: React.FC<Props> = (props: Props) => {
           {props.epochData?.minChallengePeriodEnd !== 0 && (
             <MinTimeRemaining>
               Minimum time remaining:{' '}
-              {secondsToHms(props.epochData.minChallengePeriodEnd - new Date().getTime() / 1000)}
+              {secondsToHms(props.epochData.minChallengePeriodEnd - new Date().getTime() / 1000)} (
+              {props.epochData.minChallengePeriodEnd})
             </MinTimeRemaining>
           )}
         </Info>
