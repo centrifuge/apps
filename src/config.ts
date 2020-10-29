d7835b475518eed7f698feca8be8b8ca52fb1737
Jeroen Offerijns <jeroen@offerijns.com>
2020-10-29 11:50:18 +0100

    feat: use pool config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,4 @@
+const poolConfigs = require('tinlake-pool-config')
 require('dotenv').config()
 
 export interface Config {
@@ -14,19 +15,30 @@ export interface Config {
   nftRegistry: string
 }
 
+const network = process.env.ETH_NETWORK || 'kovan'
+const networkConfigs = poolConfigs[network === 'kovan' ? 'kovanStaging' : 'mainnetProduction']
+const pool = networkConfigs.find((pool: Pool) => pool.addresses.ROOT_CONTRACT.toLowerCase() === process.env.POOL_ID.toLowerCase())
+
 export const config: Config = {
   gatewayUrl: process.env.GATEWAY_URL,
   tinlakeUrl: process.env.TINLAKE_URL,
   rpcUrl: process.env.RPC_URL,
-  ethNetwork: process.env.ETH_NETWORK || 'kovan',
+  ethNetwork: network,
   ethAdminAddress: process.env.ETH_ADMIN_ADDRESS,
   ethAdminPrivateKey: process.env.ETH_ADMIN_PRIVATE_KEY,
   ethBorrowerAddress: process.env.ETH_BORROWER_ADDRESS,
   ethBorrowerPrivateKey: process.env.ETH_BORROWER_PRIVATE_KEY,
-  pool: JSON.parse(process.env.TINLAKE_POOL),
+  pool: pool,
   gasLimit: Number(process.env.GAS_LIMIT|| '100000') ,
   nftRegistry: process.env.NFT_REGISTRY,
 }
+
+console.log(
+  `Running Tinlake end-to-end test suite:\n` +
+    ` - Pool: ${pool.name || pool.shortName} (${pool.addresses.ROOT_CONTRACT})\n` +
+    ` - Tinlake URL: ${config.tinlakeUrl}\n` +
+    ` - Gateway URL: ${config.gatewayUrl}\n`
+)
 
 export interface Pool {
   addresses: Addresses
