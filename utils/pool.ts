bc5b241b64672712b94bfb040a6fb8614462e9b4
Jeroen Offerijns <jeroen@offerijns.com>
2020-11-18 14:45:21 +0100

    fix: upcoming pool page, remove more v2 code

--- utils/pool.ts
+++ utils/pool.ts
@@ -6,7 +6,7 @@ export const getPoolStatus = (pool: PoolData): PoolStatus => {
   if (pool.isUpcoming) return 'Upcoming'
   if (
     (pool.totalDebt.eqn(0) && pool.totalRepaysAggregatedAmount.eqn(0)) ||
-    (pool.version === 3 && pool.totalDebt.gtn(0))
+    pool.totalDebt.gtn(0)
   ) {
     return 'Active'
   }
