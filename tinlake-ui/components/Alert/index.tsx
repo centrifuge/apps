2e9460108fa1b8a21766693f01ac8a43262371bd
Jeroen Offerijns <jeroen@offerijns.com>
2020-12-09 14:39:36 +0100

    fix: format, env var in workflow

--- tinlake-ui/components/Alert/index.tsx
+++ tinlake-ui/components/Alert/index.tsx
@@ -1,6 +1,6 @@
 import { Box } from 'grommet'
-import styled from 'styled-components'
 import * as React from 'react'
+import styled from 'styled-components'
 
 interface Props {
   type: 'error' | 'info' | 'success'
