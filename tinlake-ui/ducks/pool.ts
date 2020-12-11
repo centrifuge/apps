1e0e0d547c01de240ea1c7c963d1198e0f98f26b
Jeroen Offerijns <jeroen@offerijns.com>
2020-12-11 14:56:09 +0100

    fix: remove remaining non multicall calls

--- tinlake-ui/ducks/pool.ts
+++ tinlake-ui/ducks/pool.ts
@@ -111,148 +111,157 @@ export function loadPool(tinlake: any): ThunkAction<Promise<void>, PoolState, un
 
     const toBN = (val: BigNumber) => new BN(val.toString())
 
-    watcher.recreate(
-      [
-        {
-          target: tinlake.contractAddresses.ASSESSOR,
-          call: ['maxReserve()(uint256)'],
-          returns: [[`maxReserve`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.ASSESSOR,
-          call: ['calcJuniorTokenPrice()(uint256)'],
-          returns: [[`junior.tokenPrice`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.ASSESSOR,
-          call: ['calcSeniorTokenPrice()(uint256)'],
-          returns: [[`senior.tokenPrice`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.ASSESSOR,
-          call: ['seniorBalance_()(uint256)'],
-          returns: [[`senior.availableFunds`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.RESERVE,
-          call: ['totalBalance()(uint256)'],
-          returns: [[`reserve`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.ASSESSOR,
-          call: ['maxSeniorRatio()(uint256)'],
-          returns: [[`minJuniorRatio`, (val: BigNumber) => seniorToJuniorRatio(toBN(val))]],
-        },
-        {
-          target: tinlake.contractAddresses.ASSESSOR,
-          call: ['minSeniorRatio()(uint256)'],
-          returns: [[`maxJuniorRatio`, (val: BigNumber) => seniorToJuniorRatio(toBN(val))]],
-        },
-        {
-          target: tinlake.contractAddresses.ASSESSOR,
-          call: ['seniorRatio()(uint256)'],
-          returns: [[`currentJuniorRatio`, (val: BigNumber) => seniorToJuniorRatio(toBN(val))]],
-        },
-        {
-          target: tinlake.contractAddresses.FEED,
-          call: ['currentNAV()(uint256)'],
-          returns: [[`netAssetValue`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.PILE,
-          call: ['total()(uint256)'],
-          returns: [[`outstandingVolume`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.SENIOR_TRANCHE,
-          call: ['totalSupply()(uint256)'],
-          returns: [[`senior.pendingInvestments`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.SENIOR_TRANCHE,
-          call: ['totalRedeem()(uint256)'],
-          returns: [[`senior.pendingRedemptions`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.JUNIOR_TRANCHE,
-          call: ['totalSupply()(uint256)'],
-          returns: [[`junior.pendingInvestments`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.JUNIOR_TRANCHE,
-          call: ['totalRedeem()(uint256)'],
-          returns: [[`junior.pendingRedemptions`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.SENIOR_TOKEN,
-          call: ['totalSupply()(uint256)'],
-          returns: [[`senior.totalSupply`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.JUNIOR_TOKEN,
-          call: ['totalSupply()(uint256)'],
-          returns: [[`junior.totalSupply`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.RESERVE,
-          call: ['currencyAvailable()(uint256)'],
-          returns: [[`availableFunds`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.ASSESSOR,
-          call: ['seniorInterestRate()(uint256)'],
-          returns: [[`senior.interestRate`, toBN]],
-        },
-        {
-          target: tinlake.contractAddresses.SENIOR_TOKEN,
-          call: ['symbol()(string)'],
-          returns: [
-            [
-              `senior.token`,
-              (symbol: string) => {
-                if (!symbol || symbol.length === 0) {
-                  return `${tinlake.contractAddresses['SENIOR_TOKEN']?.substr(2, 2).toUpperCase()}DRP`
-                }
-                return symbol
-              },
-            ],
-          ],
-        },
-        {
-          target: tinlake.contractAddresses.JUNIOR_TOKEN,
-          call: ['symbol()(string)'],
-          returns: [
-            [
-              `junior.token`,
-              (symbol: string) => {
-                if (!symbol || symbol.length === 0) {
-                  return `${tinlake.contractAddresses['JUNIOR_TOKEN']?.substr(2, 2).toUpperCase()}TIN`
-                }
-                return symbol
-              },
-            ],
-          ],
-        },
-        {
-          target: tinlake.contractAddresses.SENIOR_TOKEN,
-          call: ['decimals()(uint8)'],
-          returns: [[`senior.decimals`]],
-        },
-        {
-          target: tinlake.contractAddresses.JUNIOR_TOKEN,
-          call: ['decimals()(uint8)'],
-          returns: [[`junior.decimals`]],
-        },
-      ],
-      multicallConfig
-    )
-
-    const t0 = performance.now()
     const address = await tinlake.signer?.getAddress()
 
-    // TODO: also get this using multicall
-    const seniorInMemberlist = address ? await tinlake.checkSeniorTokenMemberlist(address) : false
-    const juniorInMemberlist = address ? await tinlake.checkJuniorTokenMemberlist(address) : false
+    const addressWatchers = address
+      ? [
+          {
+            target: tinlake.contractAddresses.SENIOR_MEMBERLIST,
+            call: ['hasMember(address)(bool)', address || '0'],
+            returns: [[`senior.inMemberlist`]],
+          },
+          {
+            target: tinlake.contractAddresses.JUNIOR_MEMBERLIST,
+            call: ['hasMember(address)(bool)', address || '0'],
+            returns: [[`junior.inMemberlist`]],
+          },
+        ]
+      : []
+
+    const globalWatchers = [
+      {
+        target: tinlake.contractAddresses.ASSESSOR,
+        call: ['maxReserve()(uint256)'],
+        returns: [[`maxReserve`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.ASSESSOR,
+        call: ['calcJuniorTokenPrice()(uint256)'],
+        returns: [[`junior.tokenPrice`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.ASSESSOR,
+        call: ['calcSeniorTokenPrice()(uint256)'],
+        returns: [[`senior.tokenPrice`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.ASSESSOR,
+        call: ['seniorBalance_()(uint256)'],
+        returns: [[`senior.availableFunds`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.RESERVE,
+        call: ['totalBalance()(uint256)'],
+        returns: [[`reserve`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.ASSESSOR,
+        call: ['maxSeniorRatio()(uint256)'],
+        returns: [[`minJuniorRatio`, (val: BigNumber) => seniorToJuniorRatio(toBN(val))]],
+      },
+      {
+        target: tinlake.contractAddresses.ASSESSOR,
+        call: ['minSeniorRatio()(uint256)'],
+        returns: [[`maxJuniorRatio`, (val: BigNumber) => seniorToJuniorRatio(toBN(val))]],
+      },
+      {
+        target: tinlake.contractAddresses.ASSESSOR,
+        call: ['seniorRatio()(uint256)'],
+        returns: [[`currentJuniorRatio`, (val: BigNumber) => seniorToJuniorRatio(toBN(val))]],
+      },
+      {
+        target: tinlake.contractAddresses.FEED,
+        call: ['currentNAV()(uint256)'],
+        returns: [[`netAssetValue`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.PILE,
+        call: ['total()(uint256)'],
+        returns: [[`outstandingVolume`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.SENIOR_TRANCHE,
+        call: ['totalSupply()(uint256)'],
+        returns: [[`senior.pendingInvestments`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.SENIOR_TRANCHE,
+        call: ['totalRedeem()(uint256)'],
+        returns: [[`senior.pendingRedemptions`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.JUNIOR_TRANCHE,
+        call: ['totalSupply()(uint256)'],
+        returns: [[`junior.pendingInvestments`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.JUNIOR_TRANCHE,
+        call: ['totalRedeem()(uint256)'],
+        returns: [[`junior.pendingRedemptions`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.SENIOR_TOKEN,
+        call: ['totalSupply()(uint256)'],
+        returns: [[`senior.totalSupply`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.JUNIOR_TOKEN,
+        call: ['totalSupply()(uint256)'],
+        returns: [[`junior.totalSupply`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.RESERVE,
+        call: ['currencyAvailable()(uint256)'],
+        returns: [[`availableFunds`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.ASSESSOR,
+        call: ['seniorInterestRate()(uint256)'],
+        returns: [[`senior.interestRate`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.SENIOR_TOKEN,
+        call: ['symbol()(string)'],
+        returns: [
+          [
+            `senior.token`,
+            (symbol: string) => {
+              if (!symbol || symbol.length === 0) {
+                return `${tinlake.contractAddresses['SENIOR_TOKEN']?.substr(2, 2).toUpperCase()}DRP`
+              }
+              return symbol
+            },
+          ],
+        ],
+      },
+      {
+        target: tinlake.contractAddresses.JUNIOR_TOKEN,
+        call: ['symbol()(string)'],
+        returns: [
+          [
+            `junior.token`,
+            (symbol: string) => {
+              if (!symbol || symbol.length === 0) {
+                return `${tinlake.contractAddresses['JUNIOR_TOKEN']?.substr(2, 2).toUpperCase()}TIN`
+              }
+              return symbol
+            },
+          ],
+        ],
+      },
+      {
+        target: tinlake.contractAddresses.SENIOR_TOKEN,
+        call: ['decimals()(uint8)'],
+        returns: [[`senior.decimals`]],
+      },
+      {
+        target: tinlake.contractAddresses.JUNIOR_TOKEN,
+        call: ['decimals()(uint8)'],
+        returns: [[`junior.decimals`]],
+      },
+    ]
+
+    watcher.recreate([...addressWatchers, ...globalWatchers], multicallConfig)
 
     try {
       const initial = {
@@ -284,9 +293,7 @@ export function loadPool(tinlake: any): ThunkAction<Promise<void>, PoolState, un
         )
 
         data.senior!.address = tinlake.contractAddresses['JUNIOR_TOKEN']
-        data.senior!.inMemberlist = seniorInMemberlist
         data.junior!.address = tinlake.contractAddresses['SENIOR_TOKEN']
-        data.junior!.inMemberlist = juniorInMemberlist
 
         const juniorRedemptionsCurrency = (data.junior?.pendingRedemptions || new BN(0))
           .mul(data.junior?.tokenPrice || new BN(0))
@@ -318,8 +325,6 @@ export function loadPool(tinlake: any): ThunkAction<Promise<void>, PoolState, un
         // }
 
         dispatch({ data: data, type: RECEIVE_POOL })
-        const t1 = performance.now()
-        console.log(`Time: ${t1 - t0}seconds`)
       })
 
       watcher.start()
