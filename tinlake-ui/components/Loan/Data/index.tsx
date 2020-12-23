cb847cf5e5f814fed8e7d74cfd396a2c6211701b
Jeroen Offerijns <jeroen@offerijns.com>
2020-12-23 09:34:21 +0100

    fix: add amount col to loan list

--- tinlake-ui/components/Loan/Data/index.tsx
+++ tinlake-ui/components/Loan/Data/index.tsx
@@ -13,7 +13,11 @@ interface Props {
 }
 
 const dateToYMD = (unix: number) => {
-  return new Date(unix * 1000).toLocaleDateString('en-US')
+  return new Date(unix * 1000).toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  })
 }
 
 class LoanData extends React.Component<Props> {
@@ -21,87 +25,60 @@ class LoanData extends React.Component<Props> {
     const { loanId, debt, principal, interestRate, status } = this.props.loan
     return (
       <Box direction="row" justify="between">
-        <Box width="420px" pad="medium" elevation="small" round="xsmall" background="white">
-          <Box>
-            <Box direction="row" margin={{ top: '0', bottom: 'small' }}>
-              <Heading level="5" margin={'0'}>
-                Asset {loanId}
-              </Heading>
-            </Box>
+        <Box>
+          <Box width="420px" pad="medium" elevation="small" round="xsmall" background="white">
+            <Box>
+              <Box direction="row" margin={{ top: '0', bottom: 'small' }}>
+                <Heading level="5" margin={'0'}>
+                  Asset {loanId}
+                </Heading>
+              </Box>
 
-            <Table margin={{ bottom: 'medium' }}>
-              <TableBody>
-                <TableRow>
-                  <TableCell scope="row">Status</TableCell>
-                  <TableCell style={{ textAlign: 'end' }}>{status}</TableCell>
-                </TableRow>
-                {this.props.loan.nft && (this.props.loan.nft as any).maturityDate && (
+              <Table margin={{ bottom: 'medium' }}>
+                <TableBody>
                   <TableRow>
-                    <TableCell scope="row">Maturity date</TableCell>
+                    <TableCell scope="row">Status</TableCell>
+                    <TableCell style={{ textAlign: 'end' }}>{status}</TableCell>
+                  </TableRow>
+                  {this.props.loan.nft && (this.props.loan.nft as any).maturityDate && (
+                    <TableRow>
+                      <TableCell scope="row">Maturity date</TableCell>
+                      <TableCell style={{ textAlign: 'end' }}>
+                        {dateToYMD((this.props.loan.nft as any).maturityDate)}
+                      </TableCell>
+                    </TableRow>
+                  )}
+                </TableBody>
+              </Table>
+              <Table margin={{ bottom: 'small' }}>
+                <TableBody>
+                  <TableRow>
+                    <TableCell scope="row">Available for Financing</TableCell>
                     <TableCell style={{ textAlign: 'end' }}>
-                      {dateToYMD((this.props.loan.nft as any).maturityDate)}
+                      {addThousandsSeparators(toPrecision(baseToDisplay(principal, 18), 2))} DAI
                     </TableCell>
                   </TableRow>
-                )}
-              </TableBody>
-            </Table>
-            <Table margin={{ bottom: 'small' }}>
-              <TableBody>
-                <TableRow>
-                  <TableCell scope="row">Available for Financing</TableCell>
-                  <TableCell style={{ textAlign: 'end' }}>
-                    {addThousandsSeparators(toPrecision(baseToDisplay(principal, 18), 2))} DAI
-                  </TableCell>
-                </TableRow>
-                <TableRow>
-                  <TableCell scope="row" border={{ color: 'transparent' }}>
-                    Outstanding
-                  </TableCell>
-                  <TableCell style={{ textAlign: 'end' }} border={{ color: 'transparent' }}>
-                    {addThousandsSeparators(toPrecision(baseToDisplay(debt, 18), 2))} DAI
-                  </TableCell>
-                </TableRow>
-              </TableBody>
-            </Table>
+                  <TableRow>
+                    <TableCell scope="row" border={{ color: 'transparent' }}>
+                      Outstanding
+                    </TableCell>
+                    <TableCell style={{ textAlign: 'end' }} border={{ color: 'transparent' }}>
+                      {addThousandsSeparators(toPrecision(baseToDisplay(debt, 18), 2))} DAI
+                    </TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
 
-            <Box gap="small" justify="end" direction="row" margin={{ top: 'small' }}>
-              <Button primary label="Finance Asset" />
-              <Button primary label="Repay" disabled />
-            </Box>
-          </Box>
-          {/* <Box direction="row" gap="medium">
-            <FormField label="Asset ID">
-              <TextInput value={loanId} disabled />
-            </FormField>
-            <FormField label="Status">
-              <TextInput value={status} disabled />
-            </FormField>
-          </Box>
-
-          <Box direction="row" gap="medium" margin={{ bottom: 'medium', top: 'large' }}>
-            <Box basis={'1/3'} gap="medium">
-              <FormField label="Available for Financing">
-                <NumberInput value={baseToDisplay(principal, 18)} suffix=" DAI" disabled precision={18} />
-              </FormField>
-            </Box>
-            <Box basis={'1/3'} gap="medium">
-              <FormField label="Outstanding">
-                <NumberInput value={baseToDisplay(debt, 18)} suffix=" DAI" precision={18} disabled />
-              </FormField>
-            </Box>
-            {this.props.loan.nft && (this.props.loan.nft as any).maturityDate && (
-              <Box basis={'1/3'} gap="medium">
-                <FormField label="Maturity Date">
-                  <TextInput value={dateToYMD((this.props.loan.nft as any).maturityDate)} disabled />
-                </FormField>
+              <Box gap="small" justify="end" direction="row" margin={{ top: 'small' }}>
+                <Button primary label="Finance Asset" />
+                <Button primary label="Repay" disabled />
               </Box>
-            )} */}
-          {/* </Box> */}
+            </Box>
+          </Box>
         </Box>
         <Box basis="1/3">
-          {console.log(this.props.loan)}
           {this.props.loan && (this.props.loan as any).riskGroup !== undefined && (this.props.loan as any).scoreCard && (
-            <Box>
+            <Box margin={{ bottom: 'medium' }}>
               <Box direction="row" margin={{ top: '0', bottom: 'small' }}>
                 <Heading level="5" margin={'0'}>
                   Score Card
--- tinlake-ui/ducks/loans.ts
+++ tinlake-ui/ducks/loans.ts
@@ -7,6 +7,7 @@ import { getLoan } from '../services/tinlake/actions'
 
 // SortableLoan adds properties of number type that support sorting in numerical order for grommet DataTable
 export interface SortableLoan extends Loan {
+  amountNum: number
   debtNum: number
   principalNum: number
   interestRateNum: number
@@ -66,6 +67,7 @@ export function loadLoans(tinlake: any): ThunkAction<Promise<void>, LoansState, 
     if (result.data) {
       const loans: SortableLoan[] = result.data.map((l: Loan) => ({
         ...l,
+        amountNum: l.debt.isZero() ? parseFloat(l.principal.toString()) : parseFloat(l.debt.toString()),
         debtNum: parseFloat(l.debt.toString()),
         principalNum: parseFloat(l.principal.toString()),
         interestRateNum: parseFloat(l.interestRate.toString()),
