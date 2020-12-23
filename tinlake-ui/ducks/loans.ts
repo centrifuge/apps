d17aeadd09b6cbc1b31d0138e7ac5c0d5dc11e9f
Jeroen Offerijns <jeroen@offerijns.com>
2020-12-23 11:59:51 +0100

    feat: finish loading basic loan data using multicall

--- tinlake-ui/ducks/loans.ts
+++ tinlake-ui/ducks/loans.ts
@@ -3,7 +3,7 @@ import { HYDRATE } from 'next-redux-wrapper'
 import { Action, AnyAction } from 'redux'
 import { ThunkAction } from 'redux-thunk'
 import Apollo from '../services/apollo'
-import { getLoan } from '../services/tinlake/actions'
+import { getNFT, addProxyDetails } from '../services/tinlake/actions'
 import { createWatcher } from '@makerdao/multicall'
 import config from '../config'
 import { BigNumber } from 'ethers'
@@ -102,7 +102,7 @@ export function loadLoan(
   loanId: string,
   refresh = false
 ): ThunkAction<Promise<void>, LoansState, undefined, Action> {
-  return async (dispatch) => {
+  return async (dispatch, getState) => {
     if (!refresh) {
       dispatch({ type: LOAD_LOAN })
     }
@@ -117,8 +117,8 @@ export function loadLoan(
       return
     }
 
-    // TODO: also get using multicall
-    const riskGroup = await tinlake.getRiskGroup(loanId)
+    // TODO: load riskGroup using multicall
+    const riskGroup = (await tinlake.getRiskGroup(loanId)).toNumber()
 
     const toBN = (val: BigNumber) => new BN(val.toString())
 
@@ -148,7 +148,7 @@ export function loadLoan(
       },
       {
         target: tinlake.contractAddresses.PILE,
-        call: ['rates(uint256)(uint256,uint256,uint256,uint48,uint256)', riskGroup.toNumber()],
+        call: ['rates(uint256)(uint256,uint256,uint256,uint48,uint256)', riskGroup],
         returns: [[`rates.pie`], [`rates.chi`], [`rates.interestRate`], [`rates.lastUpdated`], [`rates.fixedRate`]],
       },
       {
@@ -156,21 +156,77 @@ export function loadLoan(
         call: ['debt(uint256)(uint256)', loanId],
         returns: [[`debt`, toBN]],
       },
+      {
+        target: tinlake.contractAddresses.FEED,
+        call: ['thresholdRatio(uint256)(uint256)', riskGroup],
+        returns: [[`scoreCard.thresholdRatio`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.FEED,
+        call: ['ceilingRatio(uint256)(uint256)', riskGroup],
+        returns: [[`scoreCard.ceilingRatio`, toBN]],
+      },
+      {
+        target: tinlake.contractAddresses.FEED,
+        call: ['recoveryRatePD(uint256)(uint256)', riskGroup],
+        returns: [[`scoreCard.recoveryRatePD`, toBN]],
+      },
     ]
 
     watcher.recreate(globalWatchers, multicallConfig)
 
     try {
-      console.log('subscribing')
-      watcher.batch().subscribe((updates: any[]) => {
-        console.log(updates)
+      const initial = { loanId, riskGroup, scoreCard: {} }
+      const prev =
+        (getState() as any).loans.loan?.loanId === loanId ? (getState() as any).loans.loan || initial : initial
+
+      watcher.batch().subscribe(async (updates: any[]) => {
+        const data: Partial<Loan> = updates.reduce((prev: any, update: any) => {
+          const prefix = update.type.split('.')[0]
+          if (prefix === 'scoreCard') {
+            const item = update.type.split('.')[1]
+            prev[prefix][item] = update.value
+          } else if (prefix === 'rates') {
+            const item = update.type.split('.')[1]
+
+            if (item === 'interestRate') {
+              prev['interestRate'] = update.value
+              prev['scoreCard']['interestRate'] = update.value
+            }
+          } else {
+            prev[update.type] = update.value
+          }
+
+          return prev
+        }, prev)
+
+        // TODO: load getOwnerOfCollateral using multicall
+        if (
+          (await tinlake.getOwnerOfCollateral(data.registry, data.tokenId)).toString() ===
+          tinlake.contractAddresses.SHELF
+        ) {
+          data.status = 'ongoing'
+        } else if ((data.ownerOf as any) === ZERO_ADDRESS) {
+          data.status = 'closed'
+        } else {
+          data.status = 'NFT locked'
+        }
+
+        // TODO: load proxyOwner using multicall
+        await addProxyDetails(tinlake, data as Loan)
+
+        dispatch({ loan: data, type: RECEIVE_LOAN })
+
+        if (!data.nft) {
+          // TODO: load nft using multicall
+          const nftData = await getNFT((data as any).registry, tinlake, `${data.tokenId}`)
+          data.nft = (nftData && (nftData as any).nft) || {}
+          dispatch({ loan: data, type: RECEIVE_LOAN })
+        }
       })
       watcher.start()
     } catch (e) {
       console.error(e)
     }
-    const loan = await getLoan(tinlake, loanId)
-
-    dispatch({ loan, type: RECEIVE_LOAN })
   }
 }
