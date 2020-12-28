9e9e716f428da84fd47b07c36dc93c95e6b562f4
Jeroen Offerijns <jeroen@offerijns.com>
2020-12-28 22:42:34 +0100

    feat: show pool value on hover

--- tinlake-ui/components/PoolsMetrics/index.tsx
+++ tinlake-ui/components/PoolsMetrics/index.tsx
@@ -26,6 +26,10 @@ interface Props extends WithRouterProps {
 const PoolsMetrics: React.FC<Props> = (props: Props) => {
   const router = useRouter()
   const poolsDailyData = useSelector<any, PoolsDailyData[]>((state) => state.pools.poolsDailyData)
+
+  const [hoveredPoolValue, setHoveredPoolValue] = React.useState<number | undefined>(undefined)
+  const [hoveredDay, setHoveredDay] = React.useState<number | undefined>(undefined)
+
   return (
     <>
       {router.query.showAll && (
@@ -52,15 +56,18 @@ const PoolsMetrics: React.FC<Props> = (props: Props) => {
         direction="row"
         pad="medium"
       >
-        <Box pad={{ top: 'small' }}>
+        <Box pad={{ top: 'small' }} width="200px">
           <Cont>
             <TokenLogo src={`/static/dai.svg`} />
             <Value>
-              <NumberDisplay value={baseToDisplay(props.pools.totalValue, 18)} precision={0} />
+              <NumberDisplay
+                value={hoveredPoolValue?.toString() || baseToDisplay(props.pools.totalValue, 18)}
+                precision={0}
+              />
             </Value>{' '}
             <Unit>DAI</Unit>
           </Cont>
-          <Label>Current Value Locked</Label>
+          <Label>{hoveredDay ? `Value Locked on ${dateToYMD(hoveredDay)}` : 'Current Value Locked'}</Label>
         </Box>
         <Box
           width="200px"
@@ -70,14 +77,27 @@ const PoolsMetrics: React.FC<Props> = (props: Props) => {
           style={{ borderLeft: '1px solid #D8D8D8' }}
         >
           <ResponsiveContainer>
-            <AreaChart data={poolsDailyData} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
+            <AreaChart
+              data={poolsDailyData}
+              margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+              onMouseMove={(val: any) => {
+                if (val.activePayload) {
+                  setHoveredPoolValue(val.activePayload[0].payload.poolValue)
+                  setHoveredDay(val.activePayload[0].payload.day)
+                }
+              }}
+              onMouseLeave={() => {
+                setHoveredPoolValue(undefined)
+                setHoveredDay(undefined)
+              }}
+            >
               <defs>
                 <linearGradient id="colorPoolValue" x1="0" y1="0" x2="0" y2="1">
                   <stop offset="5%" stopColor="#0828BE" stopOpacity={0.2} />
                   <stop offset="95%" stopColor="#0828BE" stopOpacity={0} />
                 </linearGradient>
               </defs>
-              <Tooltip content={<CustomTooltip />} offset={20} />
+              <Tooltip content={<></>} />
               {/* <XAxis dataKey="day" mirror tickFormatter={(val: number) => dateToYMDShort(val)} /> */}
               <Area
                 type="monotone"
