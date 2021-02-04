b44d9e3aa5ec2e146cf4f65902403dc174460bf5
ana0 <sarah.anne.friend@gmail.com>
2021-02-04 19:51:02 +0100

    Deploy additional contracts and add documentation

--- e2e-tests/contracts/deploy.sh
+++ e2e-tests/contracts/deploy.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 set -e
 
-make test-config
+mv tinlake-proxy ProxyRegistry
+mv tinlake-pools-cli PoolRegistry
+mv tinlake-claim-rad TinlakeClaimRAD
 
+export DAPP_SOLC_VERSION=0.5.15
+
+# deploy dependencies not included in tinlake-deploy 
+for d in ProxyRegistry PoolRegistry TinlakeClaimRAD; do
+    echo "Deploying $d ..."
+    cd $d
+    git submodule update --init --recursive
+    dapp build
+    ADDRESS=$(dapp create "$d")
+    echo "$d : $ADDRESS"
+    cd ..
+done
+
+cd /app/tinlake-deploy
+
+# deploy contents of tinlake-deploy using test scripts
+git submodule update --init --recursive
+make build
+make test-config
 make deploy
