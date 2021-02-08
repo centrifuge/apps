eaa2a035cfc2f4f111bfaa9c1b6963da05a105a8
Jeroen Offerijns <jeroen@offerijns.com>
2021-02-08 16:19:11 +0100

    feat: add docker push for gateway

--- gateway/scripts/docker_push.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-GIT_SHORT_COMMIT=`git rev-parse --short HEAD`
-BRANCH_ABBR=`git rev-parse --abbrev-ref HEAD`
-TIMESTAMP=`date -u +%Y%m%d%H%M%S`
-TAG="${TRAVIS_BRANCH}-${TIMESTAMP}-${GIT_SHORT_COMMIT}"
-echo "$TAG"
