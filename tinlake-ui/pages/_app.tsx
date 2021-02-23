cbc52d05e00252717108515e0b95eab53d0a6545
Jeroen Offerijns <jeroen@offerijns.com>
2021-02-23 13:04:03 +0100

    feat: add matomo

--- tinlake-ui/pages/_app.tsx
+++ tinlake-ui/pages/_app.tsx
@@ -35,6 +35,21 @@ class MyApp extends App {
         <Head>
           <meta name="viewport" content="width=900, initial-scale=1" />
           <title>Tinlake | Centrifuge | Decentralized Asset Financing</title>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `var _paq = window._paq = window._paq || [];
+              /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+              _paq.push(['trackPageView']);
+              _paq.push(['enableLinkTracking']);
+              (function() {
+                var u="https://centrifuge.matomo.cloud/";
+                _paq.push(['setTrackerUrl', u+'matomo.php']);
+                _paq.push(['setSiteId', '2']);
+                var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+                g.type='text/javascript'; g.async=true; g.src='//cdn.matomo.cloud/centrifuge.matomo.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
+              })();`,
+            }}
+          />
         </Head>
         {config.enableErrorLogging && (
           <Sentry.ErrorBoundary fallback={'An error has occured'}>
