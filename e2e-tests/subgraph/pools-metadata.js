b828c12f2dc933e9ada310d9d73e5a42e86b3773
ana0 <sarah.anne.friend@gmail.com>
2021-02-27 16:36:23 +0100

    Add switch for local env in config.ts, most recent contract addresses, and first local pools metadata

---
+++ e2e-tests/subgraph/pools-metadata.js
@@ -0,0 +1,54 @@
+module.exports = [
+    {
+        "network":"development",
+        "version":3,
+        "addresses":
+        {
+            "ROOT_CONTRACT": "0x20A3a4916ae6B285c7315e1913DfB3F14Be73FF0",
+            "TINLAKE_CURRENCY":"0x2367073dbba5b000e34207d56a4Bc503e13bb54b",
+            "TITLE":"0x86878C4049adAF493AB3B937D7084FDe53191625",
+            "PILE":"0x8BB0C7262268A3CeA5ac3906984728A78230a212",
+            "SHELF":"0xa6df77B265169511E8394F6Bd91adAE9124475e5",
+            "COLLECTOR":"0x8602AFf152BBD8916cA3beBa1a5942373E60D1b4",
+            "FEED":"0xe8068263f6822606eb4ddA56E7cCF2d6D9392e65",
+            "JUNIOR_OPERATOR":"0xD52A2C0450cAD853df61D796715a806a75D0CdA3",
+            "SENIOR_OPERATOR":"0x687AafAFdD52896416db0B9D8dD1a1BF07b8E576",
+            "JUNIOR_TRANCHE":"0x493ed02bd56386636A205fF81f1Ea2527B266A97",
+            "SENIOR_TRANCHE":"0x437b566329697301B384A90247b5924581C07a3E",
+            "JUNIOR_TOKEN":"0xA4fA9310aeDb15ab939C41C265887711fD90E7eF",
+            "SENIOR_TOKEN":"0x0F719aFFE49F8690305099099Ad5DF74A743f3E3",
+            "JUNIOR_MEMBERLIST":"0xF6623A62c46afE99ca6D6056BDCe1eDD7c3dEEd6",
+            "SENIOR_MEMBERLIST":"0x893C2F430121514109Afad3dAfC5E25dA4cC388f",
+            "ASSESSOR":"0x7553805f779857886Afa3FE64fd97F1BcAe832B7",
+            "ASSESSOR_ADMIN":"0x27AB44aAF0eb30786F33f4E52f328266CD39BA7b",
+            "COORDINATOR":"0x5B7081ae0cb647FD6E595E46f530aBc7Ba49A5BB",
+            "RESERVE":"0x6711200f61B097FAF354d774dac45366C22818C3",
+            "ACTIONS":"0x4fc1d999FD7F098F1FC96687bAD43a847118B6FB",
+            "PROXY_REGISTRY":"0x45de42e18F4688A82Fe01c76d305c14055F01EB9",
+            "CLAIM_RAD":"0x17165426fd77728079624e4527a37975341F6703"
+        },
+        "metadata":
+        {
+            "name":"Kovan Revolving Pool 2",
+            "slug":"kovan-revolving-pool-2",
+            "description":"This section displays information provided by the Asset Originator, such as a description of the business model.",
+            "media":
+            {
+                "logo":"https://storage.googleapis.com/tinlake/tinlake.png"
+            },
+            "website":"https://tinlake.centrifuge.io/",
+            "email":"tinlake@centrifuge.io",
+            "details":
+            {
+                "Asset Originator":"Kovan Revolving Pool 2 LLC",
+                "Collateral":"Simplified invoices",
+                "Average Invoice Size":"$1,000",
+                "Maturity Date":"30 day invoices",
+                "Issuer":"Kovan Revolving Pool 2 LLC"
+            },
+            "discourseLink":"https://discourse.centrifuge.io/",
+            "asset":"Invoice",
+            "securitizeId":"kickfurther"
+        }
+    }
+]
