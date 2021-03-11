f460f0053cbbce628d6551fe1484156ba6440e68
philipstanislaus <6912756+philipstanislaus@users.noreply.github.com>
2021-03-11 11:08:34 +0100

    Update reward investment period (#156)
    * fix(tinlake-ui): show max reserve component for admins (#61) (#62)
    
    Co-authored-by: Jeroen Offerijns <Offerijns@users.noreply.github.com>
    
    * Fix yield data fetching (#82) (#83)
    
    * Release (#88)
    
    * Fix yield data fetching (#82)
    
    * Onboarding data model and UI improvements (#81)
    
    * fix: add cta for non connected investors
    
    * feat: add duck for onboarding state
    
    * feat: work on new onboarding card
    
    * feat: implement new status descriptions for onboarding card
    
    * feat: add checkboxes to onboarding modal
    
    * fix: user update
    
    * fix: store and return country code
    
    * fix: session verification, agreement creation, and ui redirecti ssues
    
    * fix: redirect to agreement through the api
    
    * fix: hide asset filters
    
    * fix: build
    
    * chore: remove logs
    
    * chore: remove more logs
    
    * chore: remove unused code
    
    * chore: format
    
    * refactor: create config object
    
    * fix: duplicate type
    
    * docs: add comment
    
    * chore: remove comment
    
    * çhore: format
    
    * Enable partial repay globally (#87)
    
    Co-authored-by: Jeroen Offerijns <Offerijns@users.noreply.github.com>
    
    * feat: change reward min investment period from 60 to 30 days
    
    Co-authored-by: Jeroen Offerijns <Offerijns@users.noreply.github.com>
    Co-authored-by: Jeroen Offerijns <jeroen@offerijns.com>

--- tinlake-ui/ducks/userRewards.ts
+++ tinlake-ui/ducks/userRewards.ts
@@ -33,7 +33,7 @@ export interface UserRewardsState {
 /**
  * Process to earn and claim rewards:
  * 1. User earns rewards on Ethereum for any investments on that Ethereum account `totalEarnedRewards`
- * 2. After holding a non zero investements for 60 days, those rewards become claimable
+ * 2. After holding a non zero investements for 30 (formerly 60) days, those rewards become claimable
  * 3. To claim rewards, user needs to link a Cent Chain account to the Ethereum account. If there is none, any rewards
  *    are in `unlinkedRewards`. If there is a link, rewards will be fully assigned to the (last) linked Cent Chain
  *    account.
