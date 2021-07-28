fe6cdcddebbcb66747af28c0c7aa1747014b34c3
Jeroen Offerijns <jeroen@offerijns.com>
2021-07-28 13:52:55 +0200

    chore: hide mgmt page

--- tinlake-ui/menuItems.ts
+++ tinlake-ui/menuItems.ts
@@ -4,7 +4,7 @@ export const menuItems: MenuItem[] = [
   { label: 'Overview', route: '/', inPool: true, env: '' },
   { label: 'Investments', route: '/investments', inPool: true, env: '' },
   { label: 'Assets', route: '/assets', inPool: true, env: '' },
-  { label: 'Management', route: '/management', inPool: true, env: 'admin' },
+  // { label: 'Management', route: '/management', inPool: true, env: 'admin' },
   { label: 'Mint NFT', route: '/demo/mint-nft', inPool: true, env: 'demo' },
   { label: 'Value NFT', route: '/demo/value-nft', inPool: true, env: 'demo' },
 ]
