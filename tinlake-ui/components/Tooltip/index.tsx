c98b931dd7c06959ffa03a57d30777a0f398ba6d
Onno Visser <visser.onno@gmail.com>
2021-08-06 15:23:53 +0200

    fix black line on tooltip buttons

--- tinlake-ui/components/Tooltip/index.tsx
+++ tinlake-ui/components/Tooltip/index.tsx
@@ -26,18 +26,33 @@ interface Tooltip {
 
 type Props = PropsWithID | PropsWithoutID
 
-const Wrapper = styled.span<{ underline?: boolean }>`
+const Wrapper = styled.span<{ underline?: boolean; fix?: boolean }>`
   [data-tip] > *,
   [data-tip] {
     border-bottom: ${(props) => (props.underline ? '1px dotted #bbbbbb' : undefined)};
   }
+  /* react-tooltip doesn't play nice with SSR, so this dirty fix prevents a black line from showing on tooltip buttons */
+  .__react_component_tooltip {
+    ${(props) => props.fix && `display: none;`}
+  }
 `
+
+let serverHandoffComplete = false
 
 export const Tooltip: React.FC<Props> = (props: Props) => {
   const { title, link, description } = 'id' in props ? (tooltips[props.id] as Tooltip) : props
+  const [fix, setFix] = React.useState(serverHandoffComplete ? false : true)
+
+  React.useEffect(() => {
+    serverHandoffComplete = true
+
+    if (fix) {
+      setFix(false)
+    }
+  }, [])
 
   return (
-    <Wrapper underline={props.underline}>
+    <Wrapper underline={props.underline} fix={fix}>
       <AxisTooltip
         title={title}
         description={description}
