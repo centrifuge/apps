61fccbbf2af9bbed8318bcc982fa9f52d33623c5
Onno Visser <visser.onno@gmail.com>
2021-08-19 10:41:49 +0200

    add react-error-boundary

--- tinlake-ui/utils/useOnboardingState.ts
+++ tinlake-ui/utils/useOnboardingState.ts
@@ -3,8 +3,9 @@ import { useQuery } from 'react-query'
 import { useSelector } from 'react-redux'
 import config, { Pool, UpcomingPool } from '../config'
 
-export function useOnboardingState(pool: Pool | UpcomingPool) {
-  const address = useSelector<any, string | null>((state) => state.auth.address)
+export function useOnboardingState(pool: Pool | UpcomingPool, addressOverride?: string | null) {
+  const addressState = useSelector<any, string | null>((state) => state.auth.address)
+  const address = addressOverride || addressState
   const poolId = (pool as Pool).addresses?.ROOT_CONTRACT
   const query = useQuery<AddressStatus>(
     ['onboarding', poolId, address],
