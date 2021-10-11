98cc5f57b0fcca6b220eeefdc63423ccc6aa56ae
Onno Visser <visser.onno@gmail.com>
2021-10-11 10:44:35 +0200

    remove margin tokens

--- fabric/src/theme/space.ts
+++ fabric/src/theme/space.ts
@@ -3,8 +3,5 @@ const space = [0, 8, 16, 24, 32, 40, 48, 56, 64, 72, 80] as number[] & { [key: s
 space.gutterMobile = space[2]
 space.gutterTablet = space[2]
 space.gutterDesktop = space[3]
-space.pageMarginMobile = space[2]
-space.pageMarginTablet = space[3]
-space.pageMarginDesktop = space[3]
 
 export default space
