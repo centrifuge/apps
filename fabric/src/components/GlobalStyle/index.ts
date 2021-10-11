3a9a936a16059ade8042de0f55f4ef8c466a7731
Onno Visser <visser.onno@gmail.com>
2021-10-11 09:58:01 +0200

    add GlobalStyle

---
+++ fabric/src/components/GlobalStyle/index.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+`
