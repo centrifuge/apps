dc7cf0388a848b05db3be53c77f43ec8c7ed46ae
Onno Visser <visser.onno@gmail.com>
2021-10-18 14:39:49 +0200

    Tinlake, Gateway: Update React to v17 (#468)

--- gateway/packages/ui/src/setupTests.js
+++ gateway/packages/ui/src/setupTests.js
@@ -1,7 +1,5 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import { configure } from 'enzyme'
 
-configure({ adapter: new Adapter() });
-window.scrollTo = () => {
-
-}
+configure({ adapter: new Adapter() })
+window.scrollTo = () => {}
--- nft-studio/src/pages/Collections.tsx
+++ nft-studio/src/pages/Collections.tsx
@@ -1,5 +1,12 @@
+import { Card, Container, Text } from '@centrifuge/fabric'
 import * as React from 'react'
 
 export const CollectionsPage: React.FC = () => {
-  return <div>Collections</div>
+  return (
+    <Container>
+      <Card p={3}>
+        <Text variant="heading1">Collections</Text>
+      </Card>
+    </Container>
+  )
 }
