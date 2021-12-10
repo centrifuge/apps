9c67940e6349b6624b88059d86bcb6539bd42e79
Onno Visser <visser.onno@gmail.com>
2021-12-10 12:14:46 +0100

    replace tsdx with esbuild

--- centrifuge-js/src/utils/web3.ts
+++ centrifuge-js/src/utils/web3.ts
@@ -1,7 +1,8 @@
 import { ApiPromise, WsProvider } from '@polkadot/api'
+import { RegistryTypes } from '@polkadot/types/types'
 
 const cached: { [key: string]: Promise<ApiPromise> } = {}
-export function getPolkadotApi(wsUrl: string, types: any) {
+export function getPolkadotApi(wsUrl: string, types?: RegistryTypes) {
   return (
     cached[wsUrl] ||
     (cached[wsUrl] = ApiPromise.create({
