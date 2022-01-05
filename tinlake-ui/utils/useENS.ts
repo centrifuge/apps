d32e86f68112469624d5030691d6cc856aa1da9d
Yoginth <yoginth.saravanan@cred.club>
2022-01-05 17:00:54 +0530

    chore: add ens avatar support in hook

--- tinlake-ui/utils/useENS.ts
+++ tinlake-ui/utils/useENS.ts
@@ -6,13 +6,16 @@ type ReturnType = { ensName: string | null }
 
 export const useENS = (address: string | null | undefined): ReturnType => {
   const [ensName, setENSName] = useState<string | null>(null)
+  const [ensAvatar, setENSAvatar] = useState<string | null>(null);
 
   useEffect(() => {
     async function resolveENS() {
       if (address && ethers.utils.isAddress(address)) {
         const provider = new ethers.providers.JsonRpcProvider(config.rpcUrl)
         const ensName = await provider.lookupAddress(address)
+        const ensAvatar = ensName ? await provider.getAvatar(ensName) : null;
         if (ensName) setENSName(ensName)
+        if (ensAvatar) setENSAvatar(ensAvatar);
       }
     }
     resolveENS()
