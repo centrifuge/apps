d44cbabb3003f91f27101fc835b8d91baae28ad8
Alessio Scalici <alessio@centrifuge.io>
2022-01-13 13:57:45 +0100

    feat(nft-studio): add missing elements to create pool form

--- nft-studio/src/pages/PoolForm/index.tsx
+++ nft-studio/src/pages/PoolForm/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Select, Shelf, Stack, Text } from '@centrifuge/fabric'
+import { AnchorButton, Box, Button, Grid, IconPlus, Select, Shelf, Stack, Text } from '@centrifuge/fabric'
 import * as React from 'react'
 import { FileImageUpload } from '../../components/FileImageUpload'
 import { RadioInput } from '../../components/RadioInput'
@@ -26,9 +26,30 @@ const CreatePoolForm: React.FC = () => {
   const [discountRate, setDiscountRate] = React.useState<string>('')
   const [minEpochDuration, setMinEpochDuration] = React.useState<string>('')
   const [challengeTime, setChallengeTime] = React.useState<string>('')
+  const [tranche, setTranche] = React.useState<string>('')
+  const [tokenName, setTokenName] = React.useState<string>('')
+  const [interestRate, setInterestRate] = React.useState<string>('')
+  const [minRiskBuffer, setMinRiskBuffer] = React.useState<string>('')
+
+  // TODO: call centrifuge-js and create pool
+  const onSubmit = () => {
+    console.log('submit pool:', {
+      currency,
+      minEpochDuration,
+      challengeTime,
+      poolName,
+      assetClass,
+      discountRate,
+      tranche,
+      tokenName,
+      interestRate,
+      minRiskBuffer,
+    })
+  }
+
   return (
-    <Stack gap="3">
-      <Shelf justifyContent="space-between">
+    <Grid columns={[10]} equalColumns gap={['gutterMobile', 'gutterTablet', 'gutterDesktop']}>
+      <Stack gap="3" gridColumn="1 / 5">
         <TextInput
           label="Pool name"
           placeholder="Untitled pool"
@@ -38,74 +59,133 @@ const CreatePoolForm: React.FC = () => {
           }}
         />
 
+        <Stack gap="1">
+          <Text variant="label1">Asset class</Text>
+          <Shelf gap="4">
+            {ASSET_CLASS.map(({ label, id }) => (
+              <RadioInput
+                key={id}
+                label={label}
+                checked={assetClass === id}
+                name="assetClass"
+                onChange={(ev) => {
+                  setAssetClass(id)
+                }}
+              />
+            ))}
+          </Shelf>
+        </Stack>
+
+        <Stack gap="1">
+          <Text variant="label1">Pool icon</Text>
+          <FileImageUpload onFileUpdate={() => {}} />
+        </Stack>
+
+        <Stack gap="1">
+          <Text variant="label1">Issuer logo</Text>
+          <FileImageUpload onFileUpdate={() => {}} />
+        </Stack>
+
+        <Select
+          label="Currency"
+          placeholder="Select..."
+          options={[{ value: 'dai', label: 'DAI' }]}
+          onSelect={(key) => {
+            if (key) {
+              setCurrency(key)
+            }
+          }}
+        />
+
+        <TextInput
+          label="Discount rate"
+          placeholder="0.00%"
+          value={discountRate}
+          onChange={(ev) => {
+            setDiscountRate(ev.target.value)
+          }}
+        />
+
+        <Select
+          label="Minimum epoch duration"
+          placeholder="Select..."
+          options={[{ value: '24h', label: '24 hours' }]}
+          onSelect={(key) => {
+            if (key) {
+              setMinEpochDuration(key)
+            }
+          }}
+        />
+
+        <Select
+          label="Challenge time"
+          placeholder="Select..."
+          options={[{ value: '30m', label: '30 minutes' }]}
+          onSelect={(key) => {
+            if (key) {
+              setChallengeTime(key)
+            }
+          }}
+        />
+      </Stack>
+      <Stack gap="4" gridColumn="6 / 9" marginTop="9">
+        <Text variant="heading3">Tranches</Text>
+        <Select
+          label="Tranche"
+          placeholder="Select..."
+          options={[
+            { value: 'senior', label: 'Senior' },
+            { value: 'junior', label: 'Junior' },
+          ]}
+          onSelect={(key) => {
+            if (key) {
+              setTranche(key)
+            }
+          }}
+        />
+        <TextInput
+          label="Token name"
+          placeholder="SEN"
+          value={tokenName}
+          onChange={(ev) => {
+            setTokenName(ev.target.value)
+          }}
+        />
+        <TextInput
+          label="Interest rate"
+          placeholder="0.00%"
+          value={interestRate}
+          onChange={(ev) => {
+            setInterestRate(ev.target.value)
+          }}
+        />
+        <TextInput
+          label="Minimum risk buffer"
+          placeholder="0.00%"
+          value={minRiskBuffer}
+          onChange={(ev) => {
+            setMinRiskBuffer(ev.target.value)
+          }}
+        />
+
+        <Box borderBottomWidth="1px" borderBottomStyle="solid" borderBottomColor="borderPrimary" />
+
+        <Box>
+          <Button variant="text" icon={<IconPlus />}>
+            Add another tranche
+          </Button>
+        </Box>
+      </Stack>
+      <Stack gap="3" gridColumn="9 / 11">
         <Shelf gap="2">
-          <Button variant="outlined">Cancel</Button>
-          <Button variant="contained">Create</Button>
-        </Shelf>
-      </Shelf>
-
-      <Stack gap="1">
-        <Text variant="label1">Asset class</Text>
-        <Shelf gap="4">
-          {ASSET_CLASS.map(({ label, id }) => (
-            <RadioInput
-              key={id}
-              label={label}
-              checked={assetClass === id}
-              name="assetClass"
-              onChange={(ev) => {
-                setAssetClass(id)
-              }}
-            />
-          ))}
+          <AnchorButton variant="outlined" href="/managed-pools">
+            Cancel
+          </AnchorButton>
+          <Button variant="contained" onClick={onSubmit}>
+            Create
+          </Button>
         </Shelf>
       </Stack>
-
-      <Stack gap="1">
-        <Text variant="label1">Pool icon</Text>
-        <FileImageUpload onFileUpdate={() => {}} />
-      </Stack>
-
-      <Stack gap="1">
-        <Text variant="label1">Issuer logo</Text>
-        <FileImageUpload onFileUpdate={() => {}} />
-      </Stack>
-
-      <Select
-        label="Currency"
-        placeholder="Select..."
-        options={[{ value: 'dai', label: 'DAI' }]}
-        onSelect={(it) => {
-          console.log('onSelect', it)
-        }}
-      />
-
-      <TextInput
-        label="Discount rate"
-        placeholder="0.00%"
-        value={discountRate}
-        onChange={(ev) => {
-          setDiscountRate(ev.target.value)
-        }}
-      />
-
-      <Select
-        label="Minimum epoch duration"
-        placeholder="Select..."
-        options={[{ value: '24h', label: '24 hours' }]}
-        onSelect={(it) => {
-          console.log('onSelect', it)
-        }}
-      />
-
-      <Select
-        label="Challenge time"
-        placeholder="Select..."
-        options={[{ value: '30m', label: '30 minutes' }]}
-        onSelect={(it) => {
-          console.log('onSelect', it)
-        }}
-      />
-    </Stack>
+    </Grid>
   )
 }
