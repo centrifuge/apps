439b2a48076d1518bb7ff962388d660aabbf5467
Alessio Scalici <alessioscalici@users.noreply.github.com>
2022-03-03 08:51:31 +0100

    Add avg values portfolio distribution (#648)
    * chore(tinlake-ui): add average financing fee and risk to portfolio distribution component
    
    * chore(tinlake-ui): apply style amendments
    
    * chore(tinlake-ui): apply style amendments

--- tinlake-ui/components/Scorecard/index.tsx
+++ tinlake-ui/components/Scorecard/index.tsx
@@ -1,19 +1,21 @@
 import { feeToInterestRate, interestRateToFee, toPrecision } from '@centrifuge/tinlake-js'
 import BN from 'bn.js'
 import { Box as GrommetBox, Button, Table, TableBody, TableCell, TableHeader, TableRow } from 'grommet'
-import { FormDown, Risk } from 'grommet-icons'
+import { FormDown } from 'grommet-icons'
 import * as React from 'react'
 import { connect } from 'react-redux'
 import styled from 'styled-components'
 import { Card } from '../../components/Card'
 import { SectionHeading } from '../../components/Heading'
-import { Box, Wrap } from '../../components/Layout'
+import { Box, Shelf, Stack } from '../../components/Layout'
 import { Tooltip } from '../../components/Tooltip'
 import { Pool } from '../../config'
 import { createTransaction, TransactionProps } from '../../ducks/transactions'
 import { Fixed27Base } from '../../utils/ratios'
 import { SortableLoan, useAssets } from '../../utils/useAssets'
 import { RiskGroup, usePool } from '../../utils/usePool'
+import { Divider } from '../Divider'
+import { Text } from '../Text'
 
 interface Props extends TransactionProps {
   activePool: Pool
@@ -27,6 +29,18 @@ interface RiskGroupWithId extends RiskGroup {
 const riskGroupsPerPage = 8
 
 const e18 = new BN(10).pow(new BN(18))
+
+const HeaderValue: React.FC = ({ children }) => (
+  <Text fontSize="16px" fontWeight="600" lineHeight="28px" textAlign="center">
+    {children}
+  </Text>
+)
+
+const HeaderLabel: React.FC = ({ children }) => (
+  <Text fontSize="14px" fontWeight="500" lineHeight="19.25px" textAlign="center" color="#777">
+    {children}
+  </Text>
+)
 
 const Scorecard: React.FC<Props> = (props: Props) => {
   const [open, setOpen] = React.useState(false)
@@ -62,99 +76,144 @@ const Scorecard: React.FC<Props> = (props: Props) => {
         })
     : []
 
+  const ratePerSecondAvg = React.useMemo(() => {
+    if (!existingRiskGroups || existingRiskGroups.length === 0) return new BN(0)
+    const validRates = existingRiskGroups.map((riskGroup) => riskGroup.rate.ratePerSecond).filter(isValidRatePerSecond)
+    if (validRates.length === 0) return new BN(0)
+    return validRates.reduce((acc, rate) => acc.add(rate), new BN(0)).div(new BN(validRates.length))
+  }, [existingRiskGroups])
+
+  const recoveryRatePDAvg = React.useMemo(() => {
+    if (!existingRiskGroups || existingRiskGroups.length === 0) return 0
+    return (
+      existingRiskGroups
+        .map(
+          (riskGroup) =>
+            parseFloat(
+              Fixed27Base.sub(riskGroup.recoveryRatePD)
+                .div(new BN(10).pow(new BN(22)))
+                .toString()
+            ) / 1000
+        )
+        .reduce((acc, rate) => acc + rate, 0) / existingRiskGroups.length
+    )
+  }, [existingRiskGroups])
+
   const [start, setStart] = React.useState(0)
 
   return (
     <Card interactive>
-      <Wrap p={24} gap="small" style={{ cursor: 'pointer' }} onClick={() => setOpen(!open)}>
-        <Risk />
-
+      <Shelf
+        p={16}
+        gap="small"
+        justifyContent="space-between"
+        style={{ cursor: 'pointer', overflow: 'hidden' }}
+        onClick={() => setOpen(!open)}
+      >
         <SectionHeading>Portfolio distribution</SectionHeading>
 
-        <Caret style={{ marginLeft: 'auto', position: 'relative', top: '0' }}>
-          <FormDown style={{ transform: open ? 'rotate(-180deg)' : '' }} />
-        </Caret>
-      </Wrap>
-      {open && (
-        <Box px={24} pb={24}>
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableCell size="14%">
-                  <Tooltip id="riskGroupID" underline>
-                    Risk group ID
-                  </Tooltip>
-                </TableCell>
-                <TableCell size="22%">Financing fee (APR)</TableCell>
-                <TableCell size="22%">
-                  <Tooltip id="assumedRiskAdjustment" underline>
-                    Assumed risk adjustment
-                  </Tooltip>
-                </TableCell>
-                <TableCell size="22%">Portfolio share</TableCell>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {existingRiskGroups.slice(start, start + riskGroupsPerPage).map((riskGroup: RiskGroupWithId) => (
-                <TableRow>
-                  <TableCell>{riskGroup.id}</TableCell>
-                  <TableCell>
-                    {isValidRatePerSecond(riskGroup.rate.ratePerSecond)
-                      ? `${toPrecision(feeToInterestRate(riskGroup.rate.ratePerSecond), 2)}%`
-                      : 'N/A'}
-                  </TableCell>
-                  <TableCell>
-                    {toPrecision(
-                      parseFloat(
-                        Fixed27Base.sub(riskGroup.recoveryRatePD)
-                          .div(new BN(10).pow(new BN(22)))
-                          .toString()
-                      ) / 1000,
-                      2
-                    )}
-                    %
-                  </TableCell>
-                  <TableCell>
-                    {toPrecision(
-                      parseFloat(
-                        (poolData && !poolData.reserve.add(poolData.netAssetValue).isZero()
-                          ? riskGroup.outstandingDebt
-                              .mul(e18)
-                              .div(totalDebt)
-                              .div(new BN('10').pow(new BN('14')))
-                          : new BN(0)
-                        ).toString()
-                      ) / 100,
-                      2
-                    )}
-                    %
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+        <Shelf gap="48px">
+          <Stack alignItems="center">
+            <HeaderValue>
+              {isValidRatePerSecond(ratePerSecondAvg)
+                ? `${toPrecision(feeToInterestRate(ratePerSecondAvg), 2)}%`
+                : 'N/A'}
+            </HeaderValue>
+            <HeaderLabel>Average financing fee</HeaderLabel>
+          </Stack>
 
-          <GrommetBox direction="row" justify="center" margin={{ top: 'medium' }} gap="medium">
-            <div>
-              <Button
-                size="small"
-                primary
-                label="Previous"
-                onClick={() => setStart(start - riskGroupsPerPage < 0 ? 0 : start - riskGroupsPerPage)}
-                disabled={start === 0}
-              />
-            </div>
-            <div>
-              <Button
-                size="small"
-                primary
-                label="Next"
-                onClick={() => setStart(start + riskGroupsPerPage)}
-                disabled={existingRiskGroups.length < start + riskGroupsPerPage}
-              />
-            </div>
-          </GrommetBox>
-        </Box>
+          <Stack alignItems="center">
+            <HeaderValue>{toPrecision(recoveryRatePDAvg, 2)}%</HeaderValue>
+            <HeaderLabel>Average risk adjustment</HeaderLabel>
+          </Stack>
+          <Caret style={{ marginLeft: 'auto', position: 'relative', top: '0' }}>
+            <FormDown style={{ transform: open ? 'rotate(-180deg)' : '' }} />
+          </Caret>
+        </Shelf>
+      </Shelf>
+      {open && (
+        <>
+          <Divider />
+          <Box p={24}>
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableCell size="14%">
+                    <Tooltip id="riskGroupID" underline>
+                      Risk group ID
+                    </Tooltip>
+                  </TableCell>
+                  <TableCell size="22%">Financing fee (APR)</TableCell>
+                  <TableCell size="22%">
+                    <Tooltip id="assumedRiskAdjustment" underline>
+                      Assumed risk adjustment
+                    </Tooltip>
+                  </TableCell>
+                  <TableCell size="22%">Portfolio share</TableCell>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {existingRiskGroups.slice(start, start + riskGroupsPerPage).map((riskGroup: RiskGroupWithId) => (
+                  <TableRow>
+                    <TableCell>{riskGroup.id}</TableCell>
+                    <TableCell>
+                      {isValidRatePerSecond(riskGroup.rate.ratePerSecond)
+                        ? `${toPrecision(feeToInterestRate(riskGroup.rate.ratePerSecond), 2)}%`
+                        : 'N/A'}
+                    </TableCell>
+                    <TableCell>
+                      {toPrecision(
+                        parseFloat(
+                          Fixed27Base.sub(riskGroup.recoveryRatePD)
+                            .div(new BN(10).pow(new BN(22)))
+                            .toString()
+                        ) / 1000,
+                        2
+                      )}
+                      %
+                    </TableCell>
+                    <TableCell>
+                      {toPrecision(
+                        parseFloat(
+                          (poolData && !poolData.reserve.add(poolData.netAssetValue).isZero()
+                            ? riskGroup.outstandingDebt
+                                .mul(e18)
+                                .div(totalDebt)
+                                .div(new BN('10').pow(new BN('14')))
+                            : new BN(0)
+                          ).toString()
+                        ) / 100,
+                        2
+                      )}
+                      %
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+
+            <GrommetBox direction="row" justify="center" margin={{ top: 'medium' }} gap="medium">
+              <div>
+                <Button
+                  size="small"
+                  primary
+                  label="Previous"
+                  onClick={() => setStart(start - riskGroupsPerPage < 0 ? 0 : start - riskGroupsPerPage)}
+                  disabled={start === 0}
+                />
+              </div>
+              <div>
+                <Button
+                  size="small"
+                  primary
+                  label="Next"
+                  onClick={() => setStart(start + riskGroupsPerPage)}
+                  disabled={existingRiskGroups.length < start + riskGroupsPerPage}
+                />
+              </div>
+            </GrommetBox>
+          </Box>
+        </>
       )}
     </Card>
   )
