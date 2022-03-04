4bf136c3af2eb652ce1148eaff1c5fdf944a49d8
Alessio Scalici <alessioscalici@users.noreply.github.com>
2022-03-04 10:19:54 +0100

    Dark mode altair (#675)
    * chore(nft-studio): switch to altair dark mode
    
    * chore(nft-studio): remove NFT Playground title while in S breakpoint

--- nft-studio/src/components/PageHeader.tsx
+++ nft-studio/src/components/PageHeader.tsx
@@ -3,7 +3,9 @@ import css from '@styled-system/css'
 import * as React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
+import logoUrl from '../assets/images/altair-wordmark-light.svg'
 import { ContextActions } from './ContextActions'
+import { useDebugFlags } from './DebugFlags'
 import { RouterTextLink } from './TextLink'
 
 type Props = {
@@ -41,43 +43,50 @@ export const PageHeader: React.FC<Props> = ({
   actions,
   walletShown = true,
 }) => {
+  const showOnlyNFT = useDebugFlags().showOnlyNFT
   return (
     <Shelf as="header" justifyContent="space-between" alignItems="flex-start" position="sticky" top="24px">
-      <Box
-        position="absolute"
-        top="-24px"
-        bottom={0}
-        right="-12px"
-        left="-12px"
-        backgroundColor="backgroundPrimary"
-        zIndex={-1}
-      />
-      <Stack gap={1}>
-        <Shelf minHeight={20}>
-          {parent && (
-            <Text variant="interactive1">
-              <BackLink to={parent.to}>{parent.label}</BackLink>
+      <Box position="absolute" top="-24px" bottom={0} right="-12px" left="-12px" zIndex={-1} />
+      {!showOnlyNFT && (
+        <Stack gap={1}>
+          <Shelf minHeight={20}>
+            {parent && (
+              <Text variant="interactive1">
+                <BackLink to={parent.to}>{parent.label}</BackLink>
+              </Text>
+            )}
+          </Shelf>
+          <Shelf gap={1}>
+            <Text variant="heading2" as="h1" style={{ wordBreak: 'break-word' }}>
+              {title}
+            </Text>
+            {titleAddition}
+          </Shelf>
+          {subtitle && (
+            <Text variant="heading6" fontWeight={500}>
+              {subtitle}
+              {subtitleLink && (
+                <>
+                  {' '}
+                  • <RouterTextLink to={subtitleLink.to}>{subtitleLink.label}</RouterTextLink>
+                </>
+              )}
             </Text>
           )}
-        </Shelf>
-        <Shelf gap={1}>
-          <Text variant="heading2" as="h1" style={{ wordBreak: 'break-word' }}>
-            {title}
-          </Text>
-          {titleAddition}
-        </Shelf>
-        {subtitle && (
-          <Text variant="heading6" fontWeight={500}>
-            {subtitle}
-            {subtitleLink && (
-              <>
-                {' '}
-                • <RouterTextLink to={subtitleLink.to}>{subtitleLink.label}</RouterTextLink>
-              </>
-            )}
-          </Text>
-        )}
-      </Stack>
+        </Stack>
+      )}
+
+      {showOnlyNFT && (
+        <>
+          <Box height="48px" width="60px" marginTop="-8px">
+            <img src={logoUrl} alt="" />
+          </Box>
+          <Box position="absolute" width="100%" textAlign="center" display={['none', 'block']}>
+            <Text variant="heading2">NFT Playground</Text>
+          </Box>
+        </>
+      )}
+
       <ContextActions actions={actions} walletShown={walletShown} />
     </Shelf>
   )
