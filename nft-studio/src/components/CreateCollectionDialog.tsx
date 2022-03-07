231a609cd20feab7d8ce04ddd22313a574b99ad9
Alessio Scalici <alessioscalici@users.noreply.github.com>
2022-03-07 10:01:44 +0100

    Redirect to collection page after creation (#678)
    * chore(nft-studio): redirect to collection page after creation
    
    * chore(nft-studio): use state redirect

--- nft-studio/src/components/CreateCollectionDialog.tsx
+++ nft-studio/src/components/CreateCollectionDialog.tsx
@@ -1,6 +1,7 @@
 import { Button, Shelf, Stack, Text } from '@centrifuge/fabric'
-import * as React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useQueryClient } from 'react-query'
+import { Redirect } from 'react-router'
 import { ButtonGroup } from '../components/ButtonGroup'
 import { Dialog } from '../components/Dialog'
 import { useWeb3 } from '../components/Web3Provider'
@@ -20,10 +21,11 @@ const CREATE_FEE_ESTIMATE = 2
 export const CreateCollectionDialog: React.FC<{ open: boolean; onClose: () => void }> = ({ open, onClose }) => {
   const queryClient = useQueryClient()
   const { selectedAccount } = useWeb3()
-  const [name, setName] = React.useState('')
-  const [description, setDescription] = React.useState('')
+  const [name, setName] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
   const cent = useCentrifuge()
   const { data: balance } = useBalance()
+  const [redirect, setRedirect] = useState<string>('')
 
   const isConnected = !!selectedAccount?.address
 
@@ -33,9 +35,10 @@ export const CreateCollectionDialog: React.FC<{ open: boolean; onClose: () => vo
     reset: resetLastTransaction,
     isLoading: transactionIsPending,
   } = useCentrifugeTransaction('Create collection', (cent) => cent.nfts.createCollection, {
-    onSuccess: () => {
+    onSuccess: ([collectionId]) => {
       queryClient.invalidateQueries('collections')
       queryClient.invalidateQueries('balance')
+      setRedirect(`/collection/${collectionId}`)
     },
   })
 
@@ -57,7 +60,7 @@ export const CreateCollectionDialog: React.FC<{ open: boolean; onClose: () => vo
   })
 
   // Only close if the modal is still showing the last created collection
-  React.useEffect(() => {
+  useEffect(() => {
     if (lastCreatedTransaction?.status === 'succeeded') {
       close()
     }
@@ -77,8 +80,14 @@ export const CreateCollectionDialog: React.FC<{ open: boolean; onClose: () => vo
   }
 
   const balanceLow = !balance || balance < CREATE_FEE_ESTIMATE
+  const isTxPending = metadataIsUploading || transactionIsPending
 
-  const disabled = !isConnected || !name || !description || balanceLow
+  const fieldDisabled = !isConnected || balanceLow || isTxPending
+  const disabled = !isConnected || !name || !description || balanceLow || isTxPending
+
+  if (redirect) {
+    return <Redirect to={redirect} />
+  }
 
   return (
     <Dialog isOpen={open} onClose={close}>
@@ -92,12 +101,14 @@ export const CreateCollectionDialog: React.FC<{ open: boolean; onClose: () => vo
             value={name}
             maxLength={collectionMetadataSchema.name.maxLength}
             onChange={(e) => setName(e.target.value)}
+            disabled={fieldDisabled}
           />
           <TextArea
             label="Description"
             value={description}
             maxLength={collectionMetadataSchema.description.maxLength}
             onChange={(e) => setDescription(e.target.value)}
+            disabled={fieldDisabled}
           />
           <Shelf justifyContent="space-between">
             {balanceLow && (
@@ -107,10 +118,10 @@ export const CreateCollectionDialog: React.FC<{ open: boolean; onClose: () => vo
             )}
             <ButtonGroup ml="auto">
               {uploadError && <Text color="criticalPrimary">Failed to create collection</Text>}
-              <Button variant="outlined" onClick={close}>
+              <Button variant="outlined" onClick={close} disabled={fieldDisabled}>
                 Cancel
               </Button>
-              <Button type="submit" disabled={disabled} loading={metadataIsUploading || transactionIsPending}>
+              <Button type="submit" disabled={disabled} loading={isTxPending}>
                 Create
               </Button>
             </ButtonGroup>
