85a798c31ee90c612b7126ece6509f5b2a1d9771
sophialittlejohn <littlejohn.sophia@gmail.com>
2022-04-04 21:18:56 +0200

    Add sorting to DataTable

--- nft-studio/src/components/DataTable.tsx
+++ nft-studio/src/components/DataTable.tsx
@@ -10,52 +10,73 @@ type Props = {
   onRowClicked?: (row: any) => void
 }
 
-type Column = {
-  header: React.ReactNode
+export type OrderBy = 'asc' | 'desc'
+
+export type Column = {
+  header: string | ((orderBy: OrderBy) => React.ReactNode)
   cell: (row: any) => React.ReactNode
   align?: string
   flex?: string
+  sortKey?: string
+}
+const sorter = (data: Array<any>, order: OrderBy, sortKey?: string) => {
+  if (!sortKey) return data
+  if (order === 'asc') {
+    return data.sort((a, b) => {
+      return a[sortKey] - b[sortKey]
+    })
+  }
+  return data.sort((a, b) => {
+    return b[sortKey] - a[sortKey]
+  })
 }
 
-export const DataTable: React.VFC<Props> = ({ data, columns, keyField, onRowClicked }) => {
+export const DataTable: React.VFC<Props> = ({ data: initialData, columns, keyField, onRowClicked }) => {
+  const [data, setData] = React.useState(initialData)
+  const [orderBy, setOrderBy] = React.useState<Record<string, OrderBy>>({})
+
+  const sortData = (column: Column) => {
+    if (column?.sortKey) {
+      const updatedOrderBy = orderBy[column.sortKey] === 'asc' ? 'desc' : 'asc'
+      const sortedData = sorter(data, updatedOrderBy, column?.sortKey)
+      setOrderBy({ ...orderBy, [column.sortKey]: updatedOrderBy })
+      setData(sortedData)
+    }
+  }
+
   return (
     <Stack>
       <Shelf>
-        {columns.map((col, i) =>
-          col.align === 'left' ? (
-            <HeaderColLeft key={`${col.header}-${i}`} style={{ flex: col.flex }}>
-              <Text variant="label1">{col.header}</Text>
-            </HeaderColLeft>
-          ) : (
-            <HeaderCol key={`${col.header}-${i}`} style={{ flex: col.flex }}>
-              <Text variant="label1">{col.header}</Text>
-            </HeaderCol>
-          )
-        )}
+        {columns.map((col: any, i) => (
+          <HeaderCol
+            key={`${col.header}-${i}`}
+            style={{ flex: col.flex }}
+            tabIndex={col?.sortKey ? 0 : undefined}
+            as={col?.sortKey ? 'button' : 'div'}
+            onClick={col?.sortKey && (() => sortData(col))}
+            align={col?.align}
+          >
+            <Text variant="label1">
+              {typeof col?.header !== 'string' && col?.sortKey ? col.header(orderBy[col.sortKey]) : col.header}
+            </Text>
+          </HeaderCol>
+        ))}
       </Shelf>
       <Card>
-        {data.map((row: any, i) => {
-          return (
-            <Row
-              as={onRowClicked ? 'button' : 'div'}
-              key={keyField ? row[keyField] : i}
-              onClick={onRowClicked && (() => onRowClicked(row))}
-              tabIndex={onRowClicked ? 0 : undefined}
-            >
-              {columns.map((col) =>
-                col.align === 'left' ? (
-                  <React.Fragment key={`${col.header}-${i}`}>
-                    <DataColLeft style={{ flex: col.flex }}>{col.cell(row)}</DataColLeft>
-                  </React.Fragment>
-                ) : (
-                  <React.Fragment key={`${col.header}-${i}`}>
-                    <DataCol style={{ flex: col.flex }}>{col.cell(row)}</DataCol>
-                  </React.Fragment>
-                )
-              )}
-            </Row>
-          )
-        })}
+        {data.map((row: any, i) => (
+          <Row
+            as={onRowClicked ? 'button' : 'div'}
+            key={keyField ? row[keyField] : i}
+            onClick={onRowClicked && (() => onRowClicked(row))}
+            tabIndex={onRowClicked ? 0 : undefined}
+          >
+            {columns.map((col) => (
+              <DataCol style={{ flex: col.flex }} align={col?.align} key={`${col.header}-${i}`}>
+                {col.cell(row)}
+              </DataCol>
+            ))}
+          </Row>
+        ))}
       </Card>
     </Stack>
   )
@@ -91,20 +112,28 @@ const Row = styled(Shelf)(
   })
 )
 
-const DataCol = styled.div`
-  flex: 1 1 160px;
-  padding: 8px 24px;
-  display: flex;
-  justify-content: flex-end;
-`
-
-const DataColLeft = styled.div`
-  flex: 1 1 160px;
-  padding: 16px 12px 16px 24px;
-  display: flex;
-  justify-content: flex-start;
+const DataCol = styled.div<{ align: Column['align'] }>`
+  background: initial;
+  border: none;
+  ${({ align }) => {
+    switch (align) {
+      case 'left':
+        return css({
+          flex: '1 1 160px',
+          padding: '16px 12px 16px 24px',
+          display: 'flex',
+          justifyContent: 'flex-start',
+        })
+      case 'right':
+      default:
+        return css({
+          flex: '1 1 160px',
+          padding: '8px 24px',
+          display: 'flex',
+          justifyContent: 'flex-end',
+        })
+    }
+  }}
 `
 
 const HeaderCol = styled(DataCol)``
-
-const HeaderColLeft = styled(DataColLeft)``
--- nft-studio/src/components/TokenList.tsx
+++ nft-studio/src/components/TokenList.tsx
@@ -1,11 +1,11 @@
 import { Pool, Tranche } from '@centrifuge/centrifuge-js'
-import { IconChevronRight, Shelf, Text } from '@centrifuge/fabric'
+import { IconArrowDown, IconChevronRight, Shelf, Text } from '@centrifuge/fabric'
 import { BN } from 'bn.js'
 import * as React from 'react'
 import { useHistory } from 'react-router'
 import { usePoolMetadata } from '../utils/usePools'
 import { useCentrifuge } from './CentrifugeProvider'
-import { DataTable } from './DataTable'
+import { Column, DataTable, OrderBy } from './DataTable'
 import { TokenAvatar } from './TokenAvatar'
 
 type Props = {
@@ -13,31 +13,39 @@ type Props = {
 }
 
 type Row = Tranche & {
-  pool: Pool
+  poolMetadata: string
+  yield: string
+  protection: string
 }
 
-const columns = [
+type RowProps = {
+  token: Row
+}
+
+const columns: Column[] = [
   {
     align: 'left',
     header: 'Token',
-    cell: (i: Row) => <TokenName token={i} />,
+    cell: (token: Row) => <TokenName token={token} />,
     flex: '2',
   },
   {
     align: 'left',
     header: 'Asset class',
-    cell: (i: Row) => <AssetClass token={i} />,
+    cell: (token: Row) => <AssetClass token={token} />,
     flex: '2',
   },
   {
-    header: 'Yield',
-    cell: (i: Row) => <Yield token={i} />,
+    header: (orderBy: OrderBy) => <SortableHeader label="Yield" orderBy={orderBy} />,
+    cell: (token: Row) => <Yield token={token} />,
     flex: '1',
+    sortKey: 'yield',
   },
   {
-    header: 'Protection',
-    cell: (i: Row) => <Protection token={i} />,
+    header: (orderBy: OrderBy) => <SortableHeader label="Protection" orderBy={orderBy} />,
+    cell: (token: Row) => <Protection token={token} />,
     flex: '1',
+    sortKey: 'protection',
   },
   {
     header: '',
@@ -48,49 +56,73 @@ const columns = [
 
 export const TokenList: React.FC<Props> = ({ pools }) => {
   const history = useHistory()
-  const tokens = pools?.map((pool) => pool.tranches.map((tranche) => ({ ...tranche, pool }))).flat()
+  const { utils } = useCentrifuge()
+  const tokens = pools
+    ?.map((pool) =>
+      pool.tranches.map((tranche) => ({
+        ...tranche,
+        poolMetadata: pool.metadata,
+        // feeToApr is a temporary solution for calculating yield
+        // bc we don't have a way to query for historical token prices yet
+        // Use this formula when prices can be fetched: https://docs.centrifuge.io/learn/terms/#30d-drop-yield
+        yield: utils.feeToApr(tranche.interestPerSec),
+        // for now proctection is being calculated as a percentage of the ratio
+        // replace with proper protection calculation when token prices are available
+        protection: tranche.ratio,
+      }))
+    )
+    .flat()
 
   return (
     <DataTable
       data={tokens}
       columns={columns}
-      onRowClicked={(i: Row) => {
-        history.push(`/tokens/${i.name}`)
+      onRowClicked={(token: Row) => {
+        history.push(`/tokens/${token.name}`)
       }}
     />
   )
 }
 
-const TokenName: React.VFC<{ token: Row }> = ({ token }) => {
-  const { data: metadata } = usePoolMetadata(token?.pool)
+const TokenName: React.VFC<RowProps> = ({ token }) => {
+  const { data: metadata } = usePoolMetadata({ metadata: token.poolMetadata })
   const symbol = metadata?.tranches?.find((_, index) => index === token.index)?.symbol
   return (
     <Shelf gap="2">
       <TokenAvatar label={symbol || ''} size="small" />
       <Text variant="body2" color="textPrimary" fontWeight={600}>
-        {metadata?.pool?.name} {token?.name}
+        {metadata?.pool?.name || 'Nameless Pool'} {token?.name}
       </Text>
     </Shelf>
   )
 }
 
-const AssetClass: React.VFC<{ token: Row }> = ({ token }) => {
-  const { data: metadata } = usePoolMetadata(token?.pool)
-  return <Text variant="body2">{metadata?.pool?.asset.class}</Text>
+const AssetClass: React.VFC<RowProps> = ({ token }) => {
+  const { data: metadata } = usePoolMetadata({ metadata: token.poolMetadata })
+  return <Text variant="body2">{metadata?.pool?.asset.class || '-'}</Text>
 }
 
-const Protection: React.VFC<{ token: Row }> = ({ token }) => {
-  const centrifuge = useCentrifuge()
+const Yield: React.VFC<RowProps> = ({ token }) => {
+  const apr = parseInt(token.yield, 10)
+  return <Text variant="body2">{apr > 0 ? `Target: ${apr.toFixed(2)}%` : '-'}</Text>
+}
+
+const Protection: React.VFC<RowProps> = ({ token }) => {
+  const { utils } = useCentrifuge()
   return (
-    <Text variant="body2">{centrifuge.utils.formatPercentage(token.ratio, new BN(10).pow(new BN(18)).toString())}</Text>
+    <Text variant="body2">
+      {parseInt(token.protection, 10) > 0
+        ? utils.formatPercentage(token.protection, new BN(10).pow(new BN(18)).toString())
+        : '-'}
+    </Text>
   )
 }
 
-const Yield: React.VFC<{ token: Row }> = ({ token }) => {
-  const { utils } = useCentrifuge()
-  // feeToApr is a temporary solution for calculating yield
-  // bc we don't have a way to query for historical token prices yet
-  // Use this formula when prices can be fetched: https://docs.centrifuge.io/learn/terms/#30d-drop-yield
-  const APR = parseInt(utils.feeToApr(token.interestPerSec), 10)
-  return <Text variant="body2">{APR > 0 ? `Target: ${APR.toFixed(2)}%` : '-'}</Text>
+const SortableHeader: React.VFC<{ label: string; orderBy: OrderBy }> = ({ label, orderBy }) => {
+  return (
+    <Shelf>
+      {label}
+      <IconArrowDown size={16} style={{ transform: orderBy === 'asc' ? 'rotate(180deg)' : 'rotate(0deg)' }} />
+    </Shelf>
+  )
 }
