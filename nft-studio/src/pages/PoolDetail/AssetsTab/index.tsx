97376bbf7a66aad4d26edf674cf29d709081fb03
Onno Visser <visser.onno@gmail.com>
2022-05-10 20:40:45 +0200

    add temp assets list

--- nft-studio/src/pages/PoolDetail/AssetsTab/index.tsx
+++ nft-studio/src/pages/PoolDetail/AssetsTab/index.tsx
@@ -1,6 +1,12 @@
+import { Text } from '@centrifuge/fabric'
 import * as React from 'react'
+import { useHistory, useParams } from 'react-router'
 import { LoadBoundary } from '../../../components/LoadBoundary'
+import { LoanList } from '../../../components/LoanList'
+import { PageSection } from '../../../components/PageSection'
 import { PageWithSideBar } from '../../../components/PageWithSideBar'
+import { useLoans } from '../../../utils/useLoans'
+import { usePool } from '../../../utils/usePools'
 import { PoolDetailHeader } from '../Header'
 
 export const PoolDetailAssetsTab: React.FC = () => {
@@ -15,5 +21,25 @@ export const PoolDetailAssetsTab: React.FC = () => {
 }
 
 const PoolDetailAssets: React.FC = () => {
-  return <div>Assets</div>
+  const { pid: poolId } = useParams<{ pid: string }>()
+  const pool = usePool(poolId)
+  const loans = useLoans(poolId)
+  const history = useHistory()
+
+  if (!pool || !loans) return null
+
+  return (
+    <PageSection title="Assets">
+      {loans.length ? (
+        <LoanList
+          loans={loans}
+          onLoanClicked={(loan) => {
+            history.push(`/pools/${pool.id}/assets/${loan.id}`)
+          }}
+        />
+      ) : (
+        <Text>No assets have been originated yet</Text>
+      )}
+    </PageSection>
+  )
 }
