a9126e858fd0aab3846038d3d3e5c08e03ac0927
Jeroen Offerijns <Offerijns@users.noreply.github.com>
2022-06-13 13:25:08 +0200

    Update subquery calls (#862)

--- centrifuge-js/src/types/subquery.ts
+++ centrifuge-js/src/types/subquery.ts
@@ -1,32 +1,12 @@
-export type SubqueryTrancheState = {
-  __typename?: 'TrancheState'
+export type SubqueryPoolSnapshot = {
+  __typename?: 'PoolSnapshot'
   id: string
-  poolState: SubqueryPoolState
-  supply: number
-  price: number
-  outstandingInvestOrders: number
-  outstandingRedeemOrders: number
-  yield30Days?: number | null
-  yield90Days?: number | null
-  yieldSinceInception?: number | null
-}
-
-export type SubqueryPoolState = {
-  __typename?: 'PoolState'
-  id: string
+  timestamp: string
   netAssetValue: number
   totalReserve: number
   availableReserve: number
   maxReserve: number
   totalDebt?: number | null
-  trancheStates?: Array<SubqueryTrancheState | null> | null
-}
-
-export type SubqueryDailyPoolState = {
-  __typename?: 'DailyPoolState'
-  id: string
-  timestamp: string
-  poolState: SubqueryPoolState
   totalBorrowed?: number | null
   totalRepaid?: number | null
   totalInvested?: number | null
