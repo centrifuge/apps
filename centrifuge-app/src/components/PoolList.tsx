63c6df3b64f0ce1a1ace7d2f2c975c2f9f9e5040
sophialittlejohn <littlejohn.sophia@gmail.com>
2022-06-14 20:09:25 +0200

    Add pool logo

--- centrifuge-app/src/components/PoolList.tsx
+++ centrifuge-app/src/components/PoolList.tsx
@@ -1,8 +1,10 @@
 import { Pool } from '@centrifuge/centrifuge-js'
-import { IconChevronRight, Text } from '@centrifuge/fabric'
+import { IconChevronRight, Shelf, Text, Thumbnail } from '@centrifuge/fabric'
 import * as React from 'react'
 import { useHistory, useRouteMatch } from 'react-router'
+import styled from 'styled-components'
 import { formatBalance } from '../utils/formatting'
+import { parseMetadataUrl } from '../utils/parseMetadataUrl'
 import { usePoolMetadata } from '../utils/usePools'
 import { Column, DataTable } from './DataTable'
 import { TextWithPlaceholder } from './TextWithPlaceholder'
@@ -52,11 +54,32 @@ export const PoolList: React.FC<Props> = ({ pools }) => {
 const PoolName: React.VFC<{ pool: Pool }> = ({ pool }) => {
   const { data, isLoading } = usePoolMetadata(pool)
   return (
-    <TextWithPlaceholder isLoading={isLoading} variant="body2" fontWeight={600} textOverflow="ellipsis">
-      {data?.pool?.name ?? 'Unnamed Pool'}
-    </TextWithPlaceholder>
+    <Shelf alignItems="center" gap={1}>
+      {data?.pool?.icon ? (
+        <StyledLogo src={parseMetadataUrl(data.pool.icon)} alt="pool logo" height="24" width="24" />
+      ) : (
+        <Thumbnail
+          type="pool"
+          label={
+            data?.pool?.name
+              .split(' ')
+              .map((letters) => letters[0].toUpperCase())
+              .slice(0, 2)
+              .join('') ?? ''
+          }
+          size="small"
+        />
+      )}
+      <TextWithPlaceholder isLoading={isLoading} variant="body2" fontWeight={600} textOverflow="ellipsis">
+        {data?.pool?.name ?? 'Unnamed Pool'}
+      </TextWithPlaceholder>
+    </Shelf>
   )
 }
+
+const StyledLogo = styled.img`
+  border-radius: 4px;
+`
 
 const AssetClass: React.VFC<{ pool: Pool }> = ({ pool }) => {
   const { data, isLoading } = usePoolMetadata(pool)
