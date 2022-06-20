587cd407ef94bf49a9c31885e5561fe845b17d9c
sophialittlejohn <littlejohn.sophia@gmail.com>
2022-06-20 18:40:21 +0200

    Remove unused history

--- centrifuge-app/src/pages/Token.tsx
+++ centrifuge-app/src/pages/Token.tsx
@@ -1,6 +1,6 @@
 import { Shelf, Stack, Text, Thumbnail } from '@centrifuge/fabric'
 import * as React from 'react'
-import { useHistory, useParams, useRouteMatch } from 'react-router'
+import { useParams, useRouteMatch } from 'react-router'
 import { InvestRedeem } from '../components/InvestRedeem'
 import { PageHeader } from '../components/PageHeader'
 import { PageSection } from '../components/PageSection'
@@ -22,7 +22,6 @@ export const TokenDetailPage: React.FC = () => {
 }
 
 const TokenDetail: React.FC = () => {
-  const history = useHistory()
   const basePath = useRouteMatch(['/investments', '/issuer'])?.path || ''
   const { pid: poolId, tid: trancheId } = useParams<{ pid: string; tid: string }>()
   const pool = usePool(poolId)
