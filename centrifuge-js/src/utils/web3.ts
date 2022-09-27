d57c0bce908d5b5826fa6d97a5ca3dadf1a7c339
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2022-09-27 16:10:55 +0200

    temp fix

--- centrifuge-js/src/utils/web3.ts
+++ centrifuge-js/src/utils/web3.ts
@@ -1,17 +1,18 @@
 import { ApiRx, WsProvider } from '@polkadot/api'
-import { DefinitionRpc, DefinitionRpcSub, RegistryTypes } from '@polkadot/types/types'
+import { DefinitionRpc, DefinitionRpcSub } from '@polkadot/types/types'
 import { Observable } from 'rxjs'
 
 const cached: { [key: string]: Observable<ApiRx> } = {}
 export function getPolkadotApi(
   wsUrl: string,
-  types?: RegistryTypes,
+  types?: any,
   rpc?: Record<string, Record<string, DefinitionRpc | DefinitionRpcSub>>
 ) {
   return (
     cached[wsUrl] ||
     (cached[wsUrl] = ApiRx.create({
       provider: new WsProvider(wsUrl),
+      // @ts-ignore
       types,
       rpc,
     }))
