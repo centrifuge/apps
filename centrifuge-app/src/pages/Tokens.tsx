e7fdaea8b252fc4d6fb63743b12edcf6bdfbb04f
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2022-10-04 10:18:01 +0200

    hide unlisted pools

--- centrifuge-app/src/pages/Tokens.tsx
+++ centrifuge-app/src/pages/Tokens.tsx
@@ -1,15 +1,19 @@
-import { Shelf, Stack, Text } from '@centrifuge/fabric'
+import { PoolMetadata } from '@centrifuge/centrifuge-js'
+import { IconChevronRight, Shelf, Stack, Text } from '@centrifuge/fabric'
 import * as React from 'react'
+import { DataTable } from '../components/DataTable'
 import { MenuSwitch } from '../components/MenuSwitch'
 import { PageHeader } from '../components/PageHeader'
 import { PageSummary } from '../components/PageSummary'
 import { PageWithSideBar } from '../components/PageWithSideBar'
+import { TextWithPlaceholder } from '../components/TextWithPlaceholder'
 import { TokenList, TokenTableData } from '../components/TokenList'
 import { Tooltips } from '../components/Tooltips'
 import { config } from '../config'
 import { Dec } from '../utils/Decimal'
 import { formatBalance, getCurrencySymbol } from '../utils/formatting'
-import { usePools, useTokens } from '../utils/usePools'
+import { useMetadataMulti } from '../utils/useMetadata'
+import { usePools } from '../utils/usePools'
 
 export const TokenOverviewPage: React.FC = () => {
   return (
@@ -20,12 +24,24 @@ export const TokenOverviewPage: React.FC = () => {
 }
 
 const TokenOverview: React.FC = () => {
-  const dataTokens = useTokens()
   const pools = usePools()
+
+  const poolMetas = useMetadataMulti<PoolMetadata>(pools?.map((p) => p.metadata) ?? [])
+
+  const [listedPools, listedTokens] = React.useMemo(
+    () => {
+      const listedPools = pools?.filter((_, i) => poolMetas[i].data?.pool?.listed)
+      const listedTokens = listedPools?.flatMap((p) => p.tranches)
+
+      return [listedPools, listedTokens]
+    },
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    poolMetas.map((q) => q.data)
+  )
 
   const tokens: TokenTableData[] | undefined = React.useMemo(
     () =>
-      dataTokens
+      listedTokens
         ?.map((tranche) => {
           return {
             ...tranche,
@@ -44,12 +60,12 @@ const TokenOverview: React.FC = () => {
           }
         })
         .flat() || [],
-    [dataTokens]
+    [listedTokens]
   )
 
   const totalValueLocked = React.useMemo(() => {
     return (
-      dataTokens
+      listedTokens
         ?.map((tranche) => ({
           valueLocked: tranche.totalIssuance
             .toDecimal()
@@ -58,14 +74,14 @@ const TokenOverview: React.FC = () => {
         }))
         .reduce((prev, curr) => prev.add(curr.valueLocked), Dec(0)) ?? Dec(0)
     )
-  }, [dataTokens])
+  }, [listedTokens])
 
   const pageSummaryData = [
     {
       label: <Tooltips type="tvl" />,
       value: formatBalance(Dec(totalValueLocked || 0), getCurrencySymbol(config.baseCurrency)),
     },
-    { label: 'Pools', value: pools?.length || 0 },
+    { label: 'Pools', value: listedPools?.length || 0 },
     { label: <Tooltips type="tokens" />, value: tokens?.length || 0 },
   ]
 
@@ -76,6 +92,56 @@ const TokenOverview: React.FC = () => {
         <>
           <PageSummary data={pageSummaryData} />
           <TokenList tokens={tokens} />
+        </>
+      ) : pools?.length ? (
+        <>
+          <PageSummary data={pageSummaryData} />
+          <DataTable
+            rounded={false}
+            data={[{}]}
+            columns={[
+              {
+                align: 'left',
+                header: 'Token',
+                cell: () => <TextWithPlaceholder isLoading />,
+                flex: '9',
+              },
+              {
+                align: 'left',
+                header: 'Asset class',
+                cell: () => <TextWithPlaceholder isLoading />,
+                flex: '6',
+              },
+              {
+                header: 'Yield',
+                cell: () => <TextWithPlaceholder isLoading width={4} />,
+                flex: '4',
+                sortKey: 'yield',
+              },
+              {
+                header: 'Protection',
+                cell: () => <TextWithPlaceholder isLoading width={4} />,
+                flex: '4',
+              },
+              {
+                header: 'Value locked',
+                cell: () => <TextWithPlaceholder isLoading width={6} />,
+                flex: '4',
+                sortKey: 'valueLocked',
+              },
+              {
+                header: 'Capacity',
+                cell: () => <TextWithPlaceholder isLoading width={6} />,
+                flex: '4',
+              },
+
+              {
+                header: '',
+                cell: () => <IconChevronRight size={24} color="textPrimary" />,
+                flex: '0 1 52px',
+              },
+            ]}
+          />
         </>
       ) : (
         <Shelf p={4} justifyContent="center" textAlign="center">
