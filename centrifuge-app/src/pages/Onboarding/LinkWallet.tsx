565437c5a9bfd693f0a879444edbf7a15e104967
JP Angelle <jp@centrifuge.io>
2022-12-07 22:53:51 -0600

    integrate auth

---
+++ centrifuge-app/src/pages/Onboarding/LinkWallet.tsx
@@ -0,0 +1,129 @@
+import { Box, Button, IconCheck, Shelf, Stack, Text } from '@centrifuge/fabric'
+import { useMemo } from 'react'
+import { useCentrifuge } from '../../components/CentrifugeProvider'
+import { useWeb3 } from '../../components/Web3Provider'
+
+type Props = {
+  isAuth: boolean
+  nextStep: () => void
+  refetchAuth: () => void
+}
+
+const AUTHORIZED_ONBOARDING_PROXY_TYPES = ['Any', 'Invest', 'NonTransfer', 'NonProxy']
+
+export const LinkWallet = ({ nextStep, isAuth, refetchAuth }: Props) => {
+  const { selectedWallet, selectedAccount, connect, proxy } = useWeb3()
+  const cent = useCentrifuge()
+
+  const login = async () => {
+    try {
+      if (selectedAccount?.address && selectedWallet?.signer) {
+        const { address } = selectedAccount
+
+        const { token } = await cent.auth.generateJw3t(
+          address,
+          // @ts-expect-error Signer type version mismatch
+          selectedWallet.signer
+        )
+
+        if (token) {
+          if (proxy) {
+            const { delegator, types } = proxy
+
+            const isAuthorizedProxy = AUTHORIZED_ONBOARDING_PROXY_TYPES.some((proxyType) => types.includes(proxyType))
+
+            if (isAuthorizedProxy) {
+              sessionStorage.setItem(`centrifuge-auth-${address}-${delegator}`, token)
+            }
+          } else {
+            sessionStorage.setItem(`centrifuge-auth-${address}`, token)
+          }
+
+          // update database with address
+          refetchAuth()
+        }
+      }
+    } catch {}
+  }
+
+  const linkButtonText = useMemo(() => {
+    if (isAuth) {
+      return (
+        <Shelf gap={1}>
+          <Text variant="heading2" color="textButtonSecondaryDisabled">
+            Linked
+          </Text>
+          <IconCheck />
+        </Shelf>
+      )
+    }
+
+    return <Text variant="heading2">Link wallet</Text>
+  }, [isAuth])
+
+  const connectButtonText = useMemo(() => {
+    if (selectedAccount) {
+      return (
+        <Shelf gap={1}>
+          <Text variant="heading2" color="textButtonSecondaryDisabled">
+            Connected
+          </Text>
+          <IconCheck />
+        </Shelf>
+      )
+    }
+
+    return <Text variant="heading2">Connect wallet</Text>
+  }, [selectedAccount])
+
+  return (
+    <Stack gap={4}>
+      <Stack alignItems="flex-start" gap={10}>
+        <Text fontSize={5}>Connect and link your wallet</Text>
+        <Shelf gap={4}>
+          <button
+            onClick={() => connect()}
+            disabled={!!selectedAccount}
+            style={{
+              background: 'none',
+              border: '1px solid #e0e0e0',
+              borderRadius: '11px',
+              width: '200px',
+              height: '200px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              cursor: !!selectedAccount ? 'not-allowed' : 'pointer',
+              backgroundColor: !!selectedAccount ? '#e0e0e0' : 'white',
+            }}
+          >
+            {connectButtonText}
+          </button>
+          <button
+            onClick={() => login()}
+            disabled={!!isAuth}
+            style={{
+              background: 'none',
+              border: '1px solid #e0e0e0',
+              borderRadius: '11px',
+              width: '200px',
+              height: '200px',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              cursor: !!isAuth ? 'not-allowed' : 'pointer',
+              backgroundColor: !!isAuth ? '#e0e0e0' : 'white',
+            }}
+          >
+            {linkButtonText}
+          </button>
+        </Shelf>
+      </Stack>
+      <Box>
+        <Button variant="primary" onClick={() => nextStep()} disabled={!isAuth || !selectedAccount}>
+          Next
+        </Button>
+      </Box>
+    </Stack>
+  )
+}
