dac7cf40e1d238ebb89db774a182a3a9adb8341e
JP <jp@k-f.co>
2023-01-19 22:26:31 -0600

    add back buttons and prepare ShuftiPro (#1150)
    * add back buttons
    
    * remove useMemos, fix types

--- centrifuge-app/src/types/index.ts
+++ centrifuge-app/src/types/index.ts
@@ -52,6 +52,63 @@ export type LoanTemplate = {
 
 export type InvestorTypes = 'individual' | 'entity'
 
-export type ultimateBeneficialOwner = {
+export type UltimateBeneficialOwner = {
   name: string
+  dateOfBirth: string
 }
+
+type IndividualUserSteps = {
+  verifyIdentity: {
+    completed: boolean
+    timeStamp: string
+  }
+  signAgreements: {
+    [poolId: string]: {
+      [trancheId: string]: {
+        completed: boolean
+        timeStamp: string
+      }
+    }
+  }
+}
+
+export interface EntityOnboardingSteps extends IndividualUserSteps {
+  verifyBusiness: {
+    completed: boolean
+    timeStamp: string
+  }
+  verifyEmail: {
+    completed: boolean
+    timeStamp: string
+  }
+  confirmOwners: {
+    completed: boolean
+    timeStamp: string
+  }
+}
+
+export type EntityUser = {
+  investorType: 'entity'
+  walletAddress: string
+  businessName: string
+  email: string
+  incorporationDate: string
+  jurisdictionCode: string
+  registrationNumber: number
+  ultimateBeneficialOwners: UltimateBeneficialOwner[]
+  steps: EntityOnboardingSteps
+  name: string
+  dateOfBirth: string
+  countryOfCitizenship: string
+}
+
+type IndividualUser = {
+  investorType: 'individual'
+  walletAddress: string
+  name: string
+  dateOfBirth: string
+  countryOfCitizenship: string
+  steps: IndividualUserSteps
+}
+
+export type OnboardingUser = IndividualUser | EntityUser
