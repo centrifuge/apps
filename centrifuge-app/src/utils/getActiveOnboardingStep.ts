c0c7be6a04d8bb44d99f6b744ee9b4804b9042b2
JP Angelle <jp@k-f.co>
2023-01-30 13:21:45 -0600

    rename flow to steps

--- centrifuge-app/src/utils/getActiveOnboardingStep.ts
+++ centrifuge-app/src/utils/getActiveOnboardingStep.ts
@@ -1,11 +1,11 @@
 import { OnboardingUser } from '../types'
 
-const BASE_FLOW = {
+const BASE_STEPS = {
   LINK_WALLET: 1,
   CHOOSE_INVESTOR_TYPE: 2,
 }
 
-const ENTITY_FLOW = {
+const ENTITY_STEPS = {
   US: {
     VERIFY_ACCREDITATION: 7,
     SIGN_AGREEMENT: 8,
@@ -15,14 +15,14 @@ const ENTITY_FLOW = {
     SIGN_AGREEMENT: 7,
     COMPLETE: 8,
   },
-  ...BASE_FLOW,
+  ...BASE_STEPS,
   VERIFY_BUSINESS: 3,
   CONFIRM_OWNERS: 4,
   VERIFY_IDENTITY: 5,
   VERIFY_TAX_INFO: 6,
 }
 
-const INDIVIDUAL_FLOW = {
+const INDIVIDUAL_STEPS = {
   US: {
     VERIFY_ACCREDITATION: 5,
     SIGN_AGREEMENT: 6,
@@ -32,14 +32,14 @@ const INDIVIDUAL_FLOW = {
     SIGN_AGREEMENT: 5,
     COMPLETE: 6,
   },
-  ...BASE_FLOW,
+  ...BASE_STEPS,
   VERIFY_IDENTITY: 3,
   VERIFY_TAX_INFO: 4,
 }
 
 export const getActiveOnboardingStep = (onboardingUser: OnboardingUser, poolId: string, trancheId: string) => {
   // user does not exist
-  if (!Object.keys(onboardingUser).length) return BASE_FLOW.LINK_WALLET
+  if (!Object.keys(onboardingUser).length) return BASE_STEPS.LINK_WALLET
 
   const { investorType, countryOfCitizenship } = onboardingUser
   const { verifyIdentity, verifyTaxInfo, verifyAccreditation } = onboardingUser.steps
@@ -51,31 +51,31 @@ export const getActiveOnboardingStep = (onboardingUser: OnboardingUser, poolId: 
     const { confirmOwners, verifyBusiness } = onboardingUser.steps
 
     if (jurisdictionCode === 'us') {
-      if (completed) return ENTITY_FLOW.US.COMPLETE
-      if (verifyAccreditation.completed) return ENTITY_FLOW.US.SIGN_AGREEMENT
-      if (verifyTaxInfo.completed) return ENTITY_FLOW.US.VERIFY_ACCREDITATION
+      if (completed) return ENTITY_STEPS.US.COMPLETE
+      if (verifyAccreditation.completed) return ENTITY_STEPS.US.SIGN_AGREEMENT
+      if (verifyTaxInfo.completed) return ENTITY_STEPS.US.VERIFY_ACCREDITATION
     } else {
-      if (completed) return ENTITY_FLOW.NON_US.COMPLETE
-      if (verifyTaxInfo.completed) return ENTITY_FLOW.US.SIGN_AGREEMENT
+      if (completed) return ENTITY_STEPS.NON_US.COMPLETE
+      if (verifyTaxInfo.completed) return ENTITY_STEPS.US.SIGN_AGREEMENT
     }
 
-    if (verifyIdentity.completed) return ENTITY_FLOW.VERIFY_TAX_INFO
-    if (confirmOwners.completed) return ENTITY_FLOW.VERIFY_IDENTITY
-    if (verifyBusiness.completed) return ENTITY_FLOW.CONFIRM_OWNERS
+    if (verifyIdentity.completed) return ENTITY_STEPS.VERIFY_TAX_INFO
+    if (confirmOwners.completed) return ENTITY_STEPS.VERIFY_IDENTITY
+    if (verifyBusiness.completed) return ENTITY_STEPS.CONFIRM_OWNERS
   }
 
   if (investorType === 'individual') {
     if (countryOfCitizenship === 'us') {
-      if (completed) return INDIVIDUAL_FLOW.US.COMPLETE
-      if (verifyAccreditation.completed) return INDIVIDUAL_FLOW.US.SIGN_AGREEMENT
-      if (verifyTaxInfo.completed) return INDIVIDUAL_FLOW.US.VERIFY_ACCREDITATION
+      if (completed) return INDIVIDUAL_STEPS.US.COMPLETE
+      if (verifyAccreditation.completed) return INDIVIDUAL_STEPS.US.SIGN_AGREEMENT
+      if (verifyTaxInfo.completed) return INDIVIDUAL_STEPS.US.VERIFY_ACCREDITATION
     } else {
-      if (completed) return INDIVIDUAL_FLOW.NON_US.COMPLETE
-      if (verifyTaxInfo.completed) return INDIVIDUAL_FLOW.NON_US.SIGN_AGREEMENT
+      if (completed) return INDIVIDUAL_STEPS.NON_US.COMPLETE
+      if (verifyTaxInfo.completed) return INDIVIDUAL_STEPS.NON_US.SIGN_AGREEMENT
     }
 
-    if (verifyIdentity.completed) return INDIVIDUAL_FLOW.VERIFY_TAX_INFO
+    if (verifyIdentity.completed) return INDIVIDUAL_STEPS.VERIFY_TAX_INFO
   }
 
-  return BASE_FLOW.LINK_WALLET
+  return BASE_STEPS.LINK_WALLET
 }
