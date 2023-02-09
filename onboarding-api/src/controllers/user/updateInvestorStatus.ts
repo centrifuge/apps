772ce00357a43692d70fb8a78ea4d5925728acf8
sophian <littlejohn.sophia@gmail.com>
2023-02-08 23:27:06 -0500

    Add approval email sending

--- onboarding-api/src/controllers/user/updateInvestorStatus.ts
+++ onboarding-api/src/controllers/user/updateInvestorStatus.ts
@@ -1,15 +1,17 @@
 import { Request, Response } from 'express'
-import { InferType, object, string } from 'yup'
-import { validateAndWriteToFirestore } from '../../database'
+import { InferType, object, string, StringSchema } from 'yup'
+import { OnboardingUser, validateAndWriteToFirestore } from '../../database'
+import { sendApproveInvestorMessage } from '../../emails/sendApproveInvestorMessage'
 import { UpdateInvestorStatusPayload } from '../../emails/sendDocumentsToIssuer'
 import { fetchUser } from '../../utils/fetchUser'
 import { HttpsError } from '../../utils/httpsError'
+import { Subset } from '../../utils/types'
 import { validateInput } from '../../utils/validateInput'
 import { verifyJwt } from '../../utils/verifyJwt'
 
 const updateInvestorStatusParams = object({
   token: string().required(),
-  status: string().oneOf(['approved', 'rejected']).required(),
+  status: string().oneOf(['approved', 'rejected']).required() as StringSchema<'approved' | 'rejected'>,
 })
 
 export const updateInvestorStatusController = async (
@@ -29,23 +31,23 @@ export const updateInvestorStatusController = async (
       throw new HttpsError(400, 'Investor status may have already been updated')
     }
 
-    await validateAndWriteToFirestore(
-      walletAddress,
-      {
-        onboardingStatus: {
-          [poolId]: {
-            [trancheId]: {
-              status,
-              timeStamp: new Date().toISOString(),
-            },
+    const updatedUser: Subset<OnboardingUser> = {
+      onboardingStatus: {
+        [poolId]: {
+          [trancheId]: {
+            status,
+            timeStamp: new Date().toISOString(),
           },
         },
       },
-      'entity',
-      ['onboardingStatus']
-    )
+    }
+
+    await validateAndWriteToFirestore(walletAddress, updatedUser, 'entity', ['onboardingStatus'])
 
     // TODO: send email to investor
+    if (user.email && status === 'approved') {
+      await sendApproveInvestorMessage(user.email, poolId, trancheId)
+    }
     return res.status(204).send()
   } catch (error) {
     if (error instanceof HttpsError) {
