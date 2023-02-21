e57f730cd4005fb40866d9c1e35060940f8be279
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2023-02-21 15:09:48 +0100

    add tinlake module

---
+++ centrifuge-js/src/modules/tinlake/abi/index.ts
@@ -0,0 +1,10 @@
+import contractAbiOperator from './Operator.abi.json'
+import contractAbiCurrency from './RestrictedToken.abi.json'
+
+export const abis = {
+  TINLAKE_CURRENCY: contractAbiCurrency,
+  JUNIOR_TOKEN: contractAbiCurrency,
+  SENIOR_TOKEN: contractAbiCurrency,
+  JUNIOR_OPERATOR: contractAbiOperator,
+  SENIOR_OPERATOR: contractAbiOperator,
+}
