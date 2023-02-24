5b2eebd9a8d81cb91b9c884fdd2d93dc66a93315
sophian <littlejohn.sophia@gmail.com>
2023-02-23 19:41:06 -0500

    Allow error supression for user queries to avoid failed network requests in UI

--- onboarding-api/src/utils/fetchUser.ts
+++ onboarding-api/src/utils/fetchUser.ts
@@ -1,13 +1,22 @@
 import { OnboardingUser, userCollection } from '../database'
 import { HttpsError } from './httpsError'
 
-export async function fetchUser(walletAddress: string) {
+type FetchUserOptions<T> = T extends { suppressError?: boolean } ? T : never
+type OnboardingUserOrNull<T> = T extends { suppressError?: boolean } ? OnboardingUser | null : OnboardingUser
+
+export async function fetchUser<T>(
+  walletAddress: string,
+  options?: FetchUserOptions<T>
+): Promise<OnboardingUserOrNull<T>> {
   try {
     const userDoc = await userCollection.doc(walletAddress).get()
     if (!userDoc.exists) {
+      if (options && options.suppressError) {
+        return null as OnboardingUserOrNull<T>
+      }
       throw new Error("User doesn't exist")
     }
-    return userDoc.data() as OnboardingUser
+    return userDoc.data() as OnboardingUserOrNull<T>
   } catch (error) {
     console.error('Firestore error:', JSON.stringify(error))
     throw new HttpsError(401, 'Not allowed')
