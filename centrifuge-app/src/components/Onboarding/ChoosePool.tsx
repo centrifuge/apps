61e01511fe478ccf7a9c4fcab35b8362dbca6ba1
JP Angelle <jp@k-f.co>
2023-03-22 12:33:34 -0500

    update verbiage

--- centrifuge-app/src/components/Onboarding/ChoosePool.tsx
+++ centrifuge-app/src/components/Onboarding/ChoosePool.tsx
@@ -10,7 +10,7 @@ export const ChoosePool = () => {
       <Content>
         <ContentHeader
           title="Choose a pool"
-          body="Your identity has been verified! Click the button below to view the possible pools to finish onboarding."
+          body="Your identity has been verified. Click the button below to view the pools available for investment."
         />
       </Content>
 
