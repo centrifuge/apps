e4f5e37b6709af0c84e3484020ec1d7cdaabd955
Hornebom <you@hornebom.com>
2023-04-06 08:34:25 +0200

    logging

--- onboarding-api/src/middleware/cors.ts
+++ onboarding-api/src/middleware/cors.ts
@@ -9,9 +9,10 @@ const centrifugeDomains = [
 
 export const corsMiddleware = cors({
   origin: (origin, callback) => {
+    console.log('>> origin in cors', origin)
     const isLocalhost = /^(http:\/\/localhost:)./.test(origin)
     const isCentrifugeDomain = centrifugeDomains.some((regex) => regex.test(origin))
-    const isShuftiDomain = /^https:\/\/app\.shuftipro\.com$/.test(origin)
+    const isShuftiDomain = /^https:\/\/app\.shuftipro\.com/.test(origin)
 
     if (isLocalhost || isCentrifugeDomain || isShuftiDomain) {
       callback(null, true)
