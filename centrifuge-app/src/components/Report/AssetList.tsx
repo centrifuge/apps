8f61b26b14957eac7a6ce2088a82fa765bbac379
Hornebom <you@hornebom.com>
2023-05-16 10:29:06 +0200

    update to latest loan changes
    wip state

--- centrifuge-app/src/components/Report/AssetList.tsx
+++ centrifuge-app/src/components/Report/AssetList.tsx
@@ -1,7 +1,8 @@
-import { ActiveLoan, Loan, Pool } from '@centrifuge/centrifuge-js/dist/modules/pools'
+// import { ActiveLoan, Loan, Pool } from '@centrifuge/centrifuge-js/dist/modules/pools'
+import { Loan, Pool } from '@centrifuge/centrifuge-js/dist/modules/pools'
 import { Text } from '@centrifuge/fabric'
 import * as React from 'react'
-import { LOAN_TYPE_LABELS } from '../../pages/Loan/utils'
+// import { LOAN_TYPE_LABELS } from '../../pages/Loan/utils'
 import { formatDate } from '../../utils/date'
 import { formatBalance, formatPercentage } from '../../utils/formatting'
 import { getCSVDownloadUrl } from '../../utils/getCSVDownloadUrl'
@@ -25,29 +26,40 @@ export function AssetList({ pool }: { pool: Pool }) {
         name: '',
         value: [
           loan.id,
-          'loanInfo' in loan ? LOAN_TYPE_LABELS[loan.loanInfo.type] : '-',
+          // 'loanInfo' in loan ? LOAN_TYPE_LABELS[loan.loanInfo.type] : '-',
           loan.status === 'Created' ? 'New' : loan.status,
-          'loanInfo' in loan ? formatBalance(loan.loanInfo.value.toDecimal()) : '-',
-          formatBalance((loan as ActiveLoan).outstandingDebt.toDecimal()),
-          formatBalance((loan as ActiveLoan).totalBorrowed.toDecimal()),
-          formatBalance((loan as ActiveLoan).totalRepaid.toDecimal()),
-          loan.status !== 'Created' && loan.originationDate && Number(loan.originationDate) > 0
-            ? formatDate(loan.originationDate.toString())
-            : '-',
-          loan.status !== 'Created' && 'maturityDate' in loan.loanInfo
-            ? formatDate(loan.loanInfo.maturityDate.toString())
-            : '-',
-          'interestRatePerSec' in loan ? formatPercentage(loan.interestRatePerSec.toAprPercent()) : '-',
-          'loanInfo' in loan ? formatPercentage(loan.loanInfo.advanceRate.toPercent()) : '-',
-          loan.status !== 'Created' && 'probabilityOfDefault' in loan.loanInfo
-            ? formatPercentage(loan.loanInfo.probabilityOfDefault.toPercent())
-            : '-',
-          loan.status !== 'Created' && 'lossGivenDefault' in loan.loanInfo
-            ? formatPercentage(loan.loanInfo.lossGivenDefault.toPercent())
-            : '-',
-          loan.status !== 'Created' && 'discountRate' in loan.loanInfo
-            ? formatPercentage(loan.loanInfo.discountRate.toPercent())
-            : '-',
+          // 'loanInfo' in loan ? formatBalance(loan.loanInfo.value.toDecimal()) : '-',
+          // formatBalance((loan as ActiveLoan).outstandingDebt.toDecimal()),
+          // formatBalance((loan as ActiveLoan).totalBorrowed.toDecimal()),
+          // formatBalance((loan as ActiveLoan).totalRepaid.toDecimal()),
+          // loan.status !== 'Created' && loan.originationDate && Number(loan.originationDate) > 0
+          //   ? formatDate(loan.originationDate.toString())
+          //   : '-',
+          // loan.status !== 'Created' && 'maturityDate' in loan.loanInfo
+          //   ? formatDate(loan.loanInfo.maturityDate.toString())
+          //   : '-',
+          // 'interestRatePerSec' in loan ? formatPercentage(loan.interestRatePerSec.toAprPercent()) : '-',
+          // 'loanInfo' in loan ? formatPercentage(loan.loanInfo.advanceRate.toPercent()) : '-',
+          // loan.status !== 'Created' && 'probabilityOfDefault' in loan.loanInfo
+          //   ? formatPercentage(loan.loanInfo.probabilityOfDefault.toPercent())
+          //   : '-',
+          // loan.status !== 'Created' && 'lossGivenDefault' in loan.loanInfo
+          //   ? formatPercentage(loan.loanInfo.lossGivenDefault.toPercent())
+          //   : '-',
+          // loan.status !== 'Created' && 'discountRate' in loan.loanInfo
+          //   ? formatPercentage(loan.loanInfo.discountRate.toPercent())
+          //   : '-',
+          formatBalance(loan.pricing.value.toDecimal()),
+          'outstandingDebt' in loan ? formatBalance(loan.outstandingDebt.toDecimal()) : '-',
+          'totalBorrowed' in loan ? formatBalance(loan.totalBorrowed.toDecimal()) : '-',
+          'totalRepaid' in loan ? formatBalance(loan.totalRepaid.toDecimal()) : '-',
+          'originationDate' in loan ? formatDate(loan.originationDate) : '-',
+          formatDate(loan.pricing.maturityDate),
+          formatPercentage(loan.pricing.interestRate.toPercent()),
+          formatPercentage(loan.pricing.advanceRate.toPercent()),
+          loan.pricing.probabilityOfDefault ? formatPercentage(loan.pricing.probabilityOfDefault.toPercent()) : '-',
+          loan.pricing.lossGivenDefault ? formatPercentage(loan.pricing.lossGivenDefault.toPercent()) : '-',
+          loan.pricing.discountRate ? formatPercentage(loan.pricing.discountRate.toPercent()) : '-',
           // formatDate(loan.maturityDate.toString()),
         ],
         heading: false,
--- centrifuge-app/src/pages/Pool/Header.tsx
+++ centrifuge-app/src/pages/Pool/Header.tsx
@@ -69,7 +69,8 @@ export const PoolDetailHeader: React.FC<Props> = ({ actions }) => {
           <NavigationTabsItem to={`${basePath}/${pid}`}>Overview</NavigationTabsItem>
           <NavigationTabsItem to={`${basePath}/${pid}/assets`}>Assets</NavigationTabsItem>
           {!isTinlakePool && <NavigationTabsItem to={`${basePath}/${pid}/liquidity`}>Liquidity</NavigationTabsItem>}
-          {poolReporting && <NavigationTabsItem to={`${basePath}/${pid}/reporting`}>Reporting</NavigationTabsItem>}
+          {/* {poolReporting && <NavigationTabsItem to={`${basePath}/${pid}/reporting`}>Reporting</NavigationTabsItem>} */}
+          <NavigationTabsItem to={`${basePath}/${pid}/reporting`}>Reporting</NavigationTabsItem>
         </NavigationTabs>
       </Shelf>
     </PageHeader>
