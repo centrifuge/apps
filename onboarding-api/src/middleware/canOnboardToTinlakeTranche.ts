803bda53bc534a077017518c66e58181822e9a78
JP Angelle <jp@k-f.co>
2023-05-18 14:56:36 -0500

    fixes

--- onboarding-api/src/middleware/canOnboardToTinlakeTranche.ts
+++ onboarding-api/src/middleware/canOnboardToTinlakeTranche.ts
@@ -2,9 +2,10 @@ import { getPoolById } from '../utils/getPoolById'
 import { HttpError } from '../utils/httpError'
 
 export const canOnboardToTinlakeTranche = async (req, res, next) => {
+  const poolId = req.query.poolId || req.body.poolId
   const trancheId = req.query.trancheId || req.body.trancheId
 
-  if (trancheId?.startsWith('0x')) {
+  if (poolId?.startsWith('0x')) {
     const { metadata } = await getPoolById(trancheId.split('-')[0])
 
     const trancheName = trancheId.split('-')[1] === '0' ? 'junior' : 'senior'
