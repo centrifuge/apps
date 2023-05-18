3779f2c2e731671335efe4fcd6a6802d1bc2cdb7
JP Angelle <jp@k-f.co>
2023-05-18 15:35:17 -0500

    restrict countries

---
+++ onboarding-api/src/utils/geographyCodes.ts
@@ -0,0 +1,17 @@
+export const RESTRICTED_COUNTRY_CODES = {
+  by: 'Belarus',
+  cd: 'Congo, Dem. Republic',
+  cu: 'Cuba',
+  iq: 'Iraq',
+  ir: 'Iran',
+  kp: 'North Korea',
+  ly: 'Libya',
+  ni: 'Nicaragua',
+  ru: 'Russian',
+  sd: 'Sudan',
+  so: 'Somalia',
+  sy: 'Syria',
+  ua: 'Ukraine',
+  ve: 'Venezuela',
+  zw: 'Zimbabwe',
+}
