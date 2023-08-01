1c3f53de13bbd247e5db9917944910a29e33ee30
sophian <littlejohn.sophia@gmail.com>
2023-08-01 13:10:17 -0400

    Add supported browser banner

---
+++ centrifuge-app/src/components/SupportedBrowserBanner.tsx
@@ -0,0 +1,34 @@
+import { Banner, Text } from '@centrifuge/fabric'
+import * as React from 'react'
+
+export const SupportedBrowserBanner = () => {
+  const storageKey = 'browser-banner-seen'
+  const isSupported = navigator.userAgent.indexOf('Chrome') > -1 || navigator.userAgent.indexOf('Firefox') > -1
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  React.useEffect(() => {
+    setIsOpen(localStorage.getItem(storageKey) !== 'true')
+  }, [])
+
+  function onClose() {
+    localStorage.setItem(storageKey, 'true')
+    setIsOpen(false)
+  }
+
+  if (isSupported) {
+    return null
+  }
+
+  return (
+    <Banner
+      isOpen={isOpen}
+      onClose={() => onClose()}
+      title={
+        <Text as="h3" color="textInverted" variant="heading5">
+          Change or update your browser for a fully supported experience. Centrifuge App fully supports Chromium web
+          browsers (Brave, Google Chrome or Opera).
+        </Text>
+      }
+    />
+  )
+}
