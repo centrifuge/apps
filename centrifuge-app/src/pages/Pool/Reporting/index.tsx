21bea265026a3bea8b1ac812853dd72aeeff3b75
Hornebom <you@hornebom.com>
2023-08-04 15:55:10 +0200

    refactor: Add Reporting tab to investor view and dry out into PoolReportPage

--- centrifuge-app/src/pages/Pool/Reporting/index.tsx
+++ centrifuge-app/src/pages/Pool/Reporting/index.tsx
@@ -1,53 +1,7 @@
-import { Pool } from '@centrifuge/centrifuge-js'
 import * as React from 'react'
-import { useParams } from 'react-router'
-import { LoadBoundary } from '../../../components/LoadBoundary'
-import { PageWithSideBar } from '../../../components/PageWithSideBar'
-import { ReportComponent } from '../../../components/Report'
-import { ReportContextProvider } from '../../../components/Report/ReportContext'
-import { ReportFilter } from '../../../components/Report/ReportFilter'
-import { Spinner } from '../../../components/Spinner'
-import { usePool } from '../../../utils/usePools'
+import { PoolReportPage } from '../../../components/Report/PoolReportPage'
 import { PoolDetailHeader } from '../Header'
 
-export type GroupBy = 'day' | 'month'
-
-export type Report = 'pool-balance' | 'asset-list' | 'investor-tx' | 'borrower-tx'
-
-const titleByReport: { [key: string]: string } = {
-  'pool-balance': 'Pool balance',
-  'asset-list': 'Asset list',
-  'investor-tx': 'Investor transactions',
-  'borrower-tx': 'Borrower transactions',
-}
-
 export function PoolDetailReportingTab() {
-  const { pid: poolId } = useParams<{ pid: string }>()
-  const pool = usePool(poolId) as Pool
-
-  return (
-    <ReportContextProvider>
-      <PageWithSideBar>
-        <PoolDetailHeader />
-
-        {pool && <ReportFilter pool={pool} />}
-
-        <LoadBoundary>
-          <PoolDetailReporting pool={pool} />
-        </LoadBoundary>
-      </PageWithSideBar>
-    </ReportContextProvider>
-  )
-}
-
-export function PoolDetailReporting({ pool }: { pool: Pool }) {
-  if (!pool) {
-    return <Spinner mt={2} />
-  }
-
-  return (
-    <React.Suspense fallback={<Spinner mt={2} />}>
-      <ReportComponent pool={pool} />
-    </React.Suspense>
-  )
+  return <PoolReportPage header={<PoolDetailHeader />} />
 }
