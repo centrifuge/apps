d5e8ad10f8ca818053420263ba82390e0d63265d
sophian <littlejohn.sophia@gmail.com>
2023-10-27 17:15:34 -0400

    Convert invested token table to new style

--- centrifuge-app/src/components/Portfolio/InvestedTokens.tsx
+++ centrifuge-app/src/components/Portfolio/InvestedTokens.tsx
@@ -1,30 +1,143 @@
-import { useAddress, useBalances } from '@centrifuge/centrifuge-react'
-import { Box, Grid, Stack, Text } from '@centrifuge/fabric'
-import { useMemo, useState } from 'react'
+import { Token, TokenBalance } from '@centrifuge/centrifuge-js'
+import { formatBalance, useAddress, useBalances, useCentrifuge } from '@centrifuge/centrifuge-react'
+import {
+  AnchorButton,
+  Box,
+  Button,
+  Grid,
+  IconExternalLink,
+  IconMinus,
+  IconPlus,
+  Shelf,
+  Stack,
+  Text,
+  Thumbnail,
+} from '@centrifuge/fabric'
+import { useMemo } from 'react'
+import { useTheme } from 'styled-components'
+import { Dec } from '../../utils/Decimal'
 import { useTinlakeBalances } from '../../utils/tinlake/useTinlakeBalances'
-import { useTinlakePools } from '../../utils/tinlake/useTinlakePools'
-import { usePools } from '../../utils/usePools'
-import { FilterButton } from '../FilterButton'
-import { SortChevrons } from '../SortChevrons'
-import { sortTokens } from './sortTokens'
-import { TokenListItem } from './TokenListItem'
+import { usePool, usePoolMetadata, usePools } from '../../utils/usePools'
+import { Column, DataTable, SortableTableHeader } from '../DataTable'
+import { Eththumbnail } from '../EthThumbnail'
 
-export const COLUMN_GAPS = '200px 140px 140px 140px'
-
-export type SortOptions = {
-  sortBy: 'position' | 'market-value'
-  sortDirection: 'asc' | 'desc'
+type Row = {
+  currency: Token['currency']
+  poolId: string
+  trancheId: string
+  marketValue: TokenBalance
+  position: TokenBalance
+  tokenPrice: TokenBalance
+  canInvestRedeem: boolean
 }
+
+const columns: Column[] = [
+  {
+    align: 'left',
+    header: 'Token',
+    cell: ({ currency, poolId }: Row) => {
+      const pool = usePool(poolId, false)
+      const { data: metadata } = usePoolMetadata(pool)
+      const cent = useCentrifuge()
+      const { sizes } = useTheme()
+      const icon = metadata?.pool?.icon?.uri ? cent.metadata.parseMetadataUrl(metadata.pool.icon.uri) : null
+      return (
+        <Grid as="header" gridTemplateColumns={`${sizes.iconMedium}px 1fr`} alignItems="center" gap={2}>
+          <Eththumbnail show={!!poolId.startsWith('0x')}>
+            {icon ? (
+              <Box as="img" src={icon} alt="" height="iconMedium" width="iconMedium" />
+            ) : (
+              <Thumbnail type="pool" label="LP" size="small" />
+            )}
+          </Eththumbnail>
+
+          <Text textOverflow="ellipsis" variant="body3">
+            {currency.name}
+          </Text>
+        </Grid>
+      )
+    },
+    width: '250px',
+  },
+  {
+    align: 'left',
+    header: 'Token price',
+    cell: ({ tokenPrice }: Row) => {
+      return (
+        <Text textOverflow="ellipsis" variant="body3">
+          {formatBalance(tokenPrice.toDecimal() || 1, 'USDT', 3)}
+        </Text>
+      )
+    },
+    width: '150px',
+  },
+  {
+    align: 'right',
+    header: <SortableTableHeader label="Position" />,
+    cell: ({ currency, position }: Row) => {
+      return (
+        <Text textOverflow="ellipsis" variant="body3">
+          {formatBalance(position, currency.symbol)}
+        </Text>
+      )
+    },
+    width: '125px',
+    sortKey: 'position',
+  },
+  {
+    align: 'right',
+    header: <SortableTableHeader label="Market value" />,
+    cell: ({ marketValue }: Row) => {
+      return (
+        <Text textOverflow="ellipsis" variant="body3">
+          {formatBalance(marketValue, 'USDT', 4)}
+        </Text>
+      )
+    },
+    width: '175px',
+    sortKey: 'marketValue',
+  },
+  {
+    align: 'left',
+    header: '', // invest redeem buttons
+    cell: ({ canInvestRedeem, poolId }: Row) => {
+      const isTinlakePool = poolId.startsWith('0x')
+      return (
+        canInvestRedeem && (
+          <Shelf gap={1} justifySelf="end">
+            {isTinlakePool ? (
+              <AnchorButton
+                variant="tertiary"
+                small
+                icon={IconExternalLink}
+                href="https://legacy.tinlake.centrifuge.io/portfolio"
+                target="_blank"
+              >
+                View on Tinlake
+              </AnchorButton>
+            ) : (
+              <>
+                <Button small variant="tertiary" icon={IconMinus}>
+                  Redeem
+                </Button>
+                <Button small variant="tertiary" icon={IconPlus}>
+                  Invest
+                </Button>
+              </>
+            )}
+          </Shelf>
+        )
+      )
+    },
+    width: '300px',
+  },
+]
 
 // TODO: change canInvestRedeem to default to true once the drawer is implemented
 export const InvestedTokens = ({ canInvestRedeem = false }) => {
-  const [sortOptions, setSortOptions] = useState<SortOptions>({ sortBy: 'position', sortDirection: 'desc' })
-
   const address = useAddress()
   const centBalances = useBalances(address)
   const { data: tinlakeBalances } = useTinlakeBalances()
-
-  const { data: tinlakePools } = useTinlakePools()
   const pools = usePools()
 
   const balances = useMemo(() => {
@@ -34,61 +147,27 @@ export const InvestedTokens = ({ canInvestRedeem = false }) => {
     ]
   }, [centBalances, tinlakeBalances])
 
-  const sortedTokens =
-    balances.length && pools && tinlakePools
-      ? sortTokens(
-          balances,
-          {
-            centPools: pools,
-            tinlakePools: tinlakePools.pools,
-          },
-          sortOptions
-        )
-      : []
+  const tableData = balances.map((balance) => {
+    const pool = pools?.find((pool) => pool.id === balance.poolId)
+    const tranche = pool?.tranches.find((tranche) => tranche.id === balance.trancheId)
+    return {
+      currency: balance.currency,
+      poolId: balance.poolId,
+      trancheId: balance.trancheId,
+      position: balance.balance,
+      tokenPrice: tranche?.tokenPrice || Dec(1),
+      marketValue: tranche?.tokenPrice ? balance.balance.toDecimal().mul(tranche?.tokenPrice.toDecimal()) : Dec(0),
+      canInvestRedeem,
+    }
+  })
 
-  const handleSort = (sortOption: SortOptions['sortBy']) => {
-    setSortOptions((prev) => ({
-      sortBy: sortOption,
-      sortDirection: prev.sortBy !== sortOption ? 'desc' : prev.sortDirection === 'asc' ? 'desc' : 'asc',
-    }))
-  }
-
-  return sortedTokens.length ? (
+  return tableData.length ? (
     <Stack as="article" gap={2}>
       <Text as="h2" variant="heading2">
         Portfolio
       </Text>
-
       <Box overflow="auto">
-        <Grid gridTemplateColumns={COLUMN_GAPS} gap={3} alignItems="start" px={2}>
-          <Text as="span" variant="body3">
-            Token
-          </Text>
-
-          <FilterButton forwardedAs="span" variant="body3" onClick={() => handleSort('position')}>
-            Position
-            <SortChevrons
-              sorting={{ isActive: sortOptions.sortBy === 'position', direction: sortOptions.sortDirection }}
-            />
-          </FilterButton>
-
-          <Text as="span" variant="body3">
-            Token price
-          </Text>
-
-          <FilterButton forwardedAs="span" variant="body3" onClick={() => handleSort('market-value')}>
-            Market Value
-            <SortChevrons
-              sorting={{ isActive: sortOptions.sortBy === 'market-value', direction: sortOptions.sortDirection }}
-            />
-          </FilterButton>
-        </Grid>
-
-        <Stack as="ul" role="list" gap={1} py={1}>
-          {balances.map((balance, index) => (
-            <TokenListItem key={index} canInvestRedeem={canInvestRedeem} {...balance} />
-          ))}
-        </Stack>
+        <DataTable columns={columns} data={tableData} />
       </Box>
     </Stack>
   ) : null
