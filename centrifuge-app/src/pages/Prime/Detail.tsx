83d102d9862353e8762ce4681ef588dc2966fd3f
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2023-11-14 10:24:00 +0100

    header

--- centrifuge-app/src/pages/Prime/Detail.tsx
+++ centrifuge-app/src/pages/Prime/Detail.tsx
@@ -1,10 +1,13 @@
 import { useCentrifugeUtils } from '@centrifuge/centrifuge-react'
+import { Box, Shelf, Text } from '@centrifuge/fabric'
 import { useParams } from 'react-router'
 import { LayoutBase } from '../../components/LayoutBase'
 import { BasePadding } from '../../components/LayoutBase/BasePadding'
+import { LayoutSection } from '../../components/LayoutBase/LayoutSection'
 import { AssetAllocation } from '../../components/Portfolio/AssetAllocation'
 import { InvestedTokens } from '../../components/Portfolio/InvestedTokens'
 import { Transactions } from '../../components/Portfolio/Transactions'
+import { RouterTextLink } from '../../components/TextLink'
 import { DAOs } from '../../config'
 
 export default function PrimeDetailPage() {
@@ -26,10 +29,26 @@ function PrimeDetail() {
 
   console.log('centAddress', centAddress)
   return (
-    <BasePadding gap={3}>
-      <InvestedTokens address={centAddress} />
-      <Transactions onlyMostRecent address={centAddress} />
-      <AssetAllocation address={centAddress} />
-    </BasePadding>
+    <>
+      <LayoutSection backgroundColor="backgroundSecondary" alignItems="flex-start" pt={5}>
+        <Text variant="body3">
+          <Text color="textSecondary">
+            <RouterTextLink to="/prime" style={{ textDecoration: 'none' }}>
+              Prime
+            </RouterTextLink>
+          </Text>{' '}
+          / {dao.name} DAO Investments
+        </Text>
+        <Shelf gap={2}>
+          <Box as="img" src={dao.icon} alt={dao.name} width="iconRegular" height="iconRegular" borderRadius="50%" />
+          <Text variant="heading1">{dao.name} DAO Investments</Text>
+        </Shelf>
+      </LayoutSection>
+      <BasePadding gap={3}>
+        <InvestedTokens address={centAddress} />
+        <Transactions onlyMostRecent address={centAddress} />
+        <AssetAllocation address={centAddress} />
+      </BasePadding>
+    </>
   )
 }
