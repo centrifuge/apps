e159171afab68b2e7019b67767f687ad6670e4f1
sophian <littlejohn.sophia@gmail.com>
2023-11-16 17:16:06 -0500

    Attempt to run test in docker on CI

---
+++ tests/start-xvfb.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+SCREEN_WIDTH=${SCREEN_WIDTH:-$SE_SCREEN_WIDTH}
+SCREEN_HEIGHT=${SCREEN_HEIGHT:-$SE_SCREEN_HEIGHT}
+SCREEN_DEPTH=${SCREEN_DEPTH:-$SE_SCREEN_DEPTH}
+SCREEN_DPI=${SCREEN_DPI:-$SE_SCREEN_DPI}
+
+if [ "${START_XVFB:-$SE_START_XVFB}" = true ] ; then
+  export GEOMETRY="${SCREEN_WIDTH}""x""${SCREEN_HEIGHT}""x""${SCREEN_DEPTH}"
+
+  rm -f /tmp/.X*lock
+
+  # Command reference
+  # http://manpages.ubuntu.com/manpages/focal/man1/xvfb-run.1.html
+  # http://manpages.ubuntu.com/manpages/focal/man1/Xvfb.1.html
+  # http://manpages.ubuntu.com/manpages/focal/man1/Xserver.1.html
+  /usr/bin/xvfb-run --server-num=${DISPLAY_NUM} \
+    --listen-tcp \
+    --server-args="-screen 0 ${GEOMETRY} -fbdir /var/tmp -dpi ${SCREEN_DPI} -listen tcp -noreset -ac +extension RANDR" \
+    /usr/bin/fluxbox -display :99.0
+else
+  echo "Xvfb and Fluxbox won't start."
+fi
