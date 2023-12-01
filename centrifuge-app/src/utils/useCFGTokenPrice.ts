98ca33738b702ce24bc044bb80a168a83d7c406b
sophian <littlejohn.sophia@gmail.com>
2023-12-01 17:21:57 -0500

    Add hook to fetch historical token prices

--- centrifuge-app/src/utils/useCFGTokenPrice.ts
+++ centrifuge-app/src/utils/useCFGTokenPrice.ts
@@ -18,3 +18,34 @@ export const useCFGTokenPrice = () => {
   const { data: CFGPrice } = useQuery('wCFGPrice', () => getWCFGPrice())
   return CFGPrice
 }
+
+export const useDailyCFGPrice = () => {
+  return useQuery('dailyCFGPrice', async () => {
+    const currentYear = new Date().getFullYear()
+    const januaryFirst = new Date(currentYear, 0, 1)
+    const unixTimestampJanuaryFirst = Math.floor(januaryFirst.getTime() / 1000)
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query DailyTokenPrice($dateGt: Int!) {
+            tokenDayDatas(where: { token: "0xc221b7e65ffc80de234bbb6667abdd46593d34f0",  date_gt: $dateGt}, first: 365 ) {
+              date
+              priceUSD
+            }
+          }
+        `,
+        variables: {
+          dateGt: unixTimestampJanuaryFirst,
+        },
+      }),
+    }
+
+    const res = await fetch('https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3', options)
+    return res.json()
+  })
+}
