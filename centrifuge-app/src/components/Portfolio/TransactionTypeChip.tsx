d5f3f5bd5af9f921d8e2e064ae1b81ca92a476eb
sophian <littlejohn.sophia@gmail.com>
2023-12-07 13:56:47 -0500

    Add labels to tx chips

--- centrifuge-app/src/components/Portfolio/TransactionTypeChip.tsx
+++ centrifuge-app/src/components/Portfolio/TransactionTypeChip.tsx
@@ -2,82 +2,88 @@ import { BorrowerTransactionType, InvestorTransactionType } from '@centrifuge/ce
 import { StatusChip, StatusChipProps } from '@centrifuge/fabric'
 import * as React from 'react'
 
+type LabelArgs = {
+  poolCurrencySymbol?: string
+  trancheTokenSymbol?: string
+}
+
 type TransactionTypeProps = {
   type: InvestorTransactionType | BorrowerTransactionType
+  labelArgs?: LabelArgs
 }
 
 const states: {
   [Key in InvestorTransactionType | BorrowerTransactionType]: {
-    label: string
+    label: (args: LabelArgs) => string
     status: StatusChipProps['status']
   }
 } = {
   INVEST_ORDER_UPDATE: {
-    label: 'Invest order placed',
+    label: () => 'Investment order updated',
     status: 'default',
   },
   REDEEM_ORDER_UPDATE: {
-    label: 'Redeem order placed',
+    label: () => 'Redemption order updated',
     status: 'default',
   },
   INVEST_ORDER_CANCEL: {
-    label: 'Invest order cancel',
+    label: () => 'Investment order cancelled',
     status: 'default',
   },
   REDEEM_ORDER_CANCEL: {
-    label: 'Redeem order cancel',
+    label: () => 'Redemption order cancelled',
     status: 'default',
   },
   INVEST_EXECUTION: {
-    label: 'Invest executed',
+    label: () => 'Investment executed',
     status: 'ok',
   },
   REDEEM_EXECUTION: {
-    label: 'Redeem executed',
+    label: () => 'Redemption executed',
     status: 'info',
   },
   TRANSFER_IN: {
-    label: 'Transfer in',
-    status: 'default',
+    label: ({ trancheTokenSymbol }) => `Deposited ${trancheTokenSymbol}`,
+    status: 'ok',
   },
   TRANSFER_OUT: {
-    label: 'Transfer out',
-    status: 'default',
+    label: ({ trancheTokenSymbol }) => `Withdrawn ${trancheTokenSymbol}`,
+    status: 'info',
   },
   INVEST_COLLECT: {
-    label: 'Invest collect',
-    status: 'default',
+    label: ({ trancheTokenSymbol }) => `${trancheTokenSymbol} received in wallet`,
+    status: 'ok',
   },
   REDEEM_COLLECT: {
-    label: 'Redeem collect',
-    status: 'default',
+    label: ({ poolCurrencySymbol }) => `${poolCurrencySymbol} received in wallet`,
+    status: 'ok',
   },
   CREATED: {
-    label: 'Created',
+    label: () => 'Created',
     status: 'default',
   },
   BORROWED: {
-    label: 'Borrowed',
+    label: () => 'Borrowed',
     status: 'default',
   },
   REPAID: {
-    label: 'Repaid',
+    label: () => 'Repaid',
     status: 'default',
   },
   CLOSED: {
-    label: 'Closed',
+    label: () => 'Closed',
     status: 'default',
   },
   PRICED: {
-    label: 'Priced',
+    label: () => 'Priced',
     status: 'default',
   },
 }
 
-export function TransactionTypeChip({ type }: TransactionTypeProps) {
+export function TransactionTypeChip({ type, labelArgs }: TransactionTypeProps) {
   if (!states[type]) {
-    return null
+    throw new Error(`${states[type]} is not a valid transaction type}`)
   }
 
-  return <StatusChip status={states[type].status}>{states[type].label}</StatusChip>
+  return <StatusChip status={states[type].status}>{states[type].label(labelArgs ?? {})}</StatusChip>
 }
