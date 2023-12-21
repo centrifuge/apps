1fc0d1d5b1ff2596f305be4bf988597ee6072430
sophian <littlejohn.sophia@gmail.com>
2023-12-21 12:58:51 -0500

    Fix typings for cy commands

--- centrifuge-app/.eslintrc.js
+++ centrifuge-app/.eslintrc.js
@@ -1,8 +1,10 @@
 const tsconfigJson = require('./tsconfig.json')
+const path = require('path')
+const synpressPath = path.join(process.cwd(), '/node_modules/@synthetixio/synpress')
 
 module.exports = {
   ignorePatterns: ['node_modules', 'build', '.env', ...(tsconfigJson.exclude || [])],
-  extends: ['react-app', 'react-app/jest'],
+  extends: ['react-app', 'react-app/jest', `${synpressPath}/.eslintrc.js`],
   parserOptions: {
     sourceType: 'module',
   },
--- centrifuge-app/tests/e2e/specs/connect-wallet.cy.ts
+++ centrifuge-app/tests/e2e/specs/connect-wallet.cy.ts
@@ -1,8 +1,7 @@
 describe('Synpress health check', () => {
   it('Connects a wallet', () => {
     cy.visit('/pools', { failOnStatusCode: false })
-    const connectButton = cy.contains('Connect')
-    connectButton.click()
+    const connectButton = cy.contains('Connect').click()
     cy.get('button').contains('Centrifuge').click()
     cy.get('button').contains('MetaMask').click()
     cy.acceptMetamaskAccess()
