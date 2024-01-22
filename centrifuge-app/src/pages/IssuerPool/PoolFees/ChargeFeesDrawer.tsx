e920744f328ba949b15d5aa43d7fbb99744acd99
sophian <littlejohn.sophia@gmail.com>
2024-01-22 17:28:27 -0500

    Add UI of form to charge fees

--- centrifuge-app/src/pages/IssuerPool/PoolFees/ChargeFeesDrawer.tsx
+++ centrifuge-app/src/pages/IssuerPool/PoolFees/ChargeFeesDrawer.tsx
@@ -1,6 +1,9 @@
-import { Drawer, Shelf, Stack, Text } from '@centrifuge/fabric'
+import { Box, Button, CurrencyInput, Drawer, Shelf, Stack, Text } from '@centrifuge/fabric'
+import Decimal from 'decimal.js-light'
+import { Field, FieldProps, Form, FormikProvider, useFormik } from 'formik'
 import React from 'react'
 import { useLocation } from 'react-router'
+import { ButtonGroup } from '../../../components/ButtonGroup'
 import { LabelValueStack } from '../../../components/LabelValueStack'
 import { CopyToClipboard } from '../../../utils/copyToClipboard'
 import { formatBalanceAbbreviated } from '../../../utils/formatting'
@@ -15,6 +18,18 @@ export const ChargeFeesDrawer = ({ onClose, isOpen }: ChargeFeesProps) => {
   const params = new URLSearchParams(search)
   const chargeType = params.get('charge')
 
+  const form = useFormik<{ amount: Decimal | undefined }>({
+    initialValues: {
+      amount: undefined,
+    },
+    validate(values) {
+      return { amount: null }
+    },
+    onSubmit: (values, actions) => {
+      actions.setSubmitting(false)
+    },
+  })
+
   return (
     <Drawer isOpen={isOpen} onClose={onClose}>
       <Stack gap={3}>
@@ -22,11 +37,42 @@ export const ChargeFeesDrawer = ({ onClose, isOpen }: ChargeFeesProps) => {
           Charge {chargeType} fee
         </Text>
         <Shelf gap={3} alignItems="flex-start" justifyContent="flex-start">
-          <LabelValueStack label="Type" value={chargeType} />
+          <LabelValueStack label="Type" value="Direct charge" />
           <LabelValueStack label="Pending fees" value={formatBalanceAbbreviated(0, 'USD', 2)} />
           <LabelValueStack label={'Limit'} value={'1% of NAV'} />
-          <LabelValueStack label={'Receiving address'} value={<CopyToClipboard address="0x12332...wedsd" />} />
+          <LabelValueStack label={'Receiving address'} value={<CopyToClipboard address="0x123...wdsd" />} />
         </Shelf>
+        <Stack bg="backgroundTertiary" p={2}>
+          <FormikProvider value={form}>
+            <Form>
+              <Stack gap={1} maxWidth="290px">
+                <Field name="amount">
+                  {({ field, meta, form }: FieldProps) => (
+                    <CurrencyInput
+                      label="Amount to charge"
+                      currency="USDT"
+                      {...field}
+                      secondaryLabel="Maximum charge 6,000 USDC (1% of NAV)"
+                    />
+                  )}
+                </Field>
+                <Box bg="backgroundButtonSecondary" p={1} borderRadius="2px">
+                  <Text variant="body3" color="textSecondary">
+                    Charging of fees will be finalized by the issuer of the pool when executing orders
+                  </Text>
+                </Box>
+                <ButtonGroup>
+                  <Button variant="primary" onClick={() => console.log('charge')}>
+                    Charge
+                  </Button>
+                  <Button variant="secondary" onClick={() => console.log('cancel')}>
+                    Cancel
+                  </Button>
+                </ButtonGroup>
+              </Stack>
+            </Form>
+          </FormikProvider>
+        </Stack>
       </Stack>
     </Drawer>
   )
