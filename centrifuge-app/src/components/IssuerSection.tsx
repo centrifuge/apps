e4bbc224037e7c445e21b68ec67563605798d436
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2024-02-06 14:04:33 +0100

    revert pool overview

--- centrifuge-app/src/components/IssuerSection.tsx
+++ centrifuge-app/src/components/IssuerSection.tsx
@@ -2,16 +2,96 @@ import { PoolMetadata } from '@centrifuge/centrifuge-js'
 import { useCentrifuge } from '@centrifuge/centrifuge-react'
 import { Accordion, AnchorButton, Box, Card, Grid, IconExternalLink, Shelf, Stack, Text } from '@centrifuge/fabric'
 import * as React from 'react'
+import styled from 'styled-components'
 import { ExecutiveSummaryDialog } from './Dialogs/ExecutiveSummaryDialog'
 import { LabelValueStack } from './LabelValueStack'
-import { PillButton } from './PillButton'
+import { AnchorPillButton, PillButton } from './PillButton'
 import { AnchorTextLink } from './TextLink'
 
 type IssuerSectionProps = {
   metadata: Partial<PoolMetadata> | undefined
 }
 
+
 export function IssuerSection({ metadata }: IssuerSectionProps) {
+  const cent = useCentrifuge()
+  const [isDialogOpen, setIsDialogOpen] = React.useState(false)
+
+  return (
+    <>
+      <Shelf alignItems="flex-start" gap="3" flexDirection={['column', 'row']}>
+        <Stack gap={2}>
+          <Box>
+            {metadata?.pool?.issuer.logo && (
+              <StyledImage src={cent.metadata.parseMetadataUrl(metadata?.pool?.issuer.logo?.uri)} />
+            )}
+          </Box>
+          <Text variant="body2">{metadata?.pool?.issuer.description}</Text>
+        </Stack>
+        <Stack gap="2">
+          {metadata?.pool?.links.executiveSummary && (
+            <LabelValueStack
+              label="Download"
+              value={
+                <>
+                  <PillButton variant="small" onClick={() => setIsDialogOpen(true)}>
+                    Executive summary
+                  </PillButton>
+                  <ExecutiveSummaryDialog
+                    issuerName={metadata?.pool?.issuer.name}
+                    href={cent.metadata.parseMetadataUrl(metadata?.pool?.links.executiveSummary?.uri)}
+                    open={isDialogOpen}
+                    onClose={() => setIsDialogOpen(false)}
+                  />
+                </>
+              }
+            />
+          )}
+
+          {(metadata?.pool?.links.website || metadata?.pool?.links.forum || metadata?.pool?.issuer.email) && (
+            <LabelValueStack
+              label="Links"
+              value={
+                <Stack gap="4px">
+                  {metadata?.pool?.links.website && (
+                    <Shelf>
+                      <AnchorPillButton variant="small" href={metadata?.pool?.links.website}>
+                        Website
+                      </AnchorPillButton>
+                    </Shelf>
+                  )}
+                  {metadata?.pool?.links.forum && (
+                    <Shelf>
+                      <AnchorPillButton variant="small" href={metadata?.pool?.links.forum}>
+                        Forum
+                      </AnchorPillButton>
+                    </Shelf>
+                  )}
+                  {metadata?.pool?.issuer.email && (
+                    <Shelf>
+                      <AnchorPillButton variant="small" href={`mailto:${metadata?.pool?.issuer.email}`}>
+                        Email
+                      </AnchorPillButton>
+                    </Shelf>
+                  )}
+                </Stack>
+              }
+            />
+          )}
+        </Stack>
+      </Shelf>
+      {!!metadata?.pool?.details?.length && <Accordion items={metadata?.pool?.details} mt={4} />}
+    </>
+  )
+}
+
+const StyledImage = styled.img`
+  min-height: 104px;
+  min-width: 100px;
+  max-height: 104px;
+`
+
+export function IssuerSectionNew({ metadata }: IssuerSectionProps) {
   const cent = useCentrifuge()
 
   const report = metadata?.pool?.reports?.[0]
