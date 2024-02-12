486e9f1ef0b50872c3de63962bc0d2096d6818f7
JP <jp@k-f.co>
2024-02-12 10:43:46 -0600

    pool detail page layout redesign (#1949)

---
+++ centrifuge-app/src/components/PoolOverview/PoolAnalysis.tsx
@@ -0,0 +1,9 @@
+import { Card } from '@centrifuge/fabric'
+
+export const PoolAnalysis = () => {
+  return (
+    <Card width="100%" height="315px" backgroundColor="backgroundAccentSecondary">
+      Pool Analysis
+    </Card>
+  )
+}
