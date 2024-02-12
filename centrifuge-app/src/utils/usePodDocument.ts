e93667a82fb45ccc67ef09ba35ec5663aab4a804
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2024-02-12 12:43:40 +0100

    add pod read access

--- centrifuge-app/src/utils/usePodDocument.ts
+++ centrifuge-app/src/utils/usePodDocument.ts
@@ -3,14 +3,14 @@ import { useQuery } from 'react-query'
 import { usePodAuth } from './usePodAuth'
 import { usePodUrl } from './usePools'
 
-export function usePodDocument(poolId: string, documentId: string | undefined) {
+export function usePodDocument(poolId: string, loanId: string, documentId: string | undefined) {
   const cent = useCentrifuge()
   const podUrl = usePodUrl(poolId)
-  const { token } = usePodAuth(poolId)
+  const { token, account } = usePodAuth(poolId)
 
   const query = useQuery(
     ['podDocument', podUrl, documentId, token],
-    () => cent.pod.getCommittedDocument([podUrl!, token, documentId!]),
+    () => account.proxies?.length === 1 ? cent.pod.getCommittedDocument([podUrl!, token, documentId!]) : cent.pod.getInvestorAccess([podUrl!, token, documentId!, poolId!, loanId!]),
     {
       enabled: !!podUrl && !!documentId && !!token,
     }
