43b90a1834aaf647b2025d3d74878a502546d12b
JP <jp@k-f.co>
2024-02-21 11:35:43 -0600

    feat: pool performance chart (#1969)
    * feat: pool performance chart
    
    * address pr feedback
    
    * hide sections for tinlake pools
    
    * add interval logic
    
    * hide nav change
    
    * address pr feedback
    
    * revert env
    
    * remove unnecessary width and height

--- centrifuge-app/src/components/PoolOverview/PoolPerfomance.tsx
+++ centrifuge-app/src/components/PoolOverview/PoolPerfomance.tsx
@@ -1,9 +1,15 @@
-import { Card } from '@centrifuge/fabric'
+import { Card, Stack, Text } from '@centrifuge/fabric'
+import PoolPerformanceChart from '../Charts/PoolPerformanceChart'
 
 export const PoolPerformance = () => {
   return (
-    <Card width="100%" height="100%">
-      Pool Performance
+    <Card p={3}>
+      <Stack gap={2}>
+        <Text fontSize="18px" fontWeight="500">
+          Pool performance
+        </Text>
+        <PoolPerformanceChart />
+      </Stack>
     </Card>
   )
 }
