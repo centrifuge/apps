15821aefcddbe10eb384739d474923b60a2b8980
JP Angelle <jp@k-f.co>
2024-02-21 11:33:16 -0600

    remove unnecessary width and height

--- centrifuge-app/src/components/PoolOverview/PoolPerfomance.tsx
+++ centrifuge-app/src/components/PoolOverview/PoolPerfomance.tsx
@@ -3,7 +3,7 @@ import PoolPerformanceChart from '../Charts/PoolPerformanceChart'
 
 export const PoolPerformance = () => {
   return (
-    <Card width="100%" height="100%" p={3}>
+    <Card p={3}>
       <Stack gap={2}>
         <Text fontSize="18px" fontWeight="500">
           Pool performance
