29b4dc46ba757a1eaa00b5f18f720912dc3686f4
JP Angelle <jp@k-f.co>
2024-02-26 23:20:54 -0600

    delete pool analysis

--- centrifuge-app/src/components/PoolOverview/PoolAnalysis.tsx
+++
@@ -1,9 +0,0 @@
-import { Card } from '@centrifuge/fabric'
-
-export const PoolAnalysis = () => {
-  return (
-    <Card width="100%" height="315px" backgroundColor="backgroundAccentSecondary">
-      Pool Analysis
-    </Card>
-  )
-}
