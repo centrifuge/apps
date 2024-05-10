186f240249ca9dc86e81855704a75062eb0812e0
sophian <littlejohn.sophia@gmail.com>
2024-05-10 11:01:51 -0400

    Add rest of calcs

--- centrifuge-app/src/pages/NavManagement/Overview.tsx
+++ centrifuge-app/src/pages/NavManagement/Overview.tsx
@@ -3,14 +3,11 @@ import { LayoutBase } from '../../components/LayoutBase'
 import { PageSummary } from '../../components/PageSummary'
 import { Tooltips } from '../../components/Tooltips'
 import { formatBalance } from '../../utils/formatting'
-import { useInvestorTransactions, usePool, usePools } from '../../utils/usePools'
+import { useDailyPoolStates, useInvestorTransactions, usePool } from '../../utils/usePools'
 
-import { CurrencyBalance, addressToHex } from '@centrifuge/centrifuge-js'
-import { useAddress, useCentrifugeApi, useCentrifugeQuery } from '@centrifuge/centrifuge-react'
+import { CurrencyBalance } from '@centrifuge/centrifuge-js'
 import { Box, Divider, IconClockForward, Shelf, Stack, Text } from '@centrifuge/fabric'
 import { BN } from 'bn.js'
-import { map } from 'rxjs'
-import { isSubstrateAddress } from '../../utils/address'
 import { NavManagementAssetTable } from './NavManagementAssetTable'
 import { NavManagementHeader } from './NavManagementHeader'
 
@@ -27,7 +24,7 @@ export default function NavManagementOverviewPage() {
 }
 
 export const NavManagementPageSummary = ({ poolId }: { poolId: string }) => {
-  const pool = usePool(poolId, false)
+  const pool = usePool(poolId)
   const investorTransactions = useInvestorTransactions(poolId)
   const investments =
     pool &&
@@ -35,12 +32,18 @@ export const NavManagementPageSummary = ({ poolId }: { poolId: string }) => {
       ?.filter((t) => t.type === 'INVEST_COLLECT')
       .reduce((acc, tx) => (tx.currencyAmount ? acc.add(tx.currencyAmount) : new BN(0)), new BN(0))
 
+  const redemptions =
+    pool &&
+    investorTransactions
+      ?.filter((t) => t.type === 'REDEEM_COLLECT')
+      .reduce((acc, tx) => (tx.currencyAmount ? acc.add(tx.currencyAmount) : new BN(0)), new BN(0))
+
   return (
     <PageSummary
       data={[
         {
           label: <Tooltips type="totalNav" />,
-          value: formatBalance(pool?.nav.total ?? 0, pool?.currency.symbol),
+          value: formatBalance(pool?.nav.total ?? 0, pool?.currency.symbol, 2),
         },
         {
           label: 'Investments',
@@ -52,70 +55,33 @@ export const NavManagementPageSummary = ({ poolId }: { poolId: string }) => {
         },
         {
           label: 'Redemptions',
-          value: formatBalance(pool?.nav.total ?? 0, pool?.currency.symbol),
+          value: formatBalance(new CurrencyBalance(redemptions ?? 0, pool.currency.decimals), pool?.currency.symbol, 2),
         },
       ]}
     />
   )
 }
 
-function usePoolFeeders() {
-  const api = useCentrifugeApi()
-  const [storedInfo] = useCentrifugeQuery(['oracleCollectionInfos'], () =>
-    api.query.oraclePriceCollection.collectionInfo.entries().pipe(
-      map((data) => {
-        const poolsByFeeder: Record<string, string[]> = {}
-        const feedersByPool: Record<string, { minFeeders: number; valueLifetime: number; feeders: string[] }> = {}
-        data.forEach(([keys, value]) => {
-          const poolId = (keys.toHuman() as string[])[0].replace(/\D/g, '')
-          const info = value.toPrimitive() as any
-          const feeders = info.feeders
-            .filter((f: any) => !!f.system.signed)
-            .map((f: any) => addressToHex(f.system.signed)) as string[]
-
-          feeders.forEach((feeder) => {
-            if (poolsByFeeder[feeder]) {
-              poolsByFeeder[feeder].push(poolId)
-            } else {
-              poolsByFeeder[feeder] = [poolId]
-            }
-          })
-
-          feedersByPool[poolId] = {
-            valueLifetime: info.valueLifetime as number,
-            minFeeders: info.minFeeders as number,
-            feeders,
-          }
-        })
-
-        return {
-          poolsByFeeder,
-          feedersByPool,
-        }
-      })
-    )
-  )
-
-  return {
-    poolsByFeeder: storedInfo?.poolsByFeeder ?? {},
-    feedersByPool: storedInfo?.feedersByPool ?? {},
-  }
-}
-
-export function usePoolsForWhichAccountIsFeeder(address?: string) {
-  const defaultAddress = useAddress('substrate')
-  address ??= defaultAddress
-  const { poolsByFeeder } = usePoolFeeders()
-  const poolIds = (address && isSubstrateAddress(address) && poolsByFeeder[address]) || []
-  return usePools()?.filter((p) => poolIds.includes(p.id))
-}
-
 export const NavOverviewCard = ({ poolId }: { poolId: string }) => {
   const pool = usePool(poolId)
+  const today = new Date()
+  today.setHours(0, 0, 0, 0)
+  const { poolStates: dailyPoolStates } = useDailyPoolStates(poolId, new Date(pool.nav.lastUpdated), today) || {}
+
   const pendingFees = new CurrencyBalance(
-    pool?.poolFees?.map((f) => f.amounts.pending).reduce((acc, f) => acc.add(f), new BN(0)) ?? 0,
+    pool?.poolFees?.map((f) => f.amounts.pending).reduce((acc, f) => acc.add(f), new BN(0)) ?? new BN(0),
     pool.currency.decimals
   )
+  const changeInValuation = dailyPoolStates?.length
+    ? new BN(dailyPoolStates[0]?.sumBorrowedAmountByPeriod ?? 0).add(
+        new BN(dailyPoolStates.reverse()[0]?.sumBorrowedAmountByPeriod ?? new BN(0))
+      )
+    : new BN(0)
+
+  const pendingNav =
+    dailyPoolStates && dailyPoolStates?.length
+      ? new BN(dailyPoolStates.reverse()[0].portfolioValuation).add(pool.reserve.total)
+      : new BN(0)
   return (
     <Box>
       <Stack
@@ -124,33 +90,36 @@ export const NavOverviewCard = ({ poolId }: { poolId: string }) => {
         borderRadius="6px"
         maxWidth="444px"
         style={{ background: 'linear-gradient(0deg, #FEFEFE 0%, #FAFAFA 100%)' }}
-        gap={1}
       >
-        <Shelf justifyContent="space-between">
+        <Shelf justifyContent="space-between" my={2}>
           <Text variant="body2" color="textPrimary">
             Current NAV
           </Text>
-          <Text variant="body2">{formatBalance(pool?.nav.total, pool.currency.displayName)}</Text>
+          <Text variant="body2">{formatBalance(pool?.nav.total, pool.currency.displayName, 2)}</Text>
         </Shelf>
         <Divider borderColor="statusInfoBg" />
-        <Shelf justifyContent="space-between">
+        <Shelf justifyContent="space-between" mt={2} mb={1}>
           <Text variant="body2" color="textPrimary">
             Change in asset valuation
           </Text>
           <Text variant="body2" color="statusOk">
-            {formatBalance(pool?.nav.total, pool.currency.displayName)}
+            {formatBalance(
+              changeInValuation ? new CurrencyBalance(changeInValuation, pool.currency.decimals) : 0,
+              pool.currency.displayName,
+              2
+            )}
           </Text>
         </Shelf>
-        <Shelf justifyContent="space-between">
+        <Shelf justifyContent="space-between" mb={2}>
           <Text variant="body2" color="textPrimary">
             Pending fees
           </Text>
           <Text variant="body2" color="statusCritical">
-            -{formatBalance(pendingFees, pool.currency.displayName)}
+            -{formatBalance(pendingFees, pool.currency.displayName, 2)}
           </Text>
         </Shelf>
         <Divider borderColor="statusInfoBg" />
-        <Shelf justifyContent="space-between">
+        <Shelf justifyContent="space-between" my={2}>
           <Shelf gap={1}>
             <IconClockForward color="textSelected" size="iconSmall" />
             <Text variant="body2" color="textSelected">
@@ -158,7 +127,7 @@ export const NavOverviewCard = ({ poolId }: { poolId: string }) => {
             </Text>
           </Shelf>
           <Text variant="body2" color="textSelected">
-            {formatBalance(pool?.nav.total, pool.currency.displayName)}
+            {formatBalance(new CurrencyBalance(pendingNav, pool.currency.decimals), pool.currency.displayName, 2)}
           </Text>
         </Shelf>
       </Stack>
