3dfec3da2286a49433a9b7aa990732b84596f5f6
Onno Visser <visser.onno@gmail.com>
2024-05-22 09:41:28 +0200

    Report updates (#2094)

--- centrifuge-app/src/components/Report/FeeTransactions.tsx
+++ centrifuge-app/src/components/Report/FeeTransactions.tsx
@@ -2,7 +2,7 @@ import { Pool } from '@centrifuge/centrifuge-js'
 import { Text } from '@centrifuge/fabric'
 import * as React from 'react'
 import { formatDate } from '../../utils/date'
-import { formatBalanceAbbreviated } from '../../utils/formatting'
+import { formatBalance } from '../../utils/formatting'
 import { getCSVDownloadUrl } from '../../utils/getCSVDownloadUrl'
 import { useFeeTransactions, usePoolMetadata } from '../../utils/usePools'
 import { DataTable } from '../DataTable'
@@ -12,12 +12,45 @@ import { UserFeedback } from './UserFeedback'
 import type { TableDataRow } from './index'
 import { formatPoolFeeTransactionType } from './utils'
 
+const noop = (v: any) => v
+
 export function FeeTransactions({ pool }: { pool: Pool }) {
-  const { startDate, endDate, setCsvData } = React.useContext(ReportContext)
-  const transactions = useFeeTransactions(pool.id, startDate, endDate)
+  const { startDate, endDate, setCsvData, txType } = React.useContext(ReportContext)
+  const transactions = useFeeTransactions(pool.id, new Date(startDate), new Date(endDate))
   const { data: poolMetadata } = usePoolMetadata(pool)
 
-  const headers = ['Date', 'Fee name', 'Transaction type', `${pool ? `${pool.currency.symbol} amount` : '—'}`]
+  const columnConfig = [
+    {
+      header: 'Date',
+      align: 'left',
+      csvOnly: false,
+      formatter: formatDate,
+    },
+    {
+      header: 'Fee name',
+      align: 'left',
+      csvOnly: false,
+      formatter: noop,
+    },
+    {
+      header: 'Transaction type',
+      align: 'left',
+      csvOnly: false,
+      formatter: noop,
+    },
+    {
+      header: 'Currency ammount',
+      align: 'right',
+      csvOnly: false,
+      formatter: (v: any) => (typeof v === 'number' ? formatBalance(v, pool.currency.symbol, 5) : '-'),
+    },
+    {
+      header: 'Currency',
+      align: 'left',
+      csvOnly: true,
+      formatter: noop,
+    },
+  ]
 
   const data: TableDataRow[] = React.useMemo(() => {
     if (!transactions) {
@@ -26,49 +59,60 @@ export function FeeTransactions({ pool }: { pool: Pool }) {
 
     return transactions
       ?.filter((tx) => tx.type !== 'PROPOSED' && tx.type !== 'ADDED' && tx.type !== 'REMOVED')
+      .filter((tx) => (!txType || txType === 'all' ? true : tx.type === txType))
       .map((tx) => ({
         name: '',
         value: [
-          formatDate(tx.timestamp.toString()),
+          tx.timestamp.toISOString(),
           poolMetadata?.pool?.poolFees?.find((f) => f.id === tx.poolFee.feeId)?.name || '-',
           formatPoolFeeTransactionType(tx.type),
-          tx.amount ? formatBalanceAbbreviated(tx.amount, pool.currency.symbol, 3) : '-',
+          tx.amount?.toFloat() ?? '-',
+          pool.currency.symbol,
         ],
         heading: false,
       }))
-  }, [transactions, pool.currency.symbol])
+  }, [transactions, txType, poolMetadata, pool.currency.symbol])
 
-  const columns = headers.map((col, index) => ({
-    align: 'left',
-    header: col,
-    cell: (row: TableDataRow) => <Text variant="body2">{(row.value as any)[index]}</Text>,
-  }))
+  const columns = columnConfig
+    .map((col, index) => ({
+      align: col.align,
+      header: col.header,
+      cell: (row: TableDataRow) => <Text variant="body3">{col.formatter((row.value as any)[index])}</Text>,
+      csvOnly: col.csvOnly,
+    }))
+    .filter((col) => !col.csvOnly)
 
-  const dataUrl = React.useMemo(() => {
+  React.useEffect(() => {
     if (!data.length) {
       return
     }
 
-    const formatted = data
-      .map(({ value }) => value as string[])
-      .map((values) => Object.fromEntries(headers.map((_, index) => [headers[index], `"${values[index]}"`])))
-
-    return getCSVDownloadUrl(formatted)
-  }, [data])
-
-  React.useEffect(() => {
-    setCsvData(
-      dataUrl
-        ? {
-            dataUrl,
-            fileName: `${pool.id}-fee-transactions-${startDate}-${endDate}.csv`,
-          }
-        : undefined
+    const formatted = data.map(({ value: values }) =>
+      Object.fromEntries(columnConfig.map((col, index) => [col.header, `"${values[index]}"`]))
     )
+    const dataUrl = getCSVDownloadUrl(formatted)
 
-    return () => setCsvData(undefined)
+    setCsvData({
+      dataUrl,
+      fileName: `${pool.id}-fee-transactions-${formatDate(startDate, {
+        weekday: 'short',
+        month: 'short',
+        day: '2-digit',
+        year: 'numeric',
+      }).replaceAll(',', '')}-${formatDate(endDate, {
+        weekday: 'short',
+        month: 'short',
+        day: '2-digit',
+        year: 'numeric',
+      }).replaceAll(',', '')}.csv`,
+    })
+
+    return () => {
+      setCsvData(undefined)
+      URL.revokeObjectURL(dataUrl)
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [dataUrl, startDate, endDate, pool.id])
+  }, [data])
 
   if (!transactions) {
     return <Spinner mt={2} />
