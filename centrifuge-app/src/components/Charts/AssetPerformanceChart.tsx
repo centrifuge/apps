9f4368dda76e391d80e15bae1a727131d13787b4
Jeroen Offerijns <jeroen@offerijns.com>
2024-06-14 20:27:33 +0200

    Add future values

--- centrifuge-app/src/components/Charts/AssetPerformanceChart.tsx
+++ centrifuge-app/src/components/Charts/AssetPerformanceChart.tsx
@@ -1,31 +1,18 @@
-import { Box, Shelf, Stack, Text } from '@centrifuge/fabric'
+import { Shelf, Stack, Text } from '@centrifuge/fabric'
 import * as React from 'react'
 import { CartesianGrid, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
-import styled, { useTheme } from 'styled-components'
+import { useTheme } from 'styled-components'
 import { formatDate } from '../../utils/date'
 import { formatBalance, formatBalanceAbbreviated } from '../../utils/formatting'
 import { useLoan } from '../../utils/useLoans'
 import { useAssetSnapshots } from '../../utils/usePools'
 import { TooltipContainer, TooltipTitle } from './Tooltip'
-import { getRangeNumber } from './utils'
 
 type ChartData = {
   day: Date
-  presentValue: number
+  historic?: number
+  future?: number
 }
-
-const RangeFilterButton = styled(Stack)`
-  &:hover {
-    cursor: pointer;
-  }
-`
-
-const rangeFilters = [
-  { value: '30d', label: '30 days' },
-  { value: '90d', label: '90 days' },
-  { value: 'ytd', label: 'Year to date' },
-  { value: 'all', label: 'All' },
-] as const
 
 interface Props {
   poolId: string
@@ -38,67 +25,52 @@ function AssetPerformanceChart({ poolId, loanId }: Props) {
   const asset = useLoan(poolId, loanId)
   const assetSnapshots = useAssetSnapshots(poolId, loanId)
 
-  const [range, setRange] = React.useState<(typeof rangeFilters)[number]>({ value: 'all', label: 'All' })
-  const rangeNumber = getRangeNumber(range.value, 0) ?? 100
+  const data: ChartData[] = React.useMemo(() => {
+    if (!asset || !assetSnapshots) return []
 
-  const data: ChartData[] = React.useMemo(
-    () =>
-      assetSnapshots
-        ?.filter((day) => {
-          return asset && day.timestamp.getTime() <= new Date(asset?.pricing.maturityDate).getTime()
-        })
-        .map((day) => {
-          const presentValue = day.presentValue?.toDecimal().toNumber() || 0
+    const historic = assetSnapshots
+      .filter((day) => {
+        return asset && day.timestamp.getTime() <= new Date(asset?.pricing.maturityDate).getTime()
+      })
+      .map((day) => {
+        const presentValue = day.presentValue?.toDecimal().toNumber() || 0
 
-          return { day: new Date(day.timestamp), presentValue }
-        }) || [],
-    [assetSnapshots]
-  )
+        return { day: new Date(day.timestamp), historic: presentValue, future: null }
+      })
+
+    const today = new Date()
+    const maturity = new Date(asset.pricing.maturityDate)
+    if (today.getTime() >= maturity.getTime()) return historic
+
+    const days = Math.floor((maturity.getTime() - today.getTime()) / (24 * 60 * 60 * 1000)) + 2
+
+    const priceToday = assetSnapshots[assetSnapshots.length - 1].presentValue?.toFloat()
+    const priceAtMaturity =
+      asset.pricing && 'outstandingQuantity' in asset.pricing
+        ? Number(asset.pricing.outstandingQuantity.toDecimal().mul(asset.pricing.notional.toDecimal()).toString())
+        : null
+
+    if (!priceAtMaturity || !priceToday) return historic
+    const deltaPerDay = (priceAtMaturity - priceToday) / (days - 1)
+
+    return [
+      ...historic,
+      ...Array.from({ length: days }, (_, index) => {
+        const newDate = new Date(today)
+        newDate.setDate(today.getDate() + index)
+        return { day: newDate, historic: null, future: priceToday + deltaPerDay * index }
+      }),
+    ]
+  }, [asset, assetSnapshots])
 
   if (assetSnapshots && assetSnapshots?.length < 1) return <Text variant="body2">No data available</Text>
 
-  const chartData = data.slice(-rangeNumber)
-
-  const getXAxisInterval = () => {
-    if (rangeNumber <= 30) return 5
-    if (rangeNumber > 30 && rangeNumber <= 90) {
-      return 14
-    }
-    if (rangeNumber > 90 && rangeNumber <= 180) {
-      return 30
-    }
-    return 45
-  }
-
   return (
     <Stack gap={2}>
-      <Stack>
-        <Shelf justifyContent="flex-end">
-          {chartData.length > 0 &&
-            rangeFilters.map((rangeFilter, index) => (
-              <React.Fragment key={rangeFilter.label}>
-                <RangeFilterButton gap={1} onClick={() => setRange(rangeFilter)}>
-                  <Text variant="body3" whiteSpace="nowrap">
-                    <Text variant={rangeFilter.value === range.value && 'emphasized'}>{rangeFilter.label}</Text>
-                  </Text>
-                  <Box
-                    width="100%"
-                    backgroundColor={rangeFilter.value === range.value ? '#000000' : '#E0E0E0'}
-                    height="2px"
-                  />
-                </RangeFilterButton>
-                {index !== rangeFilters.length - 1 && (
-                  <Box width="24px" backgroundColor="#E0E0E0" height="2px" alignSelf="flex-end" />
-                )}
-              </React.Fragment>
-            ))}
-        </Shelf>
-      </Stack>
-
       <Shelf gap={4} width="100%" color="textSecondary">
-        {chartData?.length ? (
+        {data?.length ? (
           <ResponsiveContainer width="100%" height="100%" minHeight="200px">
-            <LineChart data={chartData} margin={{ left: -36 }}>
+            <LineChart data={data} margin={{ left: -36 }}>
               <defs>
                 <linearGradient id="colorPoolValue" x1="0" y1="0" x2="0" y2="1">
                   <stop offset="5%" stopColor={chartColor} stopOpacity={0.4} />
@@ -117,7 +89,6 @@ function AssetPerformanceChart({ poolId, loanId }: Props) {
                 }}
                 style={{ fontSize: '10px', fill: theme.colors.textSecondary, letterSpacing: '-0.5px' }}
                 dy={4}
-                interval={getXAxisInterval()}
               />
               <YAxis
                 stroke="none"
@@ -134,7 +105,7 @@ function AssetPerformanceChart({ poolId, loanId }: Props) {
                         <TooltipTitle>{formatDate(payload[0].payload.day)}</TooltipTitle>
                         {payload.map(({ value }, index) => (
                           <Shelf justifyContent="space-between" pl="4px" key={index}>
-                            <Text variant="label2">Value</Text>
+                            <Text variant="label2">{payload[0].payload.historic ? 'Value' : 'Expected value'}</Text>
                             <Text variant="label2">
                               {typeof value === 'number' ? formatBalance(value, 'USD' || '') : '-'}
                             </Text>
@@ -146,7 +117,15 @@ function AssetPerformanceChart({ poolId, loanId }: Props) {
                   return null
                 }}
               />
-              <Line type="monotone" dataKey="presentValue" stroke="#1253FF" strokeWidth={2} dot={false} />
+              <Line type="monotone" dataKey="historic" stroke="#1253FF" strokeWidth={2} dot={false} />
+              <Line
+                type="monotone"
+                dataKey="future"
+                stroke="#c2d3ff"
+                strokeWidth={2}
+                dot={false}
+                strokeDasharray="6 6"
+              />
             </LineChart>
           </ResponsiveContainer>
         ) : (
