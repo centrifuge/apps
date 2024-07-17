cdb7b5f079f7ce90da855807a8e78305338b9a2a
sophian <littlejohn.sophia@gmail.com>
2024-07-17 16:50:50 -0400

    Update charge fee UI

--- centrifuge-app/src/pages/Loan/ChargeFeesFields.tsx
+++ centrifuge-app/src/pages/Loan/ChargeFeesFields.tsx
@@ -1,6 +1,6 @@
 import { CurrencyBalance, Pool } from '@centrifuge/centrifuge-js'
 import { formatBalance, useCentrifuge } from '@centrifuge/centrifuge-react'
-import { CurrencyInput, IconButton, IconMinusCircle, Select, Shelf, Stack, Text } from '@centrifuge/fabric'
+import { Box, Button, CurrencyInput, IconMinusCircle, IconPlusCircle, Select, Shelf, Stack } from '@centrifuge/fabric'
 import { Field, FieldArray, FieldProps, useFormikContext } from 'formik'
 import { combineLatest, of } from 'rxjs'
 import { Dec } from '../../utils/Decimal'
@@ -20,80 +20,97 @@ export const ChargeFeesFields = ({ pool }: { pool: Pool }) => {
       <FieldArray name="fees">
         {({ remove, push }) => {
           return (
-            <Stack gap={2}>
-              <Select
-                options={[
-                  { label: 'Select fees', value: '' },
-                  ...(chargableFees || []).map((fee) => {
-                    const feeName =
-                      poolMetadata?.pool?.poolFees?.find((feeMeta) => feeMeta.id === fee.id)?.name || 'Unknown Fee'
-                    return {
-                      label: `${feeName} - ${fee.amounts.percentOfNav.toPercent().toString()}%`,
-                      value: fee.id.toString(),
-                    }
-                  }),
-                ]}
-                defaultValue={''}
-                label="Fees"
-                onChange={(e) => {
-                  if (e.target.value === '') return
-                  push({ id: e.target.value, amount: '' })
-                }}
-              />
+            <>
               <Stack gap={2}>
-                {form.values.fees.map((fee, index) => {
-                  const poolFee = poolFees?.find((poolFee) => poolFee.id.toString() === fee.id)
-                  const maxCharge = poolFee?.amounts.percentOfNav.toPercent().mul(pool.nav.aum.toDecimal())
-                  return (
-                    <Field
-                      key={`fees.${index}.amount`}
-                      name={`fees.${index}.amount`}
-                      validate={(value: number) => {
-                        let error
-                        if (!value) {
-                          error = 'Enter an amount or remove the fee'
-                        }
-                        if (value && Dec(value).gt(maxCharge || 0)) {
-                          error = `Amount cannot exceed ${formatBalance(maxCharge || 0, pool.currency.symbol)}`
-                        }
-                        return error
-                      }}
+                <Stack gap={2}>
+                  {form.values.fees.map((fee, index) => {
+                    const poolFee = poolFees?.find((poolFee) => poolFee.id.toString() === fee.id)
+                    const maxCharge = poolFee?.amounts.percentOfNav.toPercent().mul(pool.nav.aum.toDecimal())
+                    return (
+                      <Shelf gap={1} alignItems="flex-start">
+                        <Box flex={1}>
+                          <Select
+                            options={[
+                              { label: 'Select Fee', value: '' },
+                              ...(chargableFees || []).map((f) => {
+                                const feeName =
+                                  poolMetadata?.pool?.poolFees?.find((feeMeta) => feeMeta.id === f.id)?.name ||
+                                  'Unknown Fee'
+                                return {
+                                  label: `${feeName} - ${f.amounts.percentOfNav.toPercent().toString()}%`,
+                                  value: f.id.toString(),
+                                  disabled: form.values.fees.some((fee) => fee.id === f.id.toString()),
+                                }
+                              }),
+                            ]}
+                            defaultValue={''}
+                            label="Fees"
+                            onChange={(e) => {
+                              form.setFieldValue(`fees.${index}.id`, e.target.value)
+                            }}
+                          />
+                        </Box>
+                        <Box flex={1}>
+                          <Field
+                            key={`fees.${index}.amount`}
+                            name={`fees.${index}.amount`}
+                            validate={(value: number) => {
+                              let error
+                              if (!value) {
+                                error = 'Enter an amount or remove the fee'
+                              }
+                              if (value && Dec(value).gt(maxCharge || 0)) {
+                                error = `Amount cannot exceed ${formatBalance(maxCharge || 0, pool.currency.symbol)}`
+                              }
+                              return error
+                            }}
+                          >
+                            {({ field, meta }: FieldProps) => {
+                              return (
+                                <CurrencyInput
+                                  {...field}
+                                  label="Amount"
+                                  errorMessage={(meta.touched && meta.error) || undefined}
+                                  currency={pool.currency.symbol}
+                                  placeholder="0"
+                                  onChange={(value) => form.setFieldValue(`fees.${index}.amount`, value)}
+                                  secondaryLabel={`Max ${formatBalance(
+                                    maxCharge || 0,
+                                    pool.currency.symbol
+                                  )} (${formatPercentage(poolFee?.amounts.percentOfNav.toPercent() || 0)} NAV)`}
+                                />
+                              )
+                            }}
+                          </Field>
+                        </Box>
+                        <Box
+                          alignSelf="flex-start"
+                          background="none"
+                          border="none"
+                          as="button"
+                          mt={4}
+                          onClick={() => remove(index)}
+                        >
+                          <IconMinusCircle size="20px" />
+                        </Box>
+                      </Shelf>
+                    )
+                  })}
+                </Stack>
+                {chargableFees?.length && form.values.fees.length < chargableFees.length ? (
+                  <Box>
+                    <Button
+                      icon={<IconPlusCircle size="20px" />}
+                      variant="tertiary"
+                      disabled={!chargableFees?.length}
+                      onClick={() => push({ id: '', amount: '' })}
                     >
-                      {({ field, meta }: FieldProps) => {
-                        return (
-                          <Stack as="label" gap={1} width="100%" htmlFor={`fees.${index}.amount`}>
-                            <Text variant="label2">
-                              {poolMetadata?.pool?.poolFees?.find((f) => f.id.toString() === fee.id)?.name} -{' '}
-                              {poolFees
-                                ?.find((f) => f.id.toString() === fee.id)
-                                ?.amounts.percentOfNav.toPercent()
-                                .toString()}
-                              %
-                            </Text>
-                            <Shelf alignItems="center" gap={2} width="100%" justifyContent="space-between">
-                              <CurrencyInput
-                                {...field}
-                                errorMessage={(meta.touched && meta.error) || undefined}
-                                currency={pool.currency.symbol}
-                                placeholder="Amount"
-                                onChange={(value) => form.setFieldValue(`fees.${index}.amount`, value)}
-                                secondaryLabel={`Maximum charge ${formatBalance(
-                                  maxCharge || 0,
-                                  pool.currency.symbol
-                                )} (${formatPercentage(poolFee?.amounts.percentOfNav.toPercent() || 0)} NAV)`}
-                              />
-                              <IconButton onClick={() => remove(index)} title="Remove fee">
-                                <IconMinusCircle />
-                              </IconButton>
-                            </Shelf>
-                          </Stack>
-                        )
-                      }}
-                    </Field>
-                  )
-                })}
+                      Add fees
+                    </Button>
+                  </Box>
+                ) : null}
               </Stack>
-            </Stack>
+            </>
           )
         }}
       </FieldArray>
