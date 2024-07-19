119a78208b930dafdf5eefd0d1eddfc9d25e59b6
katty barroso <kattybarroso1321@gmail.com>
2024-07-19 09:06:30 -0500

    Remove tokens page and update title

--- centrifuge-app/src/components/PoolsTokensShared.tsx
+++ centrifuge-app/src/components/PoolsTokensShared.tsx
@@ -1,56 +1,32 @@
 import { Stack, Text } from '@centrifuge/fabric'
 import * as React from 'react'
 import { config } from '../config'
-import { useBasePath } from '../utils/useBasePath'
-import { useIsAboveBreakpoint } from '../utils/useIsAboveBreakpoint'
 import { CardTotalValueLocked } from './CardTotalValueLocked'
 import { LayoutSection } from './LayoutBase/LayoutSection'
 import { LoadBoundary } from './LoadBoundary'
-import { MenuSwitch } from './MenuSwitch'
 
 type PoolsTokensSharedProps = {
   title: string
   children: React.ReactNode
 }
 
-export function PoolsTokensShared({ title, children }: PoolsTokensSharedProps) {
-  const basePath = useBasePath()
-  const isMedium = useIsAboveBreakpoint('M')
-
-  const links = [
-    {
-      to: `${basePath}`,
-      label: 'Pools',
-    },
-    {
-      to: `${basePath}/tokens`,
-      label: 'Tokens',
-    },
-  ]
-
-  return (
-    <LayoutSection py={5}>
-      <Stack gap={4}>
-        <Stack>
-          <Text as="h1" variant="heading1">
-            {title}
-          </Text>
-          <Text as="p" variant="heading4">
-            {`Pools and tokens ${config.network === 'centrifuge' ? 'of real-world assets' : ''}`}
-          </Text>
-        </Stack>
-
-        <LoadBoundary>
-          <CardTotalValueLocked />
-        </LoadBoundary>
-
-        {isMedium && (
-          <Stack alignItems="end">
-            <MenuSwitch links={links} />
-          </Stack>
-        )}
-        {children}
+export const PoolsTokensShared = ({ title, children }: PoolsTokensSharedProps) => (
+  <LayoutSection py={5}>
+    <Stack gap={4}>
+      <Stack>
+        <Text as="h1" variant="heading1">
+          {title}
+        </Text>
+        <Text as="p" variant="heading4">
+          {`Pools ${
+            config.network === 'centrifuge' ? 'on Centrifuge let investors earn yield from real-world assets' : ''
+          }`}
+        </Text>
       </Stack>
-    </LayoutSection>
-  )
-}
+      <LoadBoundary>
+        <CardTotalValueLocked />
+      </LoadBoundary>
+      {children}
+    </Stack>
+  </LayoutSection>
+)
