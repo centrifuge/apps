80704673e96111d56776d32fa64d334272c0ba6f
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2024-07-19 17:56:45 +0200

    tinkering

--- libp2p-test/src/index.ts
+++ libp2p-test/src/index.ts
@@ -8,13 +8,25 @@ import { tcp } from '@libp2p/tcp'
 import { webSockets } from '@libp2p/websockets'
 import * as websocketsFilter from '@libp2p/websockets/filters'
 import { multiaddr } from '@multiformats/multiaddr'
+import first from 'it-first'
 import * as lp from 'it-length-prefixed'
 import map from 'it-map'
 import { pipe } from 'it-pipe'
 import { createLibp2p } from 'libp2p'
 // import path from "path";
+import { pushable } from 'it-pushable'
+// import path from "path";
 // import { fileURLToPath } from "url";
-import { CreateDocumentRequest, DataProtocolRequest, DataProtocolResponse } from './data_protocol.v1.js'
+
+import {
+  BeepRequest,
+  CreateDocumentRequest,
+  DataProtocolRequest,
+  DataProtocolResponse,
+  GetDocumentRequest,
+} from './data_protocol.v1.js'
+
+// import { fileURLToPath } from "url";
 // Cosmin: 12D3KooWDnScZfemMmJ5efz9ZkYZ7kW6d8Ezi62ADy57N2VFLUtD
 // JP:     12D3KooWCWV4hXqS28dB7ybxDTAn78MsQmPMfTGi6aTGqffMhfFs
 
@@ -52,6 +64,19 @@ async function run() {
 
   await node.start()
 
+  node.addEventListener('peer:connect', (event) => {
+    console.log('peer connect', event.detail.toString())
+  })
+  node.addEventListener('peer:disconnect', (event) => {
+    console.log('peer disconnect', event.detail.toString())
+  })
+  node.addEventListener('connection:close', (event) => {
+    console.log('connection close', event.detail.id, node.getConnections().length)
+  })
+  node.addEventListener('connection:open', (event) => {
+    console.log('connection open', event.detail.id, node.getConnections().length)
+  })
+
   const ma = multiaddr('/ip4/34.159.117.205/tcp/30333/ws/p2p/12D3KooWMspZo4aMEXWBH4UXm3gfiVkeu1AE68Y2JDdVzU723QPc')
 
   await node.dialProtocol(ma, '/centrifuge/kad')
@@ -61,15 +86,34 @@ async function run() {
   const cosminPeerId = peerIdFromString('12D3KooWDnScZfemMmJ5efz9ZkYZ7kW6d8Ezi62ADy57N2VFLUtD')
 
   const peerInfo = await node.peerRouting.findPeer(cosminPeerId)
+  console.log('peerInfo.id', peerInfo.id, peerInfo)
 
-  console.log(peerInfo)
-
-  // const stream = await node.dialProtocol(peerInfo.multiaddrs[1], '/centrifuge-data-extension/1')
-  const stream = await Promise.any(
-    peerInfo.multiaddrs.map((addr) => node.dialProtocol(addr, '/centrifuge-data-extension/1'))
+  console.log(
+    'components.connectionManager.getDialQueue()',
+    await node.components.connectionManager.dialQueue.calculateMultiaddrs(peerInfo.id)
   )
 
-  console.log('stream', stream)
+  console.log('node', node)
+
+  console.log(
+    'peer addresses',
+    peerInfo.multiaddrs.map((addr) => addr.toString())
+  )
+
+  // const connection = await node.dial(peerInfo.id)
+  // const connection = await node.dial(cosminPeerId)
+  // const connection = await node.dial(peerInfo.id)
+  // const connection = await node.dial(
+  //   (await node.components.connectionManager.dialQueue.calculateMultiaddrs(peerInfo.id)).map((e) => e.multiaddr)
+  // )
+  const connection = await Promise.any(peerInfo.multiaddrs.map((addr) => node.dial(addr)))
+  // const stream = await connection.newStream('/centrifuge-data-extension/1')
+  // const stream = await node.dialProtocol(peerInfo.multiaddrs[1], '/centrifuge-data-extension/1')
+  // const stream = await Promise.any(
+  //   peerInfo.multiaddrs.map((addr) => node.dialProtocol(addr, '/centrifuge-data-extension/1'))
+  // )
+
+  // console.log('stream', stream)
 
   // const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
   // const __dirname = path.dirname(__filename); // get the name of the directory
@@ -100,16 +144,16 @@ async function run() {
   // 	pub data: Vec<u8>,
   // }
 
-  // const message = DataProtocolRequest.create({
-  //   // beepRequest: BeepRequest,
-  //   beepRequest: BeepRequest.create(),
-  // })
-  const encoder = new TextEncoder()
   const message = DataProtocolRequest.create({
+    beepRequest: BeepRequest.create(),
+  })
+
+  const encoder = new TextEncoder()
+  const message2 = DataProtocolRequest.create({
     createDocumentRequest: CreateDocumentRequest.create({
       payload: encoder.encode(
         JSON.stringify({
-          id: 10,
+          id: Math.round(Math.random() * 1000000),
           version: 9,
           pool_id: 10,
           loan_id: 11,
@@ -120,33 +164,118 @@ async function run() {
     }),
   })
 
+  const message3 = DataProtocolRequest.create({
+    getDocumentRequest: GetDocumentRequest.create({
+      documentId: encoder.encode('10'),
+      documentVersion: 9,
+    }),
+  })
+
   // const buffer = DataProtocolRequest.encode(payload).finish();
 
   const buffer = DataProtocolRequest.encode(message).finish()
+  const buffer2 = DataProtocolRequest.encode(message2).finish()
+  const buffer3 = DataProtocolRequest.encode(message3).finish()
+
+  const outgoing = pushable()
+
+  setTimeout(async () => {
+    console.log('push1', buffer)
+    // outgoing.push(buffer)
+    send(connection, message)
+  }, 1000)
+
+  setTimeout(() => {
+    console.log('push2', buffer2)
+    // outgoing.push(buffer2)
+    send(connection, message2)
+  }, 2000)
+
+  setTimeout(async () => {
+    console.log('push3', buffer3)
+    // outgoing.push(buffer3)
+    try {
+      await send(connection, message)
+    } catch (e) {
+      console.error('fail', e)
+    }
+  }, 3000)
 
   // write
-  pipe([buffer], (source) => lp.encode(source), stream.sink)
+  // pipe([buffer], (source) => lp.encode(source), stream.sink)
 
   // read
-  pipe(
-    stream.source,
-    (source) => lp.decode(source),
-    (source) => map(source, (buf) => DataProtocolResponse.decode(buf.subarray())),
-    async (source) => {
-      for await (const msg of source) {
-        console.log('response')
-        console.log(msg)
-        const decoder = new TextDecoder()
-        if (msg.createDocumentResponse?.payload) {
-          try {
-            console.log(JSON.parse(decoder.decode(msg.createDocumentResponse.payload)))
-          } catch (e) {
-            console.error(e)
-          }
-        }
+  // pipe(
+  //   outgoing,
+  //   (source) => lp.encode(source),
+  //   stream,
+  //   (source) => lp.decode(source),
+  //   (source) => map(source, (buf) => DataProtocolResponse.decode(buf.subarray())),
+  //   async (source) => {
+  //     for await (const msg of source) {
+  //       console.log('response')
+  //       console.log(msg)
+  //       const decoder = new TextDecoder()
+  //       if (msg.createDocumentResponse?.payload) {
+  //         try {
+  //           console.log(JSON.parse(decoder.decode(msg.createDocumentResponse.payload)))
+  //         } catch (e) {
+  //           console.error(e)
+  //         }
+  //       }
+  //       if (msg.getDocumentResponse?.payload) {
+  //         try {
+  //           console.log(JSON.parse(decoder.decode(msg.getDocumentResponse.payload)))
+  //         } catch (e) {
+  //           console.error(e)
+  //         }
+  //       }
+  //     }
+  //   }
+  // )
+}
+
+async function send(connection: any, message: DataProtocolRequest) {
+  const stream = await connection.newStream('/centrifuge-data-extension/1')
+  try {
+    const buffer = DataProtocolRequest.encode(message).finish()
+    const msg = await pipe(
+      [buffer],
+      (source) => lp.encode(source),
+      stream,
+      (source) => lp.decode(source),
+      (source) => map(source, (buf) => DataProtocolResponse.decode(buf.subarray())),
+      async (source) => first(source)
+    )
+    if (!msg) return
+    console.log('response')
+    console.log(msg)
+    const decoder = new TextDecoder()
+    if (msg.createDocumentResponse?.payload) {
+      try {
+        console.log(JSON.parse(decoder.decode(msg.createDocumentResponse.payload)))
+      } catch (e) {
+        console.error(e)
       }
     }
-  )
+    if (msg.getDocumentResponse?.payload) {
+      try {
+        console.log(JSON.parse(decoder.decode(msg.getDocumentResponse.payload)))
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  } catch (err: any) {
+    console.error('error while pinging %p', connection.remotePeer, err)
+
+    stream.abort(err)
+
+    throw err
+  } finally {
+    // options.signal?.removeEventListener('abort', onAbort)
+    console.log('close stream')
+    await stream.close()
+  }
 }
 
 run()
