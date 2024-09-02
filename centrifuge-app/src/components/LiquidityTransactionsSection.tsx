e1d7a30575cf4e5af7617f02671aba600683bef4
Onno Visser <visser.onno@gmail.com>
2024-09-02 09:37:43 +0200

    Fix investments chart (#2417)

--- centrifuge-app/src/components/LiquidityTransactionsSection.tsx
+++ centrifuge-app/src/components/LiquidityTransactionsSection.tsx
@@ -7,11 +7,11 @@ import { formatBalance } from '../utils/formatting'
 import { getCSVDownloadUrl } from '../utils/getCSVDownloadUrl'
 import { useDailyPoolStates } from '../utils/usePools'
 import { Legend, LegendProps } from './Charts/Legend'
-import { StackedBarChart, StackedBarChartProps } from './Charts/StackedBarChart'
+import { StackedBarChart } from './Charts/StackedBarChart'
 import { getRangeNumber } from './Charts/utils'
+import { LoadBoundary } from './LoadBoundary'
 import { PageSection } from './PageSection'
 import { TooltipsProps } from './Tooltips'
-import { LoadBoundary } from './LoadBoundary'
 
 const rangeFilters = [
   { value: '30d', label: '30 days' },
@@ -80,86 +80,83 @@ export default function LiquidityTransactionsSection({
   }, [dailyPoolStates, dataKeys, dataNames, pool.currency.decimals, pool.currency.symbol])
 
   const chartData = React.useMemo(() => {
-    return (dailyPoolStates
-      ?.map((entry) => {
-        // subquery data is saved at end of the day
-        // data timestamp is off for 24h
-        const date = new Date(entry.timestamp)
-        date.setDate(date.getDate() - 1)
-        const top = entry[dataKeys[0]]
-          ? new CurrencyBalance(entry[dataKeys[0]]!, pool.currency.decimals).toDecimal().toNumber()
-          : 0
+    return (
+      dailyPoolStates
+        ?.map((entry) => {
+          // subquery data is saved at end of the day
+          // data timestamp is off for 24h
+          const date = new Date(entry.timestamp)
+          date.setDate(date.getDate() - 1)
+          const top = entry[dataKeys[0]]
+            ? new CurrencyBalance(entry[dataKeys[0]]!, pool.currency.decimals).toDecimal().toNumber()
+            : 0
 
-        const bottom = entry[dataKeys[1]]
-          ? new CurrencyBalance(entry[dataKeys[1]]!, pool.currency.decimals).toDecimal().toNumber()
-          : 0
+          const bottom = entry[dataKeys[1]]
+            ? new CurrencyBalance(entry[dataKeys[1]]!, pool.currency.decimals).toDecimal().toNumber()
+            : 0
 
-        if (!top && !bottom) {
-          return undefined
-        }
-        return {
-          xAxis: date.getTime(),
-          top,
-          bottom,
-          date: date.toISOString(),
-        }
-      })
-      .slice(-rangeNumber)
-      .filter(Boolean) || []) as StackedBarChartProps['data']
+          if (!top && !bottom) {
+            return null as never
+          }
+          return {
+            xAxis: date.getTime(),
+            top,
+            bottom,
+            date: date.toISOString(),
+          }
+        })
+        .slice(-rangeNumber)
+        .filter(Boolean) || []
+    )
   }, [dailyPoolStates, dataKeys, pool.currency.decimals, rangeNumber])
 
   const legend: LegendProps['data'] = React.useMemo(() => {
     const topTotal = chartData.map(({ top }) => top).reduce((a, b) => a + b, 0)
     const bottomTotal = chartData.map(({ bottom }) => bottom).reduce((a, b) => a + b, 0)
 
-    return chartData && chartData.length
-      ? [
-          {
-            color: dataColors[0],
-            tooltip: { type: tooltips[0] },
-            body: formatBalance(topTotal, pool.currency.symbol),
-          },
-          {
-            color: dataColors[1],
-            tooltip: { type: tooltips[1] },
-            body: formatBalance(bottomTotal, pool.currency.symbol),
-          },
-        ]
-      : []
+    return [
+      {
+        color: dataColors[0],
+        tooltip: { type: tooltips[0] },
+        body: formatBalance(topTotal, pool.currency.symbol),
+      },
+      {
+        color: dataColors[1],
+        tooltip: { type: tooltips[1] },
+        body: formatBalance(bottomTotal, pool.currency.symbol),
+      },
+    ]
   }, [chartData, dataColors, tooltips, pool.currency.symbol])
 
-  if(!chartData?.length) return null
-
-  return(
-  <LoadBoundary>
-    <PageSection
-      title={title}
-      titleAddition={
-        !!dailyPoolStates &&
-        !!dailyPoolStates.length &&
-        `${formatDate(dailyPoolStates[0].timestamp)} - ${formatDate(
-          dailyPoolStates[dailyPoolStates.length - 1].timestamp
-        )}`
-      }
-      headerRight={
-        !!dataUrl && (
-          <AnchorButton
-            href={dataUrl}
-            download={`Pool-${pool.id}-${dataNames.join('-')}.csv`}
-            variant="secondary"
-            icon={IconDownload}
-            small
-          >
-            Download
-          </AnchorButton>
-        )
-      }
-    >
-      <Shelf bg="backgroundPage" width="100%" gap="2">
-        {!!legend && !!legend.length && <Legend data={legend} />}
-        <Shelf justifyContent="flex-end">
-          {chartData.length > 0 &&
-            rangeFilters.map((rangeFilter, index) => (
+  return (
+    <LoadBoundary>
+      <PageSection
+        title={title}
+        titleAddition={
+          !!dailyPoolStates &&
+          !!dailyPoolStates.length &&
+          `${formatDate(dailyPoolStates[0].timestamp)} - ${formatDate(
+            dailyPoolStates[dailyPoolStates.length - 1].timestamp
+          )}`
+        }
+        headerRight={
+          !!dataUrl && (
+            <AnchorButton
+              href={dataUrl}
+              download={`Pool-${pool.id}-${dataNames.join('-')}.csv`}
+              variant="secondary"
+              icon={IconDownload}
+              small
+            >
+              Download
+            </AnchorButton>
+          )
+        }
+      >
+        <Shelf bg="backgroundPage" width="100%" gap="2">
+          <Legend data={legend} />
+          <Shelf justifyContent="flex-end">
+            {rangeFilters.map((rangeFilter, index) => (
               <React.Fragment key={rangeFilter.label}>
                 <RangeFilterButton gap={1} onClick={() => setRange(rangeFilter)}>
                   <Text variant="body3" whiteSpace="nowrap">
@@ -176,12 +173,11 @@ export default function LiquidityTransactionsSection({
                 )}
               </React.Fragment>
             ))}
+          </Shelf>
         </Shelf>
-      </Shelf>
 
-      {!!chartData && !!chartData.length && (
         <StackedBarChart data={chartData} names={dataNames} colors={dataColors} currency={pool.currency.symbol} />
-      )}
-    </PageSection>
-    </LoadBoundary>)
+      </PageSection>
+    </LoadBoundary>
+  )
 }
