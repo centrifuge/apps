14d6911c6ddbc0fd944e400d8c8a456baccd4994
sophian <littlejohn.sophia@gmail.com>
2024-09-17 16:01:31 -0400

    Fix report url and clean up pre edit view

---
+++ centrifuge-app/src/pages/IssuerCreatePool/PoolRatingInput.tsx
@@ -0,0 +1,17 @@
+import { Grid, TextInput } from '@centrifuge/fabric'
+import { FieldWithErrorMessage } from '../../components/FieldWithErrorMessage'
+
+export function PoolRatingInput() {
+  return (
+    <Grid columns={[1, 2]} equalColumns gap={2} rowGap={3}>
+      <FieldWithErrorMessage name="ratingAgency" as={TextInput} label="Rating agency" placeholder="Agency Name..." />
+      <FieldWithErrorMessage name="ratingValue" as={TextInput} label="Rating" placeholder="Rating value..." />
+      <FieldWithErrorMessage
+        name="ratingReportUrl"
+        as={TextInput}
+        label="Rating report URL"
+        placeholder="https://..."
+      />
+    </Grid>
+  )
+}
