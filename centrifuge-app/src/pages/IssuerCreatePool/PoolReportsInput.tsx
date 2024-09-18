70aa42cb0e3f64161daeee9545f4c08db45d89d1
Sophia <littlejohn.sophia@gmail.com>
2024-09-18 14:10:30 -0400

    Edit pool fields (#2455)
    * Fix report url and clean up pre edit view
    
    * Add pool structure, fix pool name in tranche edit, fix targetAPY,

--- centrifuge-app/src/pages/IssuerCreatePool/PoolReportsInput.tsx
+++ centrifuge-app/src/pages/IssuerCreatePool/PoolReportsInput.tsx
@@ -1,4 +1,4 @@
-import { FileUpload, Grid, TextInput } from '@centrifuge/fabric'
+import { FileUpload, Grid, Stack, Text, TextInput } from '@centrifuge/fabric'
 import { Field, FieldProps } from 'formik'
 import { FieldWithErrorMessage } from '../../components/FieldWithErrorMessage'
 import { combineAsync, imageFile, maxFileSize, maxImageSize } from '../../utils/validation'
@@ -6,48 +6,48 @@ import { validate } from './validate'
 
 export function PoolReportsInput() {
   return (
-    <Grid columns={[1, 2]} equalColumns gap={2} rowGap={3}>
-      <FieldWithErrorMessage
-        name="reportUrl"
-        as={TextInput}
-        label="Report URL"
-        placeholder="https://..."
-        validate={validate.websiteNotRequired}
-      />
-      <FieldWithErrorMessage
-        name="reportAuthorName"
-        as={TextInput}
-        label="Reviewer name"
-        placeholder="Name..."
-        maxLength={100}
-      />
-      <FieldWithErrorMessage
-        name="reportAuthorTitle"
-        as={TextInput}
-        label="Reviewer job title"
-        placeholder="Title..."
-        maxLength={100}
-      />
-      <Field
-        name="reportAuthorAvatar"
-        validate={combineAsync(imageFile(), maxFileSize(1024 ** 2), maxImageSize(200, 200))}
-      >
-        {({ field, meta, form }: FieldProps) => (
-          <FileUpload
-            file={field.value}
-            onFileChange={(file) => {
-              form.setFieldTouched('reportAuthorAvatar', true, false)
-              form.setFieldValue('reportAuthorAvatar', file)
-            }}
-            label="Reviewer avatar (JPG/PNG/SVG, max 40x40px)"
-            errorMessage={meta.touched && meta.error ? meta.error : undefined}
-            accept="image/*"
-          />
-        )}
-      </Field>
-      <FieldWithErrorMessage name="ratingAgency" as={TextInput} label="Rating agency" placeholder="Agency Name..." />
-      <FieldWithErrorMessage name="ratingValue" as={TextInput} label="Rating" placeholder="Rating value..." />
-      <FieldWithErrorMessage name="ratingReport" as={TextInput} label="Rating report URL" placeholder="https://..." />
-    </Grid>
+    <Stack gap={2}>
+      <Text variant="heading2">Pool analysis</Text>
+      <Grid columns={[1, 2]} equalColumns gap={2} rowGap={3}>
+        <FieldWithErrorMessage
+          name="reportUrl"
+          as={TextInput}
+          label="Report URL"
+          placeholder="https://..."
+          validate={validate.websiteNotRequired}
+        />
+        <FieldWithErrorMessage
+          name="reportAuthorName"
+          as={TextInput}
+          label="Reviewer name"
+          placeholder="Name..."
+          maxLength={100}
+        />
+        <FieldWithErrorMessage
+          name="reportAuthorTitle"
+          as={TextInput}
+          label="Reviewer job title"
+          placeholder="Title..."
+          maxLength={100}
+        />
+        <Field
+          name="reportAuthorAvatar"
+          validate={combineAsync(imageFile(), maxFileSize(1024 ** 2), maxImageSize(200, 200))}
+        >
+          {({ field, meta, form }: FieldProps) => (
+            <FileUpload
+              file={field.value}
+              onFileChange={(file) => {
+                form.setFieldTouched('reportAuthorAvatar', true, false)
+                form.setFieldValue('reportAuthorAvatar', file)
+              }}
+              label="Reviewer avatar (JPG/PNG/SVG, max 40x40px)"
+              errorMessage={meta.touched && meta.error ? meta.error : undefined}
+              accept="image/*"
+            />
+          )}
+        </Field>
+      </Grid>
+    </Stack>
   )
 }
