937ba59097f09df865199a08abfbaf9ec20c0724
katty barroso <kattybarroso1321@gmail.com>
2024-09-26 09:39:13 +0200

    Hotfix: remove tvl graph

--- centrifuge-app/src/pages/Pools.tsx
+++ centrifuge-app/src/pages/Pools.tsx
@@ -1,9 +1,7 @@
-import { Box, Stack, Text } from '@centrifuge/fabric'
+import { Stack, Text } from '@centrifuge/fabric'
 import * as React from 'react'
-import { CardTotalValueLocked } from '../components/CardTotalValueLocked'
 import { LayoutBase } from '../components/LayoutBase'
 import { LayoutSection } from '../components/LayoutBase/LayoutSection'
-import { LoadBoundary } from '../components/LoadBoundary'
 import { PoolList } from '../components/PoolList'
 import { prefetchRoute } from '../components/Root'
 import { config } from '../config'
@@ -16,22 +14,15 @@ export default function PoolsPage() {
   return (
     <LayoutBase>
       <LayoutSection py={5}>
-        <Stack gap={4}>
-          <Stack>
-            <Text as="h1" variant="heading1">
-              Pools
-            </Text>
-            <Text as="p" variant="heading4">
-              {`Pools ${
-                config.network === 'centrifuge' ? 'on Centrifuge let investors earn yield from real-world assets' : ''
-              }`}
-            </Text>
-          </Stack>
-          <Box width="50%">
-            <LoadBoundary>
-              <CardTotalValueLocked />
-            </LoadBoundary>
-          </Box>
+        <Stack mb={2}>
+          <Text as="h1" variant="heading1">
+            Pools
+          </Text>
+          <Text as="p" variant="heading4">
+            {`Pools ${
+              config.network === 'centrifuge' ? 'on Centrifuge let investors earn yield from real-world assets' : ''
+            }`}
+          </Text>
         </Stack>
         <PoolList />
       </LayoutSection>
