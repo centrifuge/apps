b1fc5fb4bcf2bed634a90697d57d04eb66e62448
katty barroso <kattybarroso1321@gmail.com>
2024-10-21 12:27:47 +0200

    Adjust filters with new design

--- centrifuge-app/src/components/Report/DataFilter.tsx
+++ centrifuge-app/src/components/Report/DataFilter.tsx
@@ -1,6 +1,6 @@
 import { Loan, Pool } from '@centrifuge/centrifuge-js'
 import { useGetNetworkName } from '@centrifuge/centrifuge-react'
-import { AnchorButton, Box, DateInput, IconDownload, SearchInput, Select, Shelf } from '@centrifuge/fabric'
+import { DateInput, Grid, SearchInput, Select } from '@centrifuge/fabric'
 import * as React from 'react'
 import { useNavigate } from 'react-router'
 import { useIsAboveBreakpoint } from '../../../src/utils/useIsAboveBreakpoint'
@@ -64,7 +64,7 @@ export function DataFilter({ poolId }: ReportFilterProps) {
   ]
 
   return (
-    <Shelf
+    <Grid
       flexWrap="wrap"
       padding={2}
       margin={2}
@@ -74,198 +74,162 @@ export function DataFilter({ poolId }: ReportFilterProps) {
       borderWidth={[0, 1]}
       bg="backgroundSecondary"
       alignItems="flex-start"
+      gridTemplateColumns="repeat(auto-fill, minmax(250px, 1fr))"
+      gap={2}
     >
-      <Box display="flex" justifyContent="space-between" width="100%" flexWrap="wrap" mb={2}>
-        <Box display="flex" maxWidth={isMedium ? '60%' : '100%'}>
-          <Box mr={1}>
-            <Select
-              name="data"
-              label="Data"
-              options={reportOptions}
-              value={report}
-              onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
-                const { value } = event.target
-                if (value) {
-                  navigate(`${basePath}/${pool.id}/data/${value}`)
-                }
-              }}
-            />
-          </Box>
+      <Select
+        name="data"
+        label="Data"
+        options={reportOptions}
+        value={report}
+        onChange={(event: React.ChangeEvent<HTMLSelectElement>) => {
+          const { value } = event.target
+          if (value) {
+            navigate(`${basePath}/${pool.id}/data/${value}`)
+          }
+        }}
+      />
 
-          {['pool-balance', 'token-price'].includes(report) && (
-            <Box mr={1}>
-              <Select
-                name="groupBy"
-                label="Group by"
-                options={[
-                  { label: 'Day', value: 'day' },
-                  { label: 'Month', value: 'month' },
-                ]}
-                value={groupBy}
-                onChange={(event) => {
-                  if (event.target.value) {
-                    setGroupBy(event.target.value as GroupBy)
-                  }
-                }}
-              />
-            </Box>
-          )}
+      {['pool-balance', 'token-price'].includes(report) && (
+        <Select
+          name="groupBy"
+          label="Group by"
+          options={[
+            { label: 'Day', value: 'day' },
+            { label: 'Month', value: 'month' },
+          ]}
+          value={groupBy}
+          onChange={(event) => {
+            if (event.target.value) {
+              setGroupBy(event.target.value as GroupBy)
+            }
+          }}
+        />
+      )}
 
-          {report === 'asset-list' && (
-            <Box mr={1}>
-              <Select
-                name="loanStatus"
-                label="Status"
-                options={[
+      {report === 'asset-list' && (
+        <Select
+          name="loanStatus"
+          label="Status"
+          options={[
+            { label: 'All', value: 'all' },
+            { label: 'Ongoing', value: 'ongoing' },
+            { label: 'Repaid', value: 'repaid' },
+            { label: 'Overdue', value: 'overdue' },
+          ]}
+          value={loanStatus}
+          onChange={(event) => setLoanStatus(event.target.value)}
+        />
+      )}
+
+      {(report === 'investor-list' || report === 'investor-tx') && (
+        <Select
+          name="activeTranche"
+          label="Token"
+          options={[
+            { label: 'All tokens', value: 'all' },
+            ...pool.tranches.map((token) => ({
+              label: token.currency.name,
+              value: token.id,
+            })),
+          ]}
+          value={activeTranche}
+          onChange={(event) => {
+            if (event.target.value) {
+              setActiveTranche(event.target.value)
+            }
+          }}
+        />
+      )}
+
+      {report === 'asset-tx' && (
+        <Select
+          name="loan"
+          label="Asset"
+          onChange={(event) => setLoan(event.target.value)}
+          value={loan}
+          options={[
+            { label: 'All', value: 'all' },
+            ...(loans?.map((l) => ({
+              value: l.id,
+              label: <LoanOption loan={l as Loan} key={l.id} />,
+            })) ?? []),
+          ]}
+        />
+      )}
+
+      {['investor-tx', 'asset-tx', 'fee-tx'].includes(report) && (
+        <Select
+          name="txType"
+          label="Transaction type"
+          options={
+            report === 'investor-tx'
+              ? [
                   { label: 'All', value: 'all' },
-                  { label: 'Ongoing', value: 'ongoing' },
-                  { label: 'Repaid', value: 'repaid' },
-                  { label: 'Overdue', value: 'overdue' },
-                ]}
-                value={loanStatus}
-                onChange={(event) => setLoanStatus(event.target.value)}
-              />
-            </Box>
-          )}
-
-          {(report === 'investor-list' || report === 'investor-tx') && (
-            <Box mr={1}>
-              <Select
-                name="activeTranche"
-                label="Token"
-                options={[
-                  { label: 'All tokens', value: 'all' },
-                  ...pool.tranches.map((token) => ({
-                    label: token.currency.name,
-                    value: token.id,
-                  })),
-                ]}
-                value={activeTranche}
-                onChange={(event) => {
-                  if (event.target.value) {
-                    setActiveTranche(event.target.value)
-                  }
-                }}
-              />
-            </Box>
-          )}
-
-          {report === 'asset-tx' && (
-            <Box mr={1}>
-              <Select
-                name="loan"
-                label="Asset"
-                onChange={(event) => setLoan(event.target.value)}
-                value={loan}
-                options={[
+                  { label: 'Submitted orders', value: 'orders' },
+                  { label: 'Executed orders', value: 'executions' },
+                  { label: 'Transfers', value: 'transfers' },
+                ]
+              : report === 'asset-tx'
+              ? [
                   { label: 'All', value: 'all' },
-                  ...(loans?.map((l) => ({
-                    value: l.id,
-                    label: <LoanOption loan={l as Loan} key={l.id} />,
-                  })) ?? []),
-                ]}
-              />
-            </Box>
-          )}
-
-          {['investor-tx', 'asset-tx', 'fee-tx'].includes(report) && (
-            <Box mr={1}>
-              <Select
-                name="txType"
-                label="Transaction type"
-                options={
-                  report === 'investor-tx'
-                    ? [
-                        { label: 'All', value: 'all' },
-                        { label: 'Submitted orders', value: 'orders' },
-                        { label: 'Executed orders', value: 'executions' },
-                        { label: 'Transfers', value: 'transfers' },
-                      ]
-                    : report === 'asset-tx'
-                    ? [
-                        { label: 'All', value: 'all' },
-                        { label: 'Created', value: 'Created' },
-                        { label: 'Financed', value: 'Financed' },
-                        { label: 'Repaid', value: 'Repaid' },
-                        { label: 'Priced', value: 'Priced' },
-                        { label: 'Closed', value: 'Closed' },
-                      ]
-                    : [
-                        { label: 'All', value: 'all' },
-                        { label: formatPoolFeeTransactionType('CHARGED'), value: 'CHARGED' },
-                        { label: formatPoolFeeTransactionType('UNCHARGED'), value: 'UNCHARGED' },
-                        { label: formatPoolFeeTransactionType('ACCRUED'), value: 'ACCRUED' },
-                        { label: formatPoolFeeTransactionType('PAID'), value: 'PAID' },
-                      ]
-                }
-                value={txType}
-                onChange={(event) => {
-                  if (event.target.value) {
-                    setTxType(event.target.value)
-                  }
-                }}
-              />
-            </Box>
-          )}
-
-          {['investor-tx', 'investor-list'].includes(report) && (
-            <>
-              <Box mr={1}>
-                <Select
-                  name="network"
-                  label="Network"
-                  options={[
-                    { label: 'All', value: 'all' },
-                    { label: 'Centrifuge', value: 'centrifuge' },
-                    ...(domains ?? []).map((domain) => ({
-                      label: getNetworkName(domain.chainId),
-                      value: String(domain.chainId),
-                    })),
-                  ]}
-                  value={network}
-                  onChange={(e) => {
-                    const { value } = e.target
-                    if (value) {
-                      setNetwork(isNaN(Number(value)) ? value : Number(value))
-                    }
-                  }}
-                />
-              </Box>
-            </>
-          )}
-        </Box>
-
-        <Box display="flex" alignItems="center">
-          <Box mr={1}>
-            <DateInput label="From" value={startDate} max={endDate} onChange={(e) => setStartDate(e.target.value)} />
-          </Box>
-          <DateInput label="To" value={endDate} min={startDate} onChange={(e) => setEndDate(e.target.value)} />
-
-          <AnchorButton
-            disabled={!csvData}
-            download={csvData?.fileName}
-            href={csvData?.dataUrl}
-            icon={<IconDownload size={20} />}
-            small
-            variant="inverted"
-            style={{ marginLeft: '12px', marginTop: '22px' }}
-          >
-            Download
-          </AnchorButton>
-        </Box>
-      </Box>
+                  { label: 'Created', value: 'Created' },
+                  { label: 'Financed', value: 'Financed' },
+                  { label: 'Repaid', value: 'Repaid' },
+                  { label: 'Priced', value: 'Priced' },
+                  { label: 'Closed', value: 'Closed' },
+                ]
+              : [
+                  { label: 'All', value: 'all' },
+                  { label: formatPoolFeeTransactionType('CHARGED'), value: 'CHARGED' },
+                  { label: formatPoolFeeTransactionType('UNCHARGED'), value: 'UNCHARGED' },
+                  { label: formatPoolFeeTransactionType('ACCRUED'), value: 'ACCRUED' },
+                  { label: formatPoolFeeTransactionType('PAID'), value: 'PAID' },
+                ]
+          }
+          value={txType}
+          onChange={(event) => {
+            if (event.target.value) {
+              setTxType(event.target.value)
+            }
+          }}
+        />
+      )}
 
       {['investor-tx', 'investor-list'].includes(report) && (
-        <Box width="30%" mb={2}>
-          <SearchInput
-            name="address"
-            placeholder="Filter by address..."
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-          />
-        </Box>
+        <Select
+          name="network"
+          label="Network"
+          options={[
+            { label: 'All', value: 'all' },
+            { label: 'Centrifuge', value: 'centrifuge' },
+            ...(domains ?? []).map((domain) => ({
+              label: getNetworkName(domain.chainId),
+              value: String(domain.chainId),
+            })),
+          ]}
+          value={network}
+          onChange={(e) => {
+            const { value } = e.target
+            if (value) {
+              setNetwork(isNaN(Number(value)) ? value : Number(value))
+            }
+          }}
+        />
       )}
-    </Shelf>
+      {['investor-tx', 'investor-list'].includes(report) && (
+        <SearchInput
+          name="address"
+          label="Address"
+          placeholder="Filter by address..."
+          value={address}
+          onChange={(e) => setAddress(e.target.value)}
+        />
+      )}
+
+      <DateInput label="From" value={startDate} max={endDate} onChange={(e) => setStartDate(e.target.value)} />
+      <DateInput label="To" value={endDate} min={startDate} onChange={(e) => setEndDate(e.target.value)} />
+    </Grid>
   )
 }
 
@@ -277,4 +241,18 @@ function LoanOption({ loan }: { loan: Loan }) {
       {loan.id} - {metadata?.name}
     </option>
   )
+}
+
+{
+  /* <AnchorButton
+disabled={!csvData}
+download={csvData?.fileName}
+href={csvData?.dataUrl}
+icon={<IconDownload size={20} />}
+small
+variant="inverted"
+style={{ marginLeft: '12px', marginTop: '22px' }}
+>
+Download
+</AnchorButton> */
 }
