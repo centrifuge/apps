5e8ca4abb355dbd7e7c34ca1c0a9fc6a8097da50
katty barroso <kattybarroso1321@gmail.com>
2024-10-28 11:47:09 +0100

    Add new package

--- sdk-consumer/src/App.tsx
+++ sdk-consumer/src/App.tsx
@@ -1,46 +1,25 @@
-import Centrifuge from '@centrifuge/centrifuge-sdk'
-import { useEffect, useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import { WagmiProvider, useAccount } from 'wagmi'
+import { Account } from './components/account'
+import { WalletOptions } from './components/wallet-options'
+import { wagmiConfig } from './config/wagmiConfig'
+
+const queryClient = new QueryClient()
+
+function ConnectWallet() {
+  const { isConnected } = useAccount()
+  if (isConnected) return <Account />
+  return <WalletOptions />
+}
 
 function App() {
-  const [balance, setBalance] = useState(null)
-
-  useEffect(() => {
-    const fetchBalance = async () => {
-      const centrifuge = new Centrifuge({
-        environment: 'demo',
-      })
-
-      const address = '0x423420Ae467df6e90291fd0252c0A8a637C1e03f'
-      const chainId = 11155111
-
-      try {
-        const accountQuery = centrifuge.account(address, chainId)
-
-        accountQuery.subscribe({
-          next: (account) => {
-            account.balances().subscribe({
-              next: (balanceValue) => {
-                console.log('Account Balance:', balanceValue)
-                setBalance(balanceValue.toString())
-              },
-              error: (error) => console.error('Error fetching balance:', error),
-            })
-          },
-          error: (error) => console.error('Error retrieving account:', error),
-        })
-      } catch (error) {
-        console.error('General Error:', error)
-      }
-    }
-
-    fetchBalance()
-  }, [])
-
   return (
-    <div>
-      <h1>Account Balance</h1>
-      {balance ? <p>{balance} TUSD</p> : <p>Loading balance...</p>}
-    </div>
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <ConnectWallet />
+      </QueryClientProvider>
+    </WagmiProvider>
   )
 }
 
