88c3425ae6a032663bd970fd02fe34b76c414845
katty barroso <kattybarroso1321@gmail.com>
2024-11-21 10:55:39 +0100

    Add pool details section

---
+++ centrifuge-app/src/pages/IssuerCreatePool/IssuerCategories.tsx
@@ -0,0 +1,76 @@
+import { PoolMetadataInput } from '@centrifuge/centrifuge-js'
+import { Box, IconButton, IconTrash, Select, Text, TextInput } from '@centrifuge/fabric'
+import { Field, FieldArray, FieldProps, useFormikContext } from 'formik'
+import { AddButton } from './PoolDetailsSection'
+import { StyledGrid } from './PoolStructureSection'
+
+const PROVIDERS = [
+  { label: 'Fund admin', value: 'fundAdmin' },
+  { label: 'Trustee', value: 'trustee' },
+  { label: 'Pricing oracle provider', value: 'pricingOracleProvider' },
+  { label: 'Auditor', value: 'auditor' },
+  { label: 'Custodian', value: 'custodian' },
+  { label: 'Investment manager', value: 'Investment manager' },
+  { label: 'Sub-advisor', value: 'subadvisor' },
+  { label: 'Historical default rate', value: 'historicalDefaultRate' },
+  { label: 'Other', value: 'other' },
+]
+
+const LabelWithDeleteButton = ({ onDelete }: { onDelete: () => void }) => {
+  return (
+    <Box display="flex" justifyContent="space-between" alignItems="center">
+      <Text variant="heading4">Name of provider</Text>
+      <IconButton onClick={onDelete}>
+        <IconTrash color="textSecondary" />
+      </IconButton>
+    </Box>
+  )
+}
+
+export const IssuerCategoriesSection = () => {
+  const form = useFormikContext<PoolMetadataInput>()
+  return (
+    <Box mt={4} mb={3}>
+      <Text variant="heading2">Service providers</Text>
+      <StyledGrid gridTemplateColumns={['1fr', '1fr 1fr']} mt={3}>
+        <FieldArray name="issuerCategories">
+          {({ push, remove }) => (
+            <>
+              {form.values.issuerCategories.map((_, index) => (
+                <>
+                  <Field name={`issuerCategories.${index}.type`}>
+                    {({ field, meta }: FieldProps) => (
+                      <Select
+                        name={field.name}
+                        label="Type"
+                        onChange={(event) => form.setFieldValue(field.name, event.target.value)}
+                        onBlur={field.onBlur}
+                        value={field.value}
+                        options={PROVIDERS}
+                        placeholder="Please select..."
+                      />
+                    )}
+                  </Field>
+
+                  <Field name={`issuerCategories.${index}.value`}>
+                    {({ field, meta }: FieldProps) => (
+                      <TextInput
+                        {...field}
+                        label={<LabelWithDeleteButton onDelete={() => remove(index)} />}
+                        placeholder="Type here..."
+                        maxLength={100}
+                      />
+                    )}
+                  </Field>
+                </>
+              ))}
+              <Box gridColumn="span 2">
+                <AddButton onClick={() => push({ type: '', value: '' })} />
+              </Box>
+            </>
+          )}
+        </FieldArray>
+      </StyledGrid>
+    </Box>
+  )
+}
---
+++ centrifuge-app/src/pages/IssuerCreatePool/PoolRatings.tsx
@@ -0,0 +1,80 @@
+import { PoolMetadataInput } from '@centrifuge/centrifuge-js'
+import { Box, FileUpload, Text, TextInput } from '@centrifuge/fabric'
+import { Field, FieldArray, FieldProps, useFormikContext } from 'formik'
+import { FieldWithErrorMessage } from '../../../src/components/FieldWithErrorMessage'
+import { AddButton } from './PoolDetailsSection'
+import { StyledGrid } from './PoolStructureSection'
+
+export const PoolRatingsSection = () => {
+  const form = useFormikContext<PoolMetadataInput>()
+  return (
+    <Box mt={4} mb={3}>
+      <Text variant="heading2">Pool rating</Text>
+      <StyledGrid gridTemplateColumns={['1fr', '1fr 1fr']} mt={3}>
+        <FieldArray name="poolRatings">
+          {({ push }) => (
+            <>
+              {form.values.poolRatings.map((_, index) => (
+                <>
+                  <Field name={`poolRatings.${index}.agency`}>
+                    {({ field, meta }: FieldProps) => (
+                      <FieldWithErrorMessage
+                        {...field}
+                        as={TextInput}
+                        label="Rating agency"
+                        placeholder="Type here..."
+                      />
+                    )}
+                  </Field>
+
+                  <Field name={`poolRatings.${index}.value`}>
+                    {({ field, meta }: FieldProps) => (
+                      <FieldWithErrorMessage
+                        {...field}
+                        as={TextInput}
+                        label="Rating value"
+                        placeholder="Type here..."
+                      />
+                    )}
+                  </Field>
+
+                  <Field name={`poolRatings.${index}.reportUrl`}>
+                    {({ field, meta }: FieldProps) => (
+                      <FieldWithErrorMessage
+                        {...field}
+                        as={TextInput}
+                        label="Rating report URL"
+                        placeholder="Type here..."
+                        isUrl
+                      />
+                    )}
+                  </Field>
+
+                  <Field name={`poolRatings.${index}.reportFile`}>
+                    {({ field, form }: FieldProps) => (
+                      <FileUpload
+                        file={field.value}
+                        onFileChange={(file) => {
+                          form.setFieldTouched(`poolRatings.${index}.reportFile`, true, false)
+                          form.setFieldValue(`poolRatings.${index}.reportFile`, file)
+                        }}
+                        accept="application/pdf"
+                        label="Executive summary PDF"
+                        placeholder="Choose file"
+                        small
+                      />
+                    )}
+                  </Field>
+                </>
+              ))}
+
+              <Box gridColumn="span 2">
+                <AddButton onClick={() => push({ agency: '', value: '', reportUrl: '' })} />
+              </Box>
+            </>
+          )}
+        </FieldArray>
+      </StyledGrid>
+    </Box>
+  )
+}
