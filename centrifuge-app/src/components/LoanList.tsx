540c1d05e7ab99b4d12993f5d40e32adcedddd2d
sophian <littlejohn.sophia@gmail.com>
2025-01-09 16:10:01 -0500

    Remove isLoading since it's not defined anymore

--- centrifuge-app/src/components/LoanList.tsx
+++ centrifuge-app/src/components/LoanList.tsx
@@ -262,7 +262,7 @@ export function LoanList({ loans }: Props) {
   }, [rows, pool])
 
   const csvUrl = React.useMemo(() => csvData && getCSVDownloadUrl(csvData as any), [csvData])
-  const filteredData = isLoading ? [] : showRepaid ? rows : rows.filter((row) => !row.marketValue?.isZero())
+  const filteredData = showRepaid ? rows : rows.filter((row) => !row.marketValue?.isZero())
   const pagination = usePagination({ data: filteredData, pageSize: 20 })
 
   return (
