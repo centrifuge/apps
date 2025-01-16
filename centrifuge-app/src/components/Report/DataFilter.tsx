56835126f1e6bd063222202012671fd3519b7977
katty barroso <kattybarroso1321@gmail.com>
2025-01-16 11:44:03 +0100

    Remove date on top

--- centrifuge-app/src/components/Report/DataFilter.tsx
+++ centrifuge-app/src/components/Report/DataFilter.tsx
@@ -236,16 +236,22 @@ export function DataFilter({ poolId }: ReportFilterProps) {
           </>
         )}
       </Grid>
-      <Box display="flex" alignItems="center" justifyContent="space-between" margin={2}>
-        <Box display="flex" alignItems="center">
-          <Text variant="body3" style={{ marginRight: 4, fontWeight: 600 }}>
-            {formatDate(startDate)}
-          </Text>
-          <Text variant="body3">-</Text>
-          <Text variant="body3" style={{ marginLeft: 4, fontWeight: 600 }}>
-            {formatDate(endDate)}
-          </Text>
-        </Box>
+      <Grid
+        gridTemplateColumns={[report === 'orders' ? '130px' : '1fr 130px']}
+        margin={2}
+        justifyContent={report === 'orders' ? 'end' : 'space-between'}
+      >
+        {report !== 'orders' && (
+          <Box display="flex" alignItems="center" justifyContent="flex-start">
+            <Text variant="body3" style={{ marginRight: 4, fontWeight: 600 }}>
+              {formatDate(startDate)}
+            </Text>
+            <Text variant="body3">-</Text>
+            <Text variant="body3" style={{ marginLeft: 4, fontWeight: 600 }}>
+              {formatDate(endDate)}
+            </Text>
+          </Box>
+        )}
         <AnchorButton
           disabled={!csvData}
           download={csvData?.fileName}
@@ -256,7 +262,7 @@ export function DataFilter({ poolId }: ReportFilterProps) {
         >
           Download
         </AnchorButton>
-      </Box>
+      </Grid>
     </Stack>
   )
 }
