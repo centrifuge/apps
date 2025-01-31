ae2a53b17845d1cf1f7151f373606c0a0a45f073
sophian <littlejohn.sophia@gmail.com>
2025-01-31 17:13:44 -0500

    Add styling and table to tranche slide out

--- centrifuge-app/src/pages/Dashboard/InvestorsPage.tsx
+++ centrifuge-app/src/pages/Dashboard/InvestorsPage.tsx
@@ -1,12 +1,34 @@
 import { CurrencyBalance, FileType } from '@centrifuge/centrifuge-js'
 import { NetworkIcon, formatBalance, useCentrifuge, useGetNetworkName } from '@centrifuge/centrifuge-react'
-import { Box, Button, Checkbox, Drawer, Shelf, Stack, Text } from '@centrifuge/fabric'
+import {
+  Box,
+  Button,
+  Checkbox,
+  Divider,
+  Drawer,
+  IconCopy,
+  IconExternalLink,
+  IconMoreVertical,
+  Menu,
+  MenuItem,
+  Popover,
+  Shelf,
+  Stack,
+  Text,
+  truncate,
+} from '@centrifuge/fabric'
 import { useState } from 'react'
-import { useLocation, useNavigate } from 'react-router'
+import { useNavigate, useSearchParams } from 'react-router-dom'
 import { Column, DataTable, FilterableTableHeader, SortableTableHeader } from '../../components/DataTable'
 import { CopyToClipboard } from '../../utils/copyToClipboard'
 import { useFilters } from '../../utils/useFilters'
-import { useInvestorListMulti, usePoolMetadataMulti, usePools } from '../../utils/usePools'
+import {
+  useInvestorListMulti,
+  usePool,
+  usePoolMetadataMulti,
+  usePools,
+  useTransactionsByAddress,
+} from '../../utils/usePools'
 
 type Row = {
   tokenName: string | undefined
@@ -19,28 +41,31 @@ type Row = {
   pendingRedemptions: CurrencyBalance
   investorSince: string
   poolCurrency: string | undefined
+  poolId: string
 }
 
 export default function InvestorsPage() {
-  const navigate = useNavigate()
-  const { search } = useLocation()
-  const investorId = new URLSearchParams(search).get('d_investorId')
   const pools = usePools()?.slice(0, 3)
-  const getNetworkName = useGetNetworkName()
+  const navigate = useNavigate()
   const [selectedPools, setSelectedPools] = useState<string[]>(pools?.map((p) => p.id) ?? [])
+  const [searchParams] = useSearchParams()
+  const investorParam = searchParams.get('d_investor')
   const cent = useCentrifuge()
   const poolMetadata = usePoolMetadataMulti(pools ?? [])
   const investors = useInvestorListMulti(selectedPools)
+  const getNetworkName = useGetNetworkName()
   const data: Row[] =
     investors?.map((investor) => {
       // match metadata to pool by trancheId since poolId doesnt exist in metadata
       const metadata = poolMetadata.find((p) => Object.keys(p.data?.tranches ?? {}).includes(investor.trancheId))
-      const tokenName = pools?.find((p) => p.tranches.find((t) => t.id === investor.trancheId))?.tranches[0].currency
-        .displayName
-      const poolCurrency = pools?.find((p) => p.id === investor.poolId)?.tranches[0].currency.displayName
+      const tokenName = pools
+        ?.find((p) => p.tranches.find((t) => t.id === investor.trancheId))
+        ?.tranches.find((t) => t.id === investor.trancheId)?.currency.displayName
+      const poolCurrency = pools?.find((p) => p.id === investor.poolId)?.currency.displayName
       return {
         tokenName,
         trancheId: investor.trancheId,
+        poolId: investor.poolId,
         poolIcon: metadata?.data?.pool?.icon,
         poolCurrency,
         wallet: investor?.evmAddress || investor.accountId || '',
@@ -49,6 +74,7 @@ export default function InvestorsPage() {
         pendingInvestments: investor.pendingInvestCurrency,
         pendingRedemptions: investor.pendingRedeemTrancheTokens,
         investorSince: '', // TODO: get investorSince
+        investorId: `${investor.evmAddress || investor.accountId}-${investor.trancheId}-${investor.chainId}`,
       }
     }) ?? []
 
@@ -122,13 +148,15 @@ export default function InvestorsPage() {
   ]
   return (
     <Stack gap={4} py={3} px={3}>
-      <InvestorDrawer
-        isOpen={!!investorId}
-        onClose={() => {
-          navigate('/dashboard/investors')
-        }}
-        investor={filters.data?.find((i) => `${i.wallet}-${i.trancheId}-${i.network}` === investorId) ?? null}
-      />
+      {filters.data?.find((i) => `${i.wallet}-${i.trancheId}-${i.network}` === investorParam) && (
+        <InvestorDrawer
+          isOpen={!!investorParam}
+          onClose={() => {
+            navigate('/dashboard/investors')
+          }}
+          investor={filters.data.find((i) => `${i.wallet}-${i.trancheId}-${i.network}` === investorParam)!}
+        />
+      )}
       <Shelf gap={1}>
         {pools?.map((p) => (
           <Checkbox
@@ -177,25 +205,199 @@ export default function InvestorsPage() {
           defaultSortKey="poolTokenId"
           defaultSortOrder="asc"
           scrollable
-          onRowClicked={(row) => `?d_investorId=${row.wallet}-${row.trancheId}-${row.network}`}
+          onRowClicked={(row) => `?d_investor=${row.wallet}-${row.trancheId}-${row.network}`}
         />
       </Box>
     </Stack>
   )
 }
 
-function InvestorDrawer({ isOpen, onClose, investor }: { isOpen: boolean; onClose: () => void; investor: Row | null }) {
+function InvestorDrawer({ isOpen, onClose, investor }: { isOpen: boolean; onClose: () => void; investor: Row }) {
+  const cent = useCentrifuge()
+  const pool = usePool(investor.poolId)
+  const getNetworkName = useGetNetworkName()
+  const columns: Column[] = [
+    {
+      header: '1. Wallet',
+      align: 'left',
+      cell: (row) => (
+        <Text variant="body3" fontWeight="400">
+          {row.firstColumn}
+        </Text>
+      ),
+      width: '40%',
+    },
+    {
+      header: truncate(investor.wallet),
+      align: 'left',
+      cell: (row) => (
+        <Text variant="body3" fontWeight="600">
+          {row.secondColumn}
+        </Text>
+      ),
+      width: '40%',
+    },
+    {
+      header: (
+        <Popover
+          renderTrigger={(props, ref) => (
+            <Box ref={ref}>
+              <Button
+                {...props}
+                variant="tertiary"
+                onClick={(event) => {
+                  console.log('clicked')
+                  props?.onClick?.(event)
+                }}
+                small
+                icon={<IconMoreVertical size="iconSmall" />}
+              />
+            </Box>
+          )}
+          renderContent={(props, ref) => (
+            <Box ref={ref} {...props} width="200px">
+              <Menu backgroundColor="white">
+                <MenuItem label="Disable" onClick={() => console.log('Action 1')} />
+                <MenuItem label="Freeze" onClick={() => console.log('Action 2')} />
+                <Divider />
+                <Shelf gap={1} justifyContent="space-between" paddingRight="12px">
+                  <MenuItem label="Copy" onClick={() => console.log('Action 3')} />
+                  <IconCopy size="iconMedium" />
+                </Shelf>
+              </Menu>
+            </Box>
+          )}
+        ></Popover>
+      ),
+      align: 'right',
+      cell: () => '',
+    },
+  ]
+  const data = [
+    {
+      firstColumn: 'Network',
+      secondColumn: (
+        <Shelf gap="4px">
+          <NetworkIcon size="iconSmall" network={investor.network || 'centrifuge'} />
+          <Text>{getNetworkName(investor.network || 'centrifuge')}</Text>
+        </Shelf>
+      ),
+    },
+    {
+      firstColumn: 'Investment position',
+      secondColumn: formatBalance(investor.holdings, investor.tokenName, 2),
+    },
+    {
+      firstColumn: 'Pending investments',
+      secondColumn: formatBalance(investor.pendingInvestments, investor.poolCurrency, 2),
+    },
+    {
+      firstColumn: 'Pending redemptions',
+      secondColumn: formatBalance(investor.pendingRedemptions, investor.poolCurrency, 2),
+    },
+  ]
   if (!investor) return null
+  const iconUri = investor.poolIcon?.uri && cent.metadata.parseMetadataUrl(investor.poolIcon?.uri)
   return (
-    <Drawer isOpen={isOpen} onClose={onClose} width="50%">
-      <Text variant="heading1">Dashboard</Text>
-      <Text variant="heading2">{investor.tokenName}</Text>
-      <Text variant="body2">Wallet: {investor.wallet}</Text>
-      <Text variant="body2">Network: {investor.network}</Text>
-      <Text variant="body2">Holdings: {investor.holdings.toString()}</Text>
-      <Text variant="body2">Pending investments: {investor.pendingInvestments.toString()}</Text>
-      <Text variant="body2">Pending redemptions: {investor.pendingRedemptions.toString()}</Text>
-      <Text variant="body2">Investor since: {investor.investorSince}</Text>
+    <Drawer isOpen={isOpen} onClose={onClose} width="37%" innerPaddingTop={2}>
+      <Stack gap="18px">
+        <Shelf gap={1}>
+          <Box as="img" width="iconMedium" height="iconMedium" src={iconUri} borderRadius={1} />
+          <Text variant="body2" fontWeight="500">
+            {investor.tokenName}
+          </Text>
+        </Shelf>
+        <Divider />
+        <Shelf gap={1}>
+          <Stack>
+            <Text variant="body2" color="textSecondary">
+              Realized P&L ({pool.currency.displayName})
+            </Text>
+            <Text variant="body2" color="textPrimary" fontWeight="600">
+              {/* // TODO: get realized P&L */}
+              {formatBalance(investor.holdings, undefined, 2)}
+            </Text>
+          </Stack>
+          <Stack>
+            <Text variant="body2" color="textSecondary">
+              Unrealized P&L ({pool.currency.displayName})
+            </Text>
+            <Text variant="body2" color="textPrimary" fontWeight="600">
+              {/* // TODO: get unrealized P&L */}
+              {formatBalance(investor.holdings, undefined, 2)}
+            </Text>
+          </Stack>
+          <Stack>
+            <Text variant="body2" color="textSecondary">
+              Investor since
+            </Text>
+            <Text variant="body2" color="textPrimary" fontWeight="600">
+              {/* // TODO: get investor since */}
+              {investor.investorSince || 'today'}
+            </Text>
+          </Stack>
+        </Shelf>
+        <Divider />
+        <Text variant="body2" fontWeight="700">
+          Wallet
+        </Text>
+        <DataTable data={data} columns={columns} />
+        <Shelf justifyContent="space-between">
+          <Text variant="body2" fontWeight="700">
+            Transaction history
+          </Text>
+          <Button variant="tertiary" small>
+            View all
+          </Button>
+        </Shelf>
+        <MiniTransactionHistoryTable poolId={investor.poolId} investor={investor} />
+        <Text variant="body2">Network: {investor.network}</Text>
+        <Text variant="body2">Holdings: {investor.holdings.toString()}</Text>
+        <Text variant="body2">Pending investments: {investor.pendingInvestments.toString()}</Text>
+        <Text variant="body2">Pending redemptions: {investor.pendingRedemptions.toString()}</Text>
+        <Text variant="body2">Investor since: {investor.investorSince}</Text>
+      </Stack>
     </Drawer>
   )
+}
+
+const MiniTransactionHistoryTable = ({ poolId, wallet }: { poolId: string; wallet: string }) => {
+  const transactions = useTransactionsByAddress('0x30d3bbae8623d0e9c0db5c27b82dcda39de40997')
+  const columns: Column[] = [
+    {
+      header: 'Action',
+      align: 'left',
+      cell: (row) => <Text>{row.action}</Text>,
+    },
+    {
+      header: 'Token',
+      align: 'left',
+      cell: (row) => (
+        <Stack>
+          <Text>{row.tokenName}</Text>
+        </Stack>
+      ),
+    },
+    {
+      header: 'Amount',
+      align: 'left',
+      cell: (row) => <Text>{formatBalance(row.amount, row.tokenName, 2)}</Text>,
+    },
+    {
+      header: '',
+      align: 'left',
+      cell: (row) => <IconExternalLink size="iconSmall" />,
+    },
+  ]
+
+  const data =
+    transactions.data?.investorTransactions.map((t) => ({
+      date: t.timestamp,
+      action: 'Some action',
+      tokenName: 'Some token',
+      tokenPrice: t.tokenPrice,
+      amount: t.currencyAmount,
+    })) ?? []
+
+  return <DataTable data={data} columns={columns} />
 }
