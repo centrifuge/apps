dd6b9248458c95f7c777122aac3b5fc6bb2bc42f
katty barroso <kattybarroso1321@gmail.com>
2025-02-06 17:44:51 +0100

    Add dashboard landing page and context

--- centrifuge-app/src/pages/Dashboard/index.tsx
+++ centrifuge-app/src/pages/Dashboard/index.tsx
@@ -1,16 +1,24 @@
 import { Route, Routes } from 'react-router'
+import { SelectedPoolsProvider, useSelectedPools } from '../../../src/utils/contexts/SelectedPoolsContext'
 import AccountsPage from './AccountsPage'
 import AssetsPage from './AssetsPage'
 import Dashboard from './Dashboard'
 import InvestorsPage from './InvestorsPage'
 
 export default function DashboardPage() {
+  const { pools } = useSelectedPools()
+
+  // Talk to product about a message here or?
+  if (!pools?.length) return
+
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/accounts" element={<AccountsPage />} />
-      <Route path="/assets" element={<AssetsPage />} />
-      <Route path="/investors" element={<InvestorsPage />} />
-    </Routes>
+    <SelectedPoolsProvider>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/accounts" element={<AccountsPage />} />
+        <Route path="/assets" element={<AssetsPage pools={pools} />} />
+        <Route path="/investors" element={<InvestorsPage />} />
+      </Routes>
+    </SelectedPoolsProvider>
   )
 }
---
+++ centrifuge-app/src/utils/contexts/SelectedPoolsContext.tsx
@@ -0,0 +1,50 @@
+import { Pool } from '@centrifuge/centrifuge-js'
+import React, { ReactNode, createContext, useContext, useState } from 'react'
+import { usePools } from '../usePools'
+
+interface SelectedPoolsContextProps {
+  selectedPools: string[]
+  togglePoolSelection: (poolId: string) => void
+  setSelectedPools: React.Dispatch<React.SetStateAction<string[]>>
+  clearSelectedPools: () => void
+  pools: Pool[] | undefined
+}
+
+const SelectedPoolsContext = createContext<SelectedPoolsContextProps | undefined>(undefined)
+
+export const useSelectedPools = (): SelectedPoolsContextProps => {
+  const context = useContext(SelectedPoolsContext)
+  if (!context) {
+    throw new Error('useSelectedPools must be used within a SelectedPoolsProvider')
+  }
+  return context
+}
+
+interface SelectedPoolsProviderProps {
+  children: ReactNode
+}
+
+export const SelectedPoolsProvider = ({ children }: SelectedPoolsProviderProps) => {
+  // const pools = usePoolsThatAnyConnectedAddressHasPermissionsFor()
+  // FOR TESTING ONLY
+  const pools = usePools()
+  const [selectedPools, setSelectedPools] = useState<string[]>([])
+
+  const togglePoolSelection = (poolId: string) => {
+    setSelectedPools((prevSelected) =>
+      prevSelected.includes(poolId) ? prevSelected.filter((id) => id !== poolId) : [...prevSelected, poolId]
+    )
+  }
+
+  const clearSelectedPools = () => {
+    setSelectedPools([])
+  }
+
+  return (
+    <SelectedPoolsContext.Provider
+      value={{ selectedPools, togglePoolSelection, setSelectedPools, clearSelectedPools, pools: pools ?? [] }}
+    >
+      {children}
+    </SelectedPoolsContext.Provider>
+  )
+}
