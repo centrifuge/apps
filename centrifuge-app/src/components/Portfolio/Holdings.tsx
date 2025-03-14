5c788707f39de5281860526ae124fea61ba0f1b2
Onno Visser <23527729+onnovisser@users.noreply.github.com>
2025-03-14 14:59:45 +0100

    cleanup logs

--- centrifuge-app/src/components/Portfolio/Holdings.tsx
+++ centrifuge-app/src/components/Portfolio/Holdings.tsx
@@ -165,9 +165,6 @@ const columns: Column[] = [
 
 export function useHoldings(address?: string, chainId?: number, showActions = true) {
   const centAddress = address && chainId && isEvmAddress(address) ? evmToSubstrateAddress(address, chainId) : address
-  console.log('centAddress', centAddress)
-  console.log('address', address)
-  console.log('chainId', chainId)
   const { data: tinlakeBalances } = useTinlakeBalances(address && isEvmAddress(address) ? address : undefined)
   const centBalances = useBalances(centAddress)
   const match = useMatch('/portfolio')
--- centrifuge-react/src/components/WalletProvider/WalletProvider.tsx
+++ centrifuge-react/src/components/WalletProvider/WalletProvider.tsx
@@ -514,8 +514,6 @@ export function WalletProvider({
     }
   }, [connect, disconnect, proxies, state, isConnectError, isConnecting, isSmartContractWallet])
 
-  console.log('walletcontext', ctx)
-
   return (
     <WalletContext.Provider value={ctx}>
       {children}
