140e6e29482eec145a7963b47a8c2e4aa3a860af
Katty Barroso <kattybarroso1321@gmail.com>
2025-03-18 07:02:26 +0100

    Add eth migration page

--- centrifuge-app/src/pages/Portfolio/CFGTokenMigration.tsx
+++ centrifuge-app/src/pages/Portfolio/CFGTokenMigration.tsx
@@ -1,26 +1,64 @@
 import { ConnectionGuard, truncateAddress, useAddress } from '@centrifuge/centrifuge-react'
-import { Box, Button, CurrencyInput, Divider, Grid, IconArrowRight, IconInfo, Text } from '@centrifuge/fabric'
+import { Box, Button, CurrencyInput, Divider, Grid, IconArrowRight, IconInfo, Stack, Text } from '@centrifuge/fabric'
 import { getAddress } from 'ethers'
 import { useState } from 'react'
 import { useTheme } from 'styled-components'
 import { LayoutSection } from '../../../src/components/LayoutBase/LayoutSection'
-import { Dec } from '../../utils/Decimal'
+import { Tooltips } from '../../components/Tooltips'
+import { Dec, Decimal } from '../../utils/Decimal'
 import { formatBalance } from '../../utils/formatting'
 import { useTinlakeBalances } from '../../utils/tinlake/useTinlakeBalances'
 import { useCFGTokenPrice } from '../../utils/useCFGTokenPrice'
+
+const TooltipText = () => {
+  return (
+    <Stack gap={2}>
+      <Text variant="body4" color="white">
+        - <b>1:1 conversion</b>: You will receive <b>1 CFG for every 1 WCFG/Legacy CFG migrated</b>.
+      </Text>
+      <Text variant="body4" color="white">
+        - <b>Gas fees</b>: A small ETH gas fee is required to complete the migration.
+      </Text>
+      <Text variant="body4" color="white">
+        - <b>Why migrate?</b>:The old Centrifuge chain will be <b>discontinued in Q4 2025</b>. Ensure your tokens are
+        upgraded.
+      </Text>
+      <Text variant="body4" color="white">
+        - <b>Snapshot date</b>: The total supply snapshot will be taken on <b>March 25, 2025</b>. For more details,
+        visit the {''}
+        <a
+          href="https://docs.centrifuge.io"
+          target="_blank"
+          rel="noreferrer"
+          style={{ color: 'white', textDecoration: 'underline' }}
+        >
+          Centrifuge Docs
+        </a>
+        .
+      </Text>
+    </Stack>
+  )
+}
 
 export default function CFGTokenMigration() {
   const theme = useTheme()
   const address = useAddress('evm')
   const { data: balances } = useTinlakeBalances(address)
-  const wcfgBalance = balances?.currencies.find((balance) => balance.currency.symbol === 'wCFG')?.balance || Dec(0)
+  const wcfg = balances?.currencies.find((balance) => balance.currency.symbol === 'wCFG')
+  const wcfgBalance = wcfg?.balance || Dec(0)
   const formattedAddress = address ? getAddress(address) : ''
   const CFGPrice = useCFGTokenPrice()
+  const convertedWcfgBalance =
+    wcfgBalance instanceof Decimal ? wcfgBalance || Dec(0) : wcfgBalance.toDecimal() || Dec(0)
+  const wcfgValue = convertedWcfgBalance.mul(Dec(CFGPrice || 0))
 
   const [amountToTransfer, setAmountToTransfer] = useState<number>(0)
   const [isMigrated, setIsMigrated] = useState<boolean>(false)
 
-  console.log(CFGPrice)
+  const migrate = () => {
+    console.log(amountToTransfer)
+    setIsMigrated(true)
+  }
 
   return (
     <ConnectionGuard networks={[1]}>
@@ -52,7 +90,12 @@ export default function CFGTokenMigration() {
               <Box>
                 <Grid gridTemplateColumns="1fr 24px" alignItems="center" mb={2}>
                   <Text variant="heading2">WCFG - Migration Successful</Text>
-                  <IconInfo size="iconSmall" />
+                  <Tooltips
+                    type="nav"
+                    placement="bottom"
+                    label={<IconInfo size="iconSmall" />}
+                    body={<TooltipText />}
+                  />
                 </Grid>
                 <Divider color="borderSecondary" />
                 <Grid gridTemplateColumns="1fr 100px" alignItems="center" mb={2} mt={2}>
@@ -98,15 +141,32 @@ export default function CFGTokenMigration() {
               <>
                 <Grid gridTemplateColumns="1fr 24px" alignItems="center" mb={2}>
                   <Text variant="heading2">{`WCFG -> CFG Migration`}</Text>
-                  <IconInfo size="iconSmall" />
+                  <Tooltips
+                    type="nav"
+                    placement="bottom"
+                    label={<IconInfo size="iconSmall" />}
+                    body={<TooltipText />}
+                  />
                 </Grid>
                 <Divider color="borderSecondary" />
-                <Grid gridTemplateColumns="1fr 1fr 1fr" alignItems="center" mb={2} maxWidth="50%" mt={2}>
+                <Grid gridTemplateColumns="1fr 1fr 1fr" alignItems="center" mb={2} mt={2}>
                   <Box>
                     <Text variant="body3" color="textSecondary">
                       Position
                     </Text>
-                    <Text variant="heading3">{formatBalance(wcfgBalance)} WCFG</Text>
+                    <Text variant="heading3">{formatBalance(wcfgBalance, '', 2)} WCFG</Text>
+                  </Box>
+                  <Box>
+                    <Text variant="body3" color="textSecondary">
+                      Value
+                    </Text>
+                    <Text variant="heading3">{formatBalance(wcfgValue, '', 2)} WCFG</Text>
+                  </Box>
+                  <Box>
+                    <Text variant="body3" color="textSecondary">
+                      CFG price
+                    </Text>
+                    <Text variant="heading3">{formatBalance(CFGPrice || 0, '', 2)} CFG</Text>
                   </Box>
                 </Grid>
                 <Box border={`1px solid ${theme.colors.borderSecondary}`} borderRadius={8} p={2} mb={3}>
@@ -116,6 +176,7 @@ export default function CFGTokenMigration() {
                       onChange={(e) => setAmountToTransfer(e === '' ? 0 : e)}
                       currency="WCFG"
                       label="Amount of WCFG to migrate"
+                      max={convertedWcfgBalance.toString()}
                     />
                     <Text style={{ marginTop: 8, alignSelf: 'flex-end' }} variant="body2">
                       Wallet balance: {formatBalance(wcfgBalance)} WCFG
@@ -123,7 +184,12 @@ export default function CFGTokenMigration() {
                   </Box>
                   <CurrencyInput value={amountToTransfer} currency="CFG" label="Amount of CFG tokens" disabled />
                 </Box>
-                <Button small style={{ width: '100%' }}>
+                <Button
+                  small
+                  style={{ width: '100%' }}
+                  disabled={amountToTransfer === 0 || wcfgBalance.isZero()}
+                  onClick={migrate}
+                >
                   Migrate
                 </Button>
               </>
