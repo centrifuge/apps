60beeb3be085655b904f3cb66be6374e7ba685ff
Katty Barroso <kattybarroso1321@gmail.com>
2025-03-19 09:51:06 +0100

    use query instead of fetch

--- centrifuge-app/src/pages/Portfolio/useTokenBalance.tsx
+++ centrifuge-app/src/pages/Portfolio/useTokenBalance.tsx
@@ -1,7 +1,7 @@
 import { CurrencyBalance } from '@centrifuge/centrifuge-js'
 import { BrowserProvider, ethers } from 'ethers'
-import { useEffect, useState } from 'react'
-import { Decimal } from '../../utils/Decimal'
+import { useQuery } from 'react-query'
+import { Decimal } from '../../../src/utils/Decimal'
 
 // Replace with your contract address
 const CONTRACT_ADDRESS = '0x657a4556e60A6097975e2E6dDFbb399E5ee9a58b'
@@ -10,31 +10,22 @@ const CONTRACT_ADDRESS = '0x657a4556e60A6097975e2E6dDFbb399E5ee9a58b'
 const ABI = ['function balanceOf(address owner) view returns (uint256)']
 
 export const useTokenBalance = (userAddress: string | undefined) => {
-  const [balance, setBalance] = useState<Decimal | null>(null)
-  const [error, setError] = useState<unknown | null>(null)
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-    if (!userAddress) return
-
-    const fetchBalance = async () => {
-      try {
-        const provider = new BrowserProvider(window.ethereum)
-
-        const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider)
-        const rawBalance = await contract.balanceOf(userAddress)
-        const balance = new CurrencyBalance(rawBalance.toString(), 18).toDecimal()
-        setBalance(balance)
-      } catch (err) {
-        console.error(err)
-        setError(err)
-      }
+  const {
+    data: balance,
+    error,
+    isLoading,
+  } = useQuery<Decimal, unknown>(
+    ['tokenBalance', userAddress],
+    async () => {
+      const provider = new BrowserProvider(window.ethereum)
+      const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider)
+      const rawBalance = await contract.balanceOf(userAddress!)
+      return new CurrencyBalance(rawBalance.toString(), 18).toDecimal()
+    },
+    {
+      enabled: !!userAddress,
     }
+  )
 
-    fetchBalance()
-    setLoading(false)
-  }, [userAddress])
-
-  return { balance, error, loading }
+  return { balance: balance ?? null, error, loading: isLoading }
 }
