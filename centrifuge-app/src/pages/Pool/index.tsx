87d49f0915894ca618e14c5af928b133c6c1fbfa
Katty Barroso <kattybarroso1321@gmail.com>
2025-06-02 19:41:21 +0200

    Add assets page behind flag

--- centrifuge-app/src/pages/Pool/index.tsx
+++ centrifuge-app/src/pages/Pool/index.tsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from 'react-router-dom'
+import { PoolDetailAssetsTab } from './Assets'
 import { PoolDetailOverviewTab } from './Overview'
 import { PoolDetailReportingTab } from './Reporting'
 
@@ -9,7 +10,7 @@ export default function PoolDetailPage() {
       <Route path="reporting/:report" element={<PoolDetailReportingTab />} />
       <Route path="data/:report" element={<PoolDetailReportingTab />} />
       <Route path="reporting" element={<PoolDetailReportingTab />} />
-      <Route path="data" element={<PoolDetailReportingTab />} />
+      <Route path="assets" element={<PoolDetailAssetsTab />} />
     </Routes>
   )
 }
